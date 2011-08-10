708c6096c9e9ff1bac554a158ecc953fc1bd50f2
Vic Lee <llyzs@163.com>
2011-08-10 09:58:42 +0800

    libfreerdp-utils: migrate profiler.

---
+++ include/freerdp/utils/stopwatch.h
@@ -0,0 +1,44 @@
+/**
+ * FreeRDP: A Remote Desktop Protocol Client
+ * Stopwatch Utils
+ *
+ * Copyright 2011 Stephen Erisman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __UTILS_STOPWATCH_H
+#define __UTILS_STOPWATCH_H
+
+#include <freerdp/utils/memory.h>
+#include <time.h>
+
+struct _STOPWATCH
+{
+	clock_t start;
+	clock_t end;
+	double elapsed;
+	clock_t count;
+};
+typedef struct _STOPWATCH STOPWATCH;
+
+STOPWATCH* stopwatch_create();
+void stopwatch_free(STOPWATCH* stopwatch);
+
+void stopwatch_start(STOPWATCH* stopwatch);
+void stopwatch_stop(STOPWATCH* stopwatch);
+void stopwatch_reset(STOPWATCH* stopwatch);
+
+double stopwatch_get_elapsed_time_in_seconds(STOPWATCH* stopwatch);
+
+#endif /* __UTILS_STOPWATCH_H */
