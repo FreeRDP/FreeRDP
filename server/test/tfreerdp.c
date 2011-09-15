95c2aede06a878e9f1d9627db3670f49502d7402
Marc-André Moreau <marcandre.moreau@gmail.com>
2011-09-15 17:39:23 -0400

    tfreerdp-server: fix compilation on Mac OS X

--- server/test/tfreerdp.c
+++ server/test/tfreerdp.c
@@ -24,6 +24,7 @@
 #include <errno.h>
 #include <pthread.h>
 #include <signal.h>
+#include <sys/time.h>
 #include <freerdp/constants.h>
 #include <freerdp/utils/sleep.h>
 #include <freerdp/utils/memory.h>
