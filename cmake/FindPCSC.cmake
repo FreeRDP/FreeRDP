7af559911388a37824334cbadc07f2a7a322aedc
Bernhard Miklautz <bmiklautz@thinstuff.com>
2012-01-27 02:13:08 +0100

    build: raise an error if pkg is not found
    Changed FFmpeg, PCSC and PulseAudio cmake find to raise an error if
    an required package was not found.

--- cmake/FindPCSC.cmake
+++ cmake/FindPCSC.cmake
@@ -1,3 +1,13 @@
-pkg_check_modules(PCSC libpcsclite)
+include(FindPkgConfig)
 
-# TODO: pcsc version
+if (PKG_CONFIG_FOUND)
+	pkg_check_modules(PCSC libpcsclite)
+endif()
+
+find_path(PCSC_INCLUDE_DIR pcsclite.h PATHS ${PCSC_INCLUDE_DIRS} 
+          PATH_SUFFIXES PCSC )
+find_library(PCSC_LIBRARY pcsclite PATHS ${PCSC_LIBRARY_DIRS})
+
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(PCSC DEFAULT_MSG PCSC_INCLUDE_DIR PCSC_LIBRARY)
+
+mark_as_advanced(PCSC_INCLUDE_DIR PCSC_LIBRARY)
