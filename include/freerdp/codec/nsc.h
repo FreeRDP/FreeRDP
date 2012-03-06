d2153e9ef2ab9383b5b4db8cc8eff1836f7df09f
Vic Lee <llyzs@163.com>
2012-03-06 18:52:28 +0800

    nsc: refactor nsc decoder #3
    o combine colorloss recovery, chroma supersampling and color space conversion
      in one step
    o define private struct and hide the internal buffer
    o make internal buffer reusable in the same session

--- include/freerdp/codec/nsc.h
+++ include/freerdp/codec/nsc.h
@@ -29,7 +29,7 @@ extern "C" {
 #endif
 
 #define BYTESUM(_bs) (_bs[0] + _bs[1] + _bs[2] + _bs[3])
-#define ROUND_UP_TO(_b, _n) (_b + ((~(_b & (_n-1)) + 0x1) & (_n-1)));
+#define ROUND_UP_TO(_b, _n) (_b + ((~(_b & (_n-1)) + 0x1) & (_n-1)))
 
 /* NSCODEC_BITMAP_STREAM */
 struct _NSC_STREAM
@@ -42,6 +42,8 @@ struct _NSC_STREAM
 };
 typedef struct _NSC_STREAM NSC_STREAM;
 
+typedef struct _NSC_CONTEXT_PRIV NSC_CONTEXT_PRIV;
+
 struct _NSC_CONTEXT
 {
 	uint32 OrgByteCount[4];	/* original byte length of luma, chroma orange, chroma green, alpha variable in order */
@@ -51,19 +53,13 @@ struct _NSC_CONTEXT
 	uint16 height;
 	uint8* bmpdata;     /* final argb values in little endian order */
 	uint32 bmpdata_length; /* the maximum length of the buffer that bmpdata points to */
-	STREAM* org_buf[4];	/* Decompressed Plane Buffers in the respective order */
+	NSC_CONTEXT_PRIV* priv;
 };
 typedef struct _NSC_CONTEXT NSC_CONTEXT;
 
 FREERDP_API NSC_CONTEXT* nsc_context_new(void);
 FREERDP_API void nsc_process_message(NSC_CONTEXT* context, uint16 bpp,
 	uint16 width, uint16 height, uint8* data, uint32 length);
-FREERDP_API void nsc_rle_decompress_data(NSC_CONTEXT* context);
-FREERDP_API void nsc_ycocg_rgb_convert(NSC_CONTEXT* context);
-FREERDP_API void nsc_chroma_supersample(NSC_CONTEXT* context);
-FREERDP_API void nsc_cl_expand(STREAM* stream, uint8 shiftcount, uint32 origsz);
-FREERDP_API void nsc_colorloss_recover(NSC_CONTEXT* context);
-FREERDP_API void nsc_ycocg_rgb(NSC_CONTEXT* context);
 FREERDP_API void nsc_context_free(NSC_CONTEXT* context);
 
 #ifdef __cplusplus
