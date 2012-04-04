99492506d803a0f5a761e1efe935ee42115b04ba
Bernhard Miklautz <bmiklautz@thinstuff.com>
2012-04-04 15:23:16 +0200

    Changed some functions from inline to static inline
    This fixes inline build issues with clang.
    http://clang.llvm.org/compatibility.html#inline
    (cherry picked from commit 1d32894775edd1bacdbcb4b6c3e129841b637374)

--- libfreerdp-core/fastpath.c
+++ libfreerdp-core/fastpath.c
@@ -82,7 +82,7 @@ uint16 fastpath_read_header(rdpFastPath* fastpath, STREAM* s)
 	return length;
 }
 
-INLINE void fastpath_read_update_header(STREAM* s, uint8* updateCode, uint8* fragmentation, uint8* compression)
+static INLINE void fastpath_read_update_header(STREAM* s, uint8* updateCode, uint8* fragmentation, uint8* compression)
 {
 	uint8 updateHeader;
 
@@ -92,7 +92,7 @@ INLINE void fastpath_read_update_header(STREAM* s, uint8* updateCode, uint8* fra
 	*compression = (updateHeader >> 6) & 0x03;
 }
 
-INLINE void fastpath_write_update_header(STREAM* s, uint8 updateCode, uint8 fragmentation, uint8 compression)
+static INLINE void fastpath_write_update_header(STREAM* s, uint8 updateCode, uint8 fragmentation, uint8 compression)
 {
 	uint8 updateHeader = 0;
 
