9b9398ec124db45ae04d2eae30940c1d1a73ab7d
Brent Collins <brent.collins.83@gmail.com>
2012-08-01 17:05:45 -0500

    Issue #684.
    Fix the problem with a smart card reader being lost when a smart card is removed during a reconnected Windows 2008r2 rdp session. This was due to a improper handling of a message with a duplicate completionID. There is a lengthy comment in the code with explicit details. Also, many minor updates for the code to more closely reflect the protocol documentation - such as correcting packet lengths, undocumented padding, etc.

--- channels/rdpdr/smartcard/scard_main.h
+++ channels/rdpdr/smartcard/scard_main.h
@@ -25,7 +25,73 @@
 
 #include "devman.h"
 #include "rdpdr_types.h"
+#include <freerdp/utils/mutex.h>        /* For CompletionIdsMutex */
+#include <freerdp/utils/list.h>
 #include <freerdp/utils/debug.h>
+
+/* 
+ * When using Windows Server 2008 R2 as the Terminal Services (TS)
+ * server, and with a smart card reader connected to the TS client machine 
+ * and used to authenticate to an existing login session, the TS server 
+ * will initiate the protocol initialization of MS-RDPEFS, Section 1.3.1, 
+ * twice as it re-establishes a connection.  The TS server starts both 
+ * initializations with a "Server Announce Request" message.
+ * When the TS client receives this message, as per Section 3.2.5.1.2,
+ * 
+ * 	The client SHOULD treat this packet as the beginning 
+ *	of a new sequence. The client SHOULD also cancel all 
+ * 	outstanding requests and release previous references to 
+ * 	all devices.
+ *
+ * As of this writing, the code does not cancel all outstanding requests.
+ * This leads to a problem where, after the first MS-RDPEFS initialization,
+ * the TS server sends an SCARD_IOCTL_GETSTATUSCHANGEx control in a message
+ * that uses an available "CompletionID".  The 
+ * TS client doesn't respond immediately because it is blocking while 
+ * waiting for a change in the smart card's status in the reader.
+ * Then the TS server initiates a second MS-RDPEFS initialization sequence.
+ * As noted above, this should cancel the outstanding 
+ * SCARD_IOCTL_GETSTATUSCHANGEx request, but it does not.
+ * At this point, the TS server is free to reuse the previously used
+ * "CompletionID", and it does reuse it for other SCARD_IOCTLs.
+ * Therefore, when the user removes (for example) the card from the reader, 
+ * the TS client sends an "IOCompetion" message in response to the 
+ * GETSTATUSCHANGEx using the original "CompletionID".  The TS server does not 
+ * expect this "CompletionID" and so, as per Section 3.1.5.2 of MS-RDPEFS, 
+ * it treats that "IOCompletion" message as an error and terminates the
+ * virtual channel.
+ *
+ * The following structure is part of a work-around for this missing
+ * capability of canceling outstanding requests.  This work-around 
+ * allows the TS client to send an "IOCompletion" back to the
+ * TS server for the second (and subsequent) SCARD_IOCTLs that use 
+ * the same "CompletionID" as the still outstanding 
+ * SCARD_IOCTL_GETSTATUSCHANGEx.  The work-around in the TS client 
+ * prevents the client from sending the "IOCompletion" back (when
+ * the user removes the card) for the SCARD_IOCTL_GETSTATUSCHANGEx.
+ * 
+ * This TS client expects the responses from the PCSC daemon for the second  
+ * and subsequent SCARD_IOCTLs that use the same "CompletionID"
+ * to arrive at the TS client before the daemon's response to the 
+ * SCARD_IOCTL_GETSTATUSCHANGEx.  This is a race condition.
+ *
+ * The "CompletionIDs" are a global pool of IDs across all "DeviceIDs".  
+ * However, this problem of duplicate "CompletionIDs" only affects smart cards.
+ *
+ * This structure tracks outstanding Terminal Services server "CompletionIDs"
+ * used by the redirected smart card device.
+ */
+struct _COMPLETIONIDINFO
+{
+        uint32 ID;              /* CompletionID */
+        boolean duplicate;      /* Indicates whether or not this 
+				 * CompletionID is a duplicate of an 
+                                 * earlier, outstanding, CompletionID.
+                                 */
+};
+
+typedef struct _COMPLETIONIDINFO COMPLETIONIDINFO;
+
 
 struct _SCARD_DEVICE
 {
@@ -37,6 +103,11 @@ struct _SCARD_DEVICE
 	LIST* irp_list;
 
 	freerdp_thread* thread;
+
+        LIST* CompletionIds;
+        freerdp_mutex CompletionIdsMutex;       /* Protect the LIST from
+                                                 * multiple thread writers.
+                                                 */
 };
 typedef struct _SCARD_DEVICE SCARD_DEVICE;
 
