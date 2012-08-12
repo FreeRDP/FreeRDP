5d6ff1536ce080ec13ec3c93b873de9397544f06
Vic Lee <llyzs@163.com>
2012-08-12 14:27:55 +0800

    winpr: fix compilation error on Windows.

--- include/winpr/wtypes.h
+++ include/winpr/wtypes.h
@@ -208,6 +208,9 @@ typedef struct _SECURITY_ATTRIBUTES
 	BOOL bInheritHandle;
 } SECURITY_ATTRIBUTES, *PSECURITY_ATTRIBUTES, *LPSECURITY_ATTRIBUTES;
 
+typedef void* HMODULE;
+typedef void* FARPROC;
+
 #endif
 
 typedef BYTE byte;
@@ -222,8 +225,5 @@ typedef unsigned long error_status_t;
 typedef LONG NTSTATUS;
 typedef NTSTATUS *PNTSTATUS;
 #endif
-
-typedef void* HMODULE;
-typedef void* FARPROC;
 
 #endif /* WINPR_WTYPES_H */
