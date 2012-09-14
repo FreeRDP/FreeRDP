403497ee5a7ad79570954cd8d75c54b2d2f887d7
Bernhard Miklautz <bmiklautz@thinstuff.com>
2012-09-14 12:34:51 +0200

    build: warn that GIT_REVISION is set to n/a for cmake < 2.8.2

--- cmake/BuildFeatureCheck.cmake
+++ cmake/BuildFeatureCheck.cmake
@@ -22,3 +22,9 @@ if (WITH_MONOLITHIC_BUILD)
 		message(FATAL_ERROR "CMAKE version >= 2.8.8 required for WITH_MONOLITHIC_BUILD")
 	endif()
 endif(WITH_MONOLITHIC_BUILD)
+
+# GetGitRevisionDescription requires FindGit which was added in version 2.8.2
+# build won't fail but GIT_REVISION is set to n/a
+if(${CMAKE_VERSION} VERSION_LESS 2.8.2)
+	message(WARNING "GetGitRevisionDescription reqires (FindGit) cmake >= 2.8.2 to work properly - GIT_REVISION will be set to n/a")
+endif()
