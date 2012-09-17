6aabffa2cf5303f4936c8e0ce21b5a7ec00792cb
Marc-André Moreau <marcandre.moreau@gmail.com>
2012-09-17 19:17:19 -0400

    libwinpr-thread: started stubbing threading API

---
+++ winpr/include/winpr/thread.h
@@ -0,0 +1,138 @@
+/**
+ * WinPR: Windows Portable Runtime
+ * Process Thread Functions
+ *
+ * Copyright 2012 Marc-Andre Moreau <marcandre.moreau@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef WINPR_THREAD_H
+#define WINPR_THREAD_H
+
+#include <winpr/winpr.h>
+#include <winpr/wtypes.h>
+
+#include <winpr/handle.h>
+
+#ifndef _WIN32
+
+typedef struct _STARTUPINFOA
+{
+	DWORD cb;
+	LPSTR lpReserved;
+	LPSTR lpDesktop;
+	LPSTR lpTitle;
+	DWORD dwX;
+	DWORD dwY;
+	DWORD dwXSize;
+	DWORD dwYSize;
+	DWORD dwXCountChars;
+	DWORD dwYCountChars;
+	DWORD dwFillAttribute;
+	DWORD dwFlags;
+	WORD wShowWindow;
+	WORD cbReserved2;
+	LPBYTE lpReserved2;
+	HANDLE hStdInput;
+	HANDLE hStdOutput;
+	HANDLE hStdError;
+} STARTUPINFOA, *LPSTARTUPINFOA;
+
+typedef struct _STARTUPINFOW
+{
+	DWORD cb;
+	LPWSTR lpReserved;
+	LPWSTR lpDesktop;
+	LPWSTR lpTitle;
+	DWORD dwX;
+	DWORD dwY;
+	DWORD dwXSize;
+	DWORD dwYSize;
+	DWORD dwXCountChars;
+	DWORD dwYCountChars;
+	DWORD dwFillAttribute;
+	DWORD dwFlags;
+	WORD wShowWindow;
+	WORD cbReserved2;
+	LPBYTE lpReserved2;
+	HANDLE hStdInput;
+	HANDLE hStdOutput;
+	HANDLE hStdError;
+} STARTUPINFOW, *LPSTARTUPINFOW;
+
+#ifdef UNICODE
+typedef STARTUPINFOW	STARTUPINFO;
+typedef LPSTARTUPINFOW	LPSTARTUPINFO;
+#else
+typedef STARTUPINFOA	STARTUPINFO;
+typedef LPSTARTUPINFOA	LPSTARTUPINFO;
+#endif
+
+WINPR_API BOOL CreateProcessA(LPCSTR lpApplicationName, LPSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes,
+		LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment,
+		LPCSTR lpCurrentDirectory, LPSTARTUPINFOA lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation);
+
+WINPR_API BOOL CreateProcessW(LPCWSTR lpApplicationName, LPWSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes,
+		LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment,
+		LPCWSTR lpCurrentDirectory, LPSTARTUPINFOW lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation);
+
+#ifdef UNICODE
+#define CreateProcess	CreateProcessW
+#else
+#define CreateProcess	CreateProcessA
+#endif
+
+WINPR_API BOOL CreateProcessAsUserA(HANDLE hToken, LPCSTR lpApplicationName, LPSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes,
+		LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment,
+		LPCSTR lpCurrentDirectory, LPSTARTUPINFOA lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation);
+
+WINPR_API BOOL CreateProcessAsUserW(HANDLE hToken, LPCWSTR lpApplicationName, LPWSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes,
+		LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment,
+		LPCWSTR lpCurrentDirectory, LPSTARTUPINFOW lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation);
+
+#ifdef UNICODE
+#define CreateProcessAsUser	CreateProcessAsUserW
+#else
+#define CreateProcessAsUser	CreateProcessAsUserA
+#endif
+
+WINPR_API HANDLE CreateThread(LPSECURITY_ATTRIBUTES lpThreadAttributes, SIZE_T dwStackSize,
+	LPTHREAD_START_ROUTINE lpStartAddress, LPVOID lpParameter, DWORD dwCreationFlags, LPDWORD lpThreadId);
+
+WINPR_API VOID ExitProcess(UINT uExitCode);
+WINPR_API VOID ExitThread(DWORD dwExitCode);
+
+WINPR_API HANDLE GetCurrentProcess(VOID);
+WINPR_API DWORD GetCurrentProcessId(VOID);
+WINPR_API DWORD GetCurrentProcessorNumber(VOID);
+
+WINPR_API HANDLE GetCurrentThread(VOID);
+WINPR_API DWORD GetCurrentThreadId(VOID);
+
+WINPR_API DWORD ResumeThread(HANDLE hThread);
+WINPR_API DWORD SuspendThread(HANDLE hThread);
+WINPR_API BOOL SwitchToThread(VOID);
+
+WINPR_API BOOL TerminateProcess(HANDLE hProcess, UINT uExitCode);
+WINPR_API BOOL TerminateThread(HANDLE hThread, DWORD dwExitCode);
+
+WINPR_API DWORD TlsAlloc(VOID);
+WINPR_API LPVOID TlsGetValue(DWORD dwTlsIndex);
+WINPR_API BOOL TlsSetValue(DWORD dwTlsIndex, LPVOID lpTlsValue);
+WINPR_API BOOL TlsFree(DWORD dwTlsIndex);
+
+#endif
+
+#endif /* WINPR_THREAD_H */
+
---
+++ winpr/libwinpr/thread/thread.c
@@ -0,0 +1,219 @@
+/**
+ * WinPR: Windows Portable Runtime
+ * Process Thread Functions
+ *
+ * Copyright 2012 Marc-Andre Moreau <marcandre.moreau@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <winpr/thread.h>
+
+/**
+ * api-ms-win-core-processthreads-l1-1-1.dll
+ * 
+ * CreateProcessA
+ * CreateProcessAsUserW
+ * CreateProcessW
+ * CreateRemoteThread
+ * CreateRemoteThreadEx
+ * CreateThread
+ * DeleteProcThreadAttributeList
+ * ExitProcess
+ * ExitThread
+ * FlushInstructionCache
+ * FlushProcessWriteBuffers
+ * GetCurrentProcess
+ * GetCurrentProcessId
+ * GetCurrentProcessorNumber
+ * GetCurrentProcessorNumberEx
+ * GetCurrentThread
+ * GetCurrentThreadId
+ * GetCurrentThreadStackLimits
+ * GetExitCodeProcess
+ * GetExitCodeThread
+ * GetPriorityClass
+ * GetProcessHandleCount
+ * GetProcessId
+ * GetProcessIdOfThread
+ * GetProcessMitigationPolicy
+ * GetProcessTimes
+ * GetProcessVersion
+ * GetStartupInfoW
+ * GetThreadContext
+ * GetThreadId
+ * GetThreadIdealProcessorEx
+ * GetThreadPriority
+ * GetThreadPriorityBoost
+ * GetThreadTimes
+ * InitializeProcThreadAttributeList
+ * IsProcessorFeaturePresent
+ * OpenProcess
+ * OpenProcessToken
+ * OpenThread
+ * OpenThreadToken
+ * ProcessIdToSessionId
+ * QueryProcessAffinityUpdateMode
+ * QueueUserAPC
+ * ResumeThread
+ * SetPriorityClass
+ * SetProcessAffinityUpdateMode
+ * SetProcessMitigationPolicy
+ * SetProcessShutdownParameters
+ * SetThreadContext
+ * SetThreadIdealProcessorEx
+ * SetThreadPriority
+ * SetThreadPriorityBoost
+ * SetThreadStackGuarantee
+ * SetThreadToken
+ * SuspendThread
+ * SwitchToThread
+ * TerminateProcess
+ * TerminateThread
+ * TlsAlloc
+ * TlsFree
+ * TlsGetValue
+ * TlsSetValue
+ * UpdateProcThreadAttribute
+ */
+
+#ifndef _WIN32
+
+#include <pthread.h>
+
+typedef void *(*pthread_start_routine)(void*);
+
+BOOL CreateProcessA(LPCSTR lpApplicationName, LPSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes,
+		LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment,
+		LPCSTR lpCurrentDirectory, LPSTARTUPINFOA lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation)
+{
+	return TRUE;
+}
+
+BOOL CreateProcessW(LPCWSTR lpApplicationName, LPWSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes,
+		LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment,
+		LPCWSTR lpCurrentDirectory, LPSTARTUPINFOW lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation)
+{
+	return TRUE;
+}
+
+BOOL CreateProcessAsUserA(HANDLE hToken, LPCSTR lpApplicationName, LPSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes,
+		LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment,
+		LPCSTR lpCurrentDirectory, LPSTARTUPINFOA lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation)
+{
+	return TRUE;
+}
+
+BOOL CreateProcessAsUserW(HANDLE hToken, LPCWSTR lpApplicationName, LPWSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes,
+		LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment,
+		LPCWSTR lpCurrentDirectory, LPSTARTUPINFOW lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation)
+{
+	return TRUE;
+}
+
+HANDLE CreateThread(LPSECURITY_ATTRIBUTES lpThreadAttributes, SIZE_T dwStackSize,
+	LPTHREAD_START_ROUTINE lpStartAddress, LPVOID lpParameter, DWORD dwCreationFlags, LPDWORD lpThreadId)
+{
+	pthread_t thread;
+	pthread_create(&thread, 0, (pthread_start_routine) lpStartAddress, lpParameter);
+	pthread_detach(thread);
+
+	return NULL;
+}
+
+VOID ExitProcess(UINT uExitCode)
+{
+
+}
+
+VOID ExitThread(DWORD dwExitCode)
+{
+
+}
+
+HANDLE GetCurrentProcess(VOID)
+{
+	return NULL;
+}
+
+DWORD GetCurrentProcessId(VOID)
+{
+	return 0;
+}
+
+DWORD GetCurrentProcessorNumber(VOID)
+{
+	return 0;
+}
+
+HANDLE GetCurrentThread(VOID)
+{
+	return NULL;
+}
+
+DWORD GetCurrentThreadId(VOID)
+{
+	return 0;
+}
+
+DWORD ResumeThread(HANDLE hThread)
+{
+	return 0;
+}
+
+DWORD SuspendThread(HANDLE hThread)
+{
+	return 0;
+}
+
+BOOL SwitchToThread(VOID)
+{
+	return TRUE;
+}
+
+BOOL TerminateProcess(HANDLE hProcess, UINT uExitCode)
+{
+	return TRUE;
+}
+
+BOOL TerminateThread(HANDLE hThread, DWORD dwExitCode)
+{
+	return TRUE;
+}
+
+DWORD TlsAlloc(VOID)
+{
+	return 0;
+}
+
+LPVOID TlsGetValue(DWORD dwTlsIndex)
+{
+	return NULL;
+}
+
+BOOL TlsSetValue(DWORD dwTlsIndex, LPVOID lpTlsValue)
+{
+	return TRUE;
+}
+
+BOOL TlsFree(DWORD dwTlsIndex)
+{
+	return TRUE;
+}
+
+#endif
+
