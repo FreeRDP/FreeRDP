7d7536f6499896c076e25d863b92f8a51b501b3c
Marc-André Moreau <marcandre.moreau@gmail.com>
2012-09-21 13:02:54 -0400

    libwinpr: add module definition files

---
+++ winpr/libwinpr/winsock/ModuleOptions.cmake
@@ -0,0 +1,9 @@
+
+set(MINWIN_LAYER "0")
+set(MINWIN_GROUP "none")
+set(MINWIN_MAJOR_VERSION "0")
+set(MINWIN_MINOR_VERSION "0")
+set(MINWIN_SHORT_NAME "ws2_32")
+set(MINWIN_LONG_NAME "Windows Sockets (Winsock)")
+set(MODULE_LIBRARY_NAME "${MINWIN_SHORT_NAME}")
+
