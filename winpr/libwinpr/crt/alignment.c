a80eeabc4817ae26e8b10ff14990039df71dfdb0
Marc-André Moreau <marcandre.moreau@gmail.com>
2012-10-01 19:10:00 -0400

    libwinpr-crt: add tests for memory aligned allocation functions

--- winpr/libwinpr/crt/alignment.c
+++ winpr/libwinpr/crt/alignment.c
@@ -27,14 +27,26 @@
 
 #ifndef _WIN32
 
+#include <stdlib.h>
+#include <malloc.h>
+
 void* _aligned_malloc(size_t size, size_t alignment)
 {
-	return NULL;
+	void* memptr;
+
+	if (posix_memalign(&memptr, alignment, size) != 0)
+		return NULL;
+
+	return memptr;
 }
 
 void* _aligned_realloc(void* memblock, size_t size, size_t alignment)
 {
-	return NULL;
+	void* memptr = NULL;
+
+	memptr = realloc(memblock, size);
+
+	return memptr;
 }
 
 void* _aligned_recalloc(void* memblock, size_t num, size_t size, size_t alignment)
@@ -44,7 +56,12 @@ void* _aligned_recalloc(void* memblock, size_t num, size_t size, size_t alignmen
 
 void* _aligned_offset_malloc(size_t size, size_t alignment, size_t offset)
 {
-	return NULL;
+	void* memptr;
+
+	if (posix_memalign(&memptr, alignment, size) != 0)
+		return NULL;
+
+	return memptr;
 }
 
 void* _aligned_offset_realloc(void* memblock, size_t size, size_t alignment, size_t offset)
@@ -64,7 +81,7 @@ size_t _aligned_msize(void* memblock, size_t alignment, size_t offset)
 
 void _aligned_free(void* memblock)
 {
-
+	free(memblock);
 }
 
 #endif
