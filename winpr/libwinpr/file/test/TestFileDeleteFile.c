bf26079d3faaa39fb027b1405bfe3a2cae5355e0
Marc-André Moreau <marcandre.moreau@gmail.com>
2012-10-04 18:57:32 -0400

    libwinpr: added tests and fix InterlockedCompareExchange64 on Windows XP

---
+++ winpr/libwinpr/file/test/TestFileDeleteFile.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#include <winpr/crt.h>
+#include <winpr/file.h>
+#include <winpr/windows.h>
+
+int TestFileDeleteFile(int argc, char* argv[])
+{
+	return 0;
+}
---
+++ winpr/libwinpr/file/test/TestFileWriteFile.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#include <winpr/crt.h>
+#include <winpr/file.h>
+#include <winpr/windows.h>
+
+int TestFileWriteFile(int argc, char* argv[])
+{
+	return 0;
+}
