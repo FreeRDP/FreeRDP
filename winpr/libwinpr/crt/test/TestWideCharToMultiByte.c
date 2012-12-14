b60754cee6c359fe088f4fbced9fd1ec474358c6
Marc-André Moreau <marcandre.moreau@gmail.com>
2012-12-14 14:45:52 -0500

    libwinpr-crt: added test strings for character set conversion

--- winpr/libwinpr/crt/test/TestWideCharToMultiByte.c
+++ winpr/libwinpr/crt/test/TestWideCharToMultiByte.c
@@ -3,6 +3,11 @@
 #include <winpr/crt.h>
 #include <winpr/windows.h>
 
+/*
+ * Conversion *from* Unicode
+ * WideCharToMultiByte: http://msdn.microsoft.com/en-us/library/windows/desktop/dd374130/
+ */
+
 int TestWideCharToMultiByte(int argc, char* argv[])
 {
 	return 0;
