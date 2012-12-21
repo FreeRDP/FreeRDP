502368dd95ab4d6cbeae5e32741312158b3c4fb6
Vic Lee <llyzs@163.com>
2012-12-21 11:32:02 +0800

    libwinpr-synch: proper handling fd event creation on Windows.

--- winpr/include/winpr/synch.h
+++ winpr/include/winpr/synch.h
@@ -258,6 +258,8 @@ WINPR_API HANDLE CreateFileDescriptorEventW(LPSECURITY_ATTRIBUTES lpEventAttribu
 		BOOL bManualReset, BOOL bInitialState, int FileDescriptor);
 WINPR_API HANDLE CreateFileDescriptorEventA(LPSECURITY_ATTRIBUTES lpEventAttributes,
 		BOOL bManualReset, BOOL bInitialState, int FileDescriptor);
+WINPR_API HANDLE CreateWaitObjectEvent(LPSECURITY_ATTRIBUTES lpEventAttributes,
+		BOOL bManualReset, BOOL bInitialState, void* pObject);
 
 #ifdef UNICODE
 #define CreateFileDescriptorEvent	CreateFileDescriptorEventW
