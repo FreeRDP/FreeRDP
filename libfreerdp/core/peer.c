3737eb658ba8d9d6adb6eebfbdfb5c4f55544b75
Corey C <can.of.tuna@gmail.com>
2013-01-15 13:31:11 -0500

    libfreerdp-core: removed left over printf

--- libfreerdp/core/peer.c
+++ libfreerdp/core/peer.c
@@ -257,8 +257,6 @@ static BOOL peer_recv_callback(rdpTransport* transport, STREAM* s, void* extra)
 	freerdp_peer* client = (freerdp_peer*) extra;
 	rdpRdp* rdp = client->context->rdp;
 
-	printf("peer_recv_callback state = %d\n", rdp->state);
-
 	switch (rdp->state)
 	{
 		case CONNECTION_STATE_INITIAL:
