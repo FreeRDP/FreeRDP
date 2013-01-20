09d55bab1e9b3bad6e6d634cd7e114a8c3c28764
Vic Lee <llyzs@163.com>
2013-01-20 10:54:53 +0800

    include: fix missing extern C decls.

--- include/freerdp/client/cmdline.h
+++ include/freerdp/client/cmdline.h
@@ -23,6 +23,10 @@
 #include <freerdp/api.h>
 #include <freerdp/freerdp.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 FREERDP_API int freerdp_client_parse_command_line_arguments(int argc, char** argv, rdpSettings* settings);
 FREERDP_API int freerdp_client_load_addins(rdpChannels* channels, rdpSettings* settings);
 
@@ -35,5 +39,9 @@ FREERDP_API int freerdp_set_connection_type(rdpSettings* settings, int type);
 FREERDP_API int freerdp_client_add_device_channel(rdpSettings* settings, int count, char** params);
 FREERDP_API int freerdp_client_add_static_channel(rdpSettings* settings, int count, char** params);
 FREERDP_API int freerdp_client_add_dynamic_channel(rdpSettings* settings, int count, char** params);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* FREERDP_CLIENT_CMDLINE_H */
