f91b88f7140571922b6d1ffad7c07c18cd7228cf
Bernhard Miklautz <bmiklautz@thinstuff.at>
2013-01-25 17:50:40 +0100

    added missing defined() in #elif
    gcc >= 4.4 requires a condition in #elif.

--- libfreerdp/primitives/prim_colors.c
+++ libfreerdp/primitives/prim_colors.c
@@ -26,7 +26,7 @@
 #include <freerdp/primitives.h>
 #ifdef WITH_SSE2
 #include <emmintrin.h>
-#elif WITH_NEON
+#elif defined(WITH_NEON)
 #include <arm_neon.h>
 #endif /* WITH_SSE2 else WITH_NEON */
 #include "prim_internal.h"
