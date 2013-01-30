c7c9f2f1a693a143a917900722b02d4287cda157
C-o-r-E <can.of.tuna@gmail.com>
2013-01-30 18:54:08 -0500

    mfreerdp-server: fixed mstsc bug.

--- server/Mac/mf_peer.c
+++ server/Mac/mf_peer.c
@@ -147,6 +147,9 @@ void mf_peer_rfx_update(freerdp_peer* client)
     rect.width = width;
     rect.height = height;
     
+    mfp->rfx_context->width = mfi->servscreen_width;
+    mfp->rfx_context->height = mfi->servscreen_height;
+    
     rfx_compose_message(mfp->rfx_context, s, &rect, 1,
                         (BYTE*) dataBits, rect.width, rect.height, pitch);
     
@@ -165,7 +168,7 @@ void mf_peer_rfx_update(freerdp_peer* client)
     
     //send
     
-	update->SurfaceBits(update->context, cmd);
+    update->SurfaceBits(update->context, cmd);
     
     //clean up
     
@@ -269,7 +272,7 @@ BOOL mf_peer_post_connect(freerdp_peer* client)
 	mfPeerContext* context = (mfPeerContext*) client->context;
     rdpSettings* settings = client->settings;
     
-	printf("Client %s is activated\n", client->hostname);
+	printf("Client %s post connect\n", client->hostname);
     
 	if (client->settings->AutoLogonEnabled)
 	{
@@ -301,9 +304,6 @@ BOOL mf_peer_post_connect(freerdp_peer* client)
     client->update->DesktopResize(client->update->context);
 	
     
-	/*printf("Client requested desktop: %dx%dx%d\n",
-     client->settings->DesktopWidth, client->settings->DesktopHeight, client->settings->ColorDepth);
-     */
 //#ifdef WITH_SERVER_CHANNELS
 	/* Iterate all channel names requested by the client and activate those supported by the server */
     int i;
@@ -334,7 +334,7 @@ BOOL mf_peer_post_connect(freerdp_peer* client)
 BOOL mf_peer_activate(freerdp_peer* client)
 {
 	mfPeerContext* context = (mfPeerContext*) client->context;
-    
+
 	rfx_context_reset(context->rfx_context);
 	context->activated = TRUE;
     
@@ -375,10 +375,12 @@ void mf_peer_keyboard_event(rdpInput* input, UINT16 flags, UINT16 code)
         state_down = TRUE;
     }
     
+    /*
     CGEventRef event;
     event = CGEventCreateKeyboardEvent(NULL, (CGKeyCode)code, state_down);
     CGEventPost(kCGHIDEventTap, event);
     CFRelease(event);
+    */
 }
 
 void mf_peer_unicode_keyboard_event(rdpInput* input, UINT16 flags, UINT16 code)
@@ -512,6 +514,7 @@ void* mf_peer_main_loop(void* arg)
 	client->settings->PrivateKeyFile = _strdup("server.key");
 	client->settings->NlaSecurity = FALSE;
 	client->settings->RemoteFxCodec = TRUE;
+    client->settings->ColorDepth = 32;
 	client->settings->SuppressOutput = TRUE;
 	client->settings->RefreshRect = FALSE;
     
