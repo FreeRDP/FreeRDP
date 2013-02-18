5795a3c375fa09b06437285e9a42086b2e4ff543
Marc-André Moreau <marcandre.moreau@gmail.com>
2013-02-18 12:01:30 -0500

    xfreerdp-server: fix image leak

--- server/X11/xf_peer.c
+++ server/X11/xf_peer.c
@@ -395,6 +395,8 @@ void xf_peer_rfx_update(freerdp_peer* client, int x, int y, int width, int heigh
 		cmd->destTop = y;
 		cmd->destRight = x + width;
 		cmd->destBottom = y + height;
+
+		XDestroyImage(image);
 	}
 
 	cmd->bpp = 32;
