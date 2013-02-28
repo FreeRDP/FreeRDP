ea63fdb18e3adfd96313b466183a6bf290e6c2b6
Marc-André Moreau <marcandre.moreau@gmail.com>
2013-02-28 15:43:57 -0500

    libwinpr-utils: start custom printf replacement

--- winpr/libwinpr/utils/test/TestPrint.c
+++ winpr/libwinpr/utils/test/TestPrint.c
@@ -1,359 +1,108 @@
 
-#include <stdio.h>
-#include <string.h>
-#include <stdarg.h>
-
 #include <winpr/crt.h>
 #include <winpr/tchar.h>
 #include <winpr/print.h>
 
-int wprintfx(const char *fmt, ...);
+/**
+ * C Programming/C Reference/stdio.h/printf:
+ * http://en.wikibooks.org/wiki/C_Programming/C_Reference/stdio.h/printf
+ *
+ * C Programming/Procedures and functions/printf:
+ * http://en.wikibooks.org/wiki/C_Programming/Procedures_and_functions/printf
+ *
+ * C Tutorial – printf, Format Specifiers, Format Conversions and Formatted Output:
+ * http://www.codingunit.com/printf-format-specifiers-format-conversions-and-formatted-output
+ */
+
+#if 0
+#define _printf printf
+#else
+#define _printf wprintfx
+#endif
 
 int TestPrint(int argc, char* argv[])
 {
-	wprintfx("this %s a test: %d\n", "is", 65);
+	int a, b;
+	float c, d;
+
+	/**
+	 * 7
+	 *   7
+	 * 007
+	 * 5.10
+	 */
+
+	a = 15;
+	b = a / 2;
+	_printf("%d\n",b);
+	_printf("%3d\n",b);
+	_printf("%03d\n",b);
+	c = 15.3; d = c / 3;
+	_printf("%3.2f\n",d);
+
+	/**
+	 *   0 -17.778
+	 *  20 -6.667
+	 *  40 04.444
+	 *  60 15.556
+	 *  80 26.667
+	 * 100 37.778
+	 * 120 48.889
+	 * 140 60.000
+	 * 160 71.111
+	 * 180 82.222
+	 * 200 93.333
+	 * 220 104.444
+	 * 240 115.556
+	 * 260 126.667
+	 * 280 137.778
+	 * 300 148.889
+	 */
+
+	for (a = 0; a <= 300; a = a + 20)
+		_printf("%3d %06.3f\n", a, (5.0 / 9.0) * (a - 32));
+
+	/**
+	 * The color: blue
+	 * First number: 12345
+	 * Second number: 0025
+	 * Third number: 1234
+	 * Float number: 3.14
+	 * Hexadecimal: ff
+	 * Octal: 377
+	 * Unsigned value: 150
+	 * Just print the percentage sign %
+	 */
+
+	_printf("The color: %s\n", "blue");
+	_printf("First number: %d\n", 12345);
+	_printf("Second number: %04d\n", 25);
+	_printf("Third number: %i\n", 1234);
+	_printf("Float number: %3.2f\n", 3.14159);
+	_printf("Hexadecimal: %x/%X\n", 255, 255);
+	_printf("Octal: %o\n", 255);
+	_printf("Unsigned value: %u\n", 150);
+	_printf("Just print the percentage sign %%\n", 10);
+
+	/**
+	 * :Hello, world!:
+	 * :  Hello, world!:
+	 * :Hello, wor:
+	 * :Hello, world!:
+	 * :Hello, world!  :
+	 * :Hello, world!:
+	 * :     Hello, wor:
+	 * :Hello, wor     :
+	 */
+
+	_printf(":%s:\n", "Hello, world!");
+	_printf(":%15s:\n", "Hello, world!");
+	_printf(":%.10s:\n", "Hello, world!");
+	_printf(":%-10s:\n", "Hello, world!");
+	_printf(":%-15s:\n", "Hello, world!");
+	_printf(":%.15s:\n", "Hello, world!");
+	_printf(":%15.10s:\n", "Hello, world!");
+	_printf(":%-15.10s:\n", "Hello, world!");
 
 	return 0;
-}
-
-/*----------------------------------------------------------------------------
-Stripped-down printf()
-Chris Giese	<geezer@execpc.com>	http://my.execpc.com/~geezer
-Release date: Feb 3, 2008
-This code is public domain (no copyright).
-You can do whatever you want with it.
-
-%[flag][width][.prec][mod][conv]
-flag:	-	left justify, pad right w/ blanks	DONE
-	0	pad left w/ 0 for numerics		DONE
-	+	always print sign, + or -		no
-	' '	(blank)					no
-	#	(???)					no
-
-width:		(field width)				DONE
-
-prec:		(precision)				no
-
-conv:	d,i	decimal int				DONE
-	u	decimal unsigned			DONE
-	o	octal					DONE
-	x,X	hex					DONE
-	f,e,g,E,G float					no
-	c	char					DONE
-	s	string					DONE
-	p	ptr					DONE
-
-mod:	N	near ptr				DONE
-	F	far ptr					no
-	h	short (16-bit) int			DONE
-	l	long (32-bit) int			DONE
-	L	long long (64-bit) int			no
-----------------------------------------------------------------------------*/
-#include <string.h> /* strlen() */
-
-/* flags used in processing format string */
-#define		PR_LJ	0x01	/* left justify */
-#define		PR_CA	0x02	/* use A-F instead of a-f for hex */
-#define		PR_SG	0x04	/* signed numeric conversion (%d vs. %u) */
-#define		PR_32	0x08	/* long (32-bit) numeric conversion */
-#define		PR_16	0x10	/* short (16-bit) numeric conversion */
-#define		PR_WS	0x20	/* PR_SG set and num was < 0 */
-#define		PR_LZ	0x40	/* pad left with '0' instead of ' ' */
-#define		PR_FP	0x80	/* pointers are far */
-
-/* largest number handled is 2^32-1, lowest radix handled is 8.
-2^32-1 in base 8 has 11 digits (add 5 for trailing NUL and for slop) */
-#define		PR_BUFLEN	16
-
-typedef int (*fnptr_t)(unsigned c, void **helper);
-/*****************************************************************************
-name:	do_printf
-action:	minimal subfunction for ?printf, calls function
-	'fn' with arg 'ptr' for each character to be output
-returns:total number of characters output
-*****************************************************************************/
-int do_printf(const char *fmt, va_list args, fnptr_t fn, void *ptr)
-{
-	unsigned flags, actual_wd, count, given_wd;
-	unsigned char *where, buf[PR_BUFLEN];
-	unsigned char state, radix;
-	long num;
-
-	state = flags = count = given_wd = 0;
-/* begin scanning format specifier list */
-	for (; *fmt; fmt++)
-	{
-		switch (state)
-		{
-/* STATE 0: AWAITING % */
-		case 0:
-			if (*fmt != '%')	/* not %... */
-			{
-				fn(*fmt, &ptr);	/* ...just echo it */
-				count++;
-				break;
-			}
-/* found %, get next char and advance state to check if next char is a flag */
-			state++;
-			fmt++;
-			/* FALL THROUGH */
-/* STATE 1: AWAITING FLAGS (%-0) */
-		case 1:
-			if (*fmt == '%')	/* %% */
-			{
-				fn(*fmt, &ptr);
-				count++;
-				state = flags = given_wd = 0;
-				break;
-			}
-			if (*fmt == '-')
-			{
-				if (flags & PR_LJ)/* %-- is illegal */
-					state = flags = given_wd = 0;
-				else
-					flags |= PR_LJ;
-				break;
-			}
-/* not a flag char: advance state to check if it's field width */
-			state++;
-/* check now for '%0...' */
-			if (*fmt == '0')
-			{
-				flags |= PR_LZ;
-				fmt++;
-			}
-			/* FALL THROUGH */
-/* STATE 2: AWAITING (NUMERIC) FIELD WIDTH */
-		case 2:
-			if (*fmt >= '0' && *fmt <= '9')
-			{
-				given_wd = 10 * given_wd + (*fmt - '0');
-				break;
-			}
-/* not field width: advance state to check if it's a modifier */
-			state++;
-			/* FALL THROUGH */
-/* STATE 3: AWAITING MODIFIER CHARS (FNlh) */
-		case 3:
-			if (*fmt == 'F')
-			{
-				flags |= PR_FP;
-				break;
-			}
-			if (*fmt == 'N')
-				break;
-			if (*fmt == 'l')
-			{
-				flags |= PR_32;
-				break;
-			}
-			if (*fmt == 'h')
-			{
-				flags |= PR_16;
-				break;
-			}
-/* not modifier: advance state to check if it's a conversion char */
-			state++;
-			/* FALL THROUGH */
-/* STATE 4: AWAITING CONVERSION CHARS (Xxpndiuocs) */
-		case 4:
-			where = buf + PR_BUFLEN - 1;
-			*where = '\0';
-			switch (*fmt)
-			{
-			case 'X':
-				flags |= PR_CA;
-				/* FALL THROUGH */
-/* xxx - far pointers (%Fp, %Fn) not yet supported */
-			case 'x':
-			case 'p':
-			case 'n':
-				radix = 16;
-				goto DO_NUM;
-			case 'd':
-			case 'i':
-				flags |= PR_SG;
-				/* FALL THROUGH */
-			case 'u':
-				radix = 10;
-				goto DO_NUM;
-			case 'o':
-				radix = 8;
-/* load the value to be printed. l=long=32 bits: */
-DO_NUM:				if (flags & PR_32)
-					num = va_arg(args, unsigned long);
-/* h=short=16 bits (signed or unsigned) */
-				else if (flags & PR_16)
-				{
-					if (flags & PR_SG)
-						num = (short) va_arg(args, int);
-					else
-						num = (unsigned short) va_arg(args, int);
-				}
-/* no h nor l: sizeof(int) bits (signed or unsigned) */
-				else
-				{
-					if (flags & PR_SG)
-						num = va_arg(args, int);
-					else
-						num = va_arg(args, unsigned int);
-				}
-/* take care of sign */
-				if (flags & PR_SG)
-				{
-					if (num < 0)
-					{
-						flags |= PR_WS;
-						num = -num;
-					}
-				}
-/* convert binary to octal/decimal/hex ASCII
-OK, I found my mistake. The math here is _always_ unsigned */
-				do
-				{
-					unsigned long temp;
-
-					temp = (unsigned long)num % radix;
-					where--;
-					if (temp < 10)
-						*where = temp + '0';
-					else if (flags & PR_CA)
-						*where = temp - 10 + 'A';
-					else
-						*where = temp - 10 + 'a';
-					num = (unsigned long)num / radix;
-				}
-				while (num != 0);
-				goto EMIT;
-			case 'c':
-/* disallow pad-left-with-zeroes for %c */
-				flags &= ~PR_LZ;
-				where--;
-				*where = (unsigned char) va_arg(args, int);
-				actual_wd = 1;
-				goto EMIT2;
-			case 's':
-/* disallow pad-left-with-zeroes for %s */
-				flags &= ~PR_LZ;
-				where = va_arg(args, unsigned char*);
-EMIT:
-				actual_wd = strlen((char*) where);
-				if (flags & PR_WS)
-					actual_wd++;
-/* if we pad left with ZEROES, do the sign now */
-				if ((flags & (PR_WS | PR_LZ)) == (PR_WS | PR_LZ))
-				{
-					fn('-', &ptr);
-					count++;
-				}
-/* pad on left with spaces or zeroes (for right justify) */
-EMIT2:				if ((flags & PR_LJ) == 0)
-				{
-					while (given_wd > actual_wd)
-					{
-						fn((flags & PR_LZ) ? '0' : ' ', &ptr);
-						count++;
-						given_wd--;
-					}
-				}
-/* if we pad left with SPACES, do the sign now */
-				if ((flags & (PR_WS | PR_LZ)) == PR_WS)
-				{
-					fn('-', &ptr);
-					count++;
-				}
-/* emit string/char/converted number */
-				while (*where != '\0')
-				{
-					fn(*where++, &ptr);
-					count++;
-				}
-/* pad on right with spaces (for left justify) */
-				if (given_wd < actual_wd)
-					given_wd = 0;
-				else given_wd -= actual_wd;
-				for (; given_wd; given_wd--)
-				{
-					fn(' ', &ptr);
-					count++;
-				}
-				break;
-			default:
-				break;
-			}
-		default:
-			state = flags = given_wd = 0;
-			break;
-		}
-	}
-	return count;
-}
-
-/*****************************************************************************
-SPRINTF
-*****************************************************************************/
-static int vsprintf_help(unsigned c, void **ptr)
-{
-	char *dst;
-
-	dst = *ptr;
-	*dst++ = (char) c;
-	*ptr = dst;
-
-	return 0;
-}
-
-int vsprintf(char *buf, const char *fmt, va_list args)
-{
-	int status;
-
-	status = do_printf(fmt, args, vsprintf_help, (void*) buf);
-	buf[status] = '\0';
-
-	return status;
-}
-
-static int discard(unsigned c_UNUSED, void **ptr_UNUSED)
-{
-	return 0;
-}
-
-int sprintf(char *buf, const char *fmt, ...)
-{
-	va_list args;
-	int status;
-
-	va_start(args, fmt);
-
-	if (!buf)
-		status = do_printf(fmt, args, discard, NULL);
-	else
-		status = vsprintf(buf, fmt, args);
-
-	va_end(args);
-
-	return status;
-}
-
-int vprintf_help(unsigned c, void **ptr_UNUSED)
-{
-	putchar(c);
-	return 0;
-}
-
-int vprintf(const char *fmt, va_list args)
-{
-	return do_printf(fmt, args, vprintf_help, NULL);
-}
-
-int wprintfx(const char *fmt, ...)
-{
-	va_list args;
-	int status;
-
-	va_start(args, fmt);
-	status = vprintf(fmt, args);
-	va_end(args);
-
-	return status;
 }
