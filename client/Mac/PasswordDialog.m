a55cf4b3b16e1278ae768d9b6346659ca471c29b
Christian Hofstaedtler <christian@hofstaedtler.name>
2013-03-11 19:59:50 +0100

    mfreerdp: cleanup PasswordDialog

--- client/Mac/PasswordDialog.m
+++ client/Mac/PasswordDialog.m
@@ -1,10 +1,21 @@
-//
-//  PasswordDialog.m
-//  FreeRDP
-//
-//  Created by Christian Hofstaedtler on 3/10/13.
-//
-//
+/**
+ * FreeRDP: A Remote Desktop Protocol Implementation
+ * MacFreeRDP
+ *
+ * Copyright 2013 Christian Hofstaedtler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import "PasswordDialog.h"
 
@@ -14,43 +25,48 @@
 
 @implementation PasswordDialog
 
-@synthesize userNameText;
+@synthesize usernameText;
 @synthesize passwordText;
 @synthesize messageLabel;
-@synthesize serverName;
-@synthesize userName;
+@synthesize serverHostname;
+@synthesize username;
 @synthesize password;
 
-- (id)init {
-    return [self initWithWindowNibName:@"PasswordDialog"];
+- (id)init
+{
+	return [self initWithWindowNibName:@"PasswordDialog"];
 }
 
 - (void)windowDidLoad
 {
-    [super windowDidLoad];
-    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
-    [self.window setTitle:self.serverName];
-    [messageLabel setStringValue:[NSString stringWithFormat:@"Authenticate to %@", self.serverName]];
-    if (self.userName != nil) {
-        [userNameText setStringValue:self.userName];
-        [self.window makeFirstResponder:passwordText];
-    }
+	[super windowDidLoad];
+	// Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+	[self.window setTitle:self.serverHostname];
+	[messageLabel setStringValue:[NSString stringWithFormat:@"Authenticate to %@", self.serverHostname]];
+	if (self.username != nil)
+	{
+		[usernameText setStringValue:self.username];
+		[self.window makeFirstResponder:passwordText];
+	}
 }
 
-- (IBAction)onOK:(NSObject *)sender {
-    self.userName = self.userNameText.stringValue;
-    self.password = self.passwordText.stringValue;
-    [self.window orderOut:nil];
-    [NSApp stopModalWithCode:TRUE];
+- (IBAction)onOK:(NSObject *)sender
+{
+	self.username = self.usernameText.stringValue;
+	self.password = self.passwordText.stringValue;
+	[self.window orderOut:nil];
+	[NSApp stopModalWithCode:TRUE];
 }
 
-- (IBAction)onCancel:(NSObject *)sender {
-    [self.window orderOut:nil];
-    [NSApp stopModalWithCode:FALSE];
+- (IBAction)onCancel:(NSObject *)sender
+{
+	[self.window orderOut:nil];
+	[NSApp stopModalWithCode:FALSE];
 }
 
-- (BOOL) runModal {
-    return [NSApp runModalForWindow:self.window];
+- (BOOL)runModal
+{
+	return [NSApp runModalForWindow:self.window];
 }
 
 @end
