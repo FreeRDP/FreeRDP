f990fbe303cf43370a48f8cff999656754ab2572
Corey C <can.of.tuna@gmail.com>
2013-03-12 17:03:13 -0400

    wfreerdp-server: cleanup output

--- server/Windows/wf_mirage.c
+++ server/Windows/wf_mirage.c
@@ -3,6 +3,7 @@
  * FreeRDP Windows Server
  *
  * Copyright 2012 Marc-Andre Moreau <marcandre.moreau@gmail.com>
+ * Copyright 2012-2013 Corey Clayton <can.of.tuna@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
