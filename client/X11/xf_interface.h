5660b94b924602a8810e51b7d04df9c687750dd1
Marc-André Moreau <marcandre.moreau@gmail.com>
2013-04-02 13:22:59 -0400

    xfreerdp: start client interface

---
+++ client/X11/xf_interface.h
@@ -0,0 +1,41 @@
+/**
+ * FreeRDP: A Remote Desktop Protocol Implementation
+ * X11 Client Interface
+ *
+ * Copyright 2013 Marc-Andre Moreau <marcandre.moreau@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __XF_INTERFACE_H
+#define __XF_INTERFACE_H
+
+#include <freerdp/freerdp.h>
+
+void xf_context_new(freerdp* instance, rdpContext* context);
+void xf_context_free(freerdp* instance, rdpContext* context);
+
+BOOL xf_pre_connect(freerdp* instance);
+BOOL xf_post_connect(freerdp* instance);
+
+BOOL xf_authenticate(freerdp* instance, char** username, char** password, char** domain);
+BOOL xf_verify_certificate(freerdp* instance, char* subject, char* issuer, char* fingerprint);
+int xf_logon_error_info(freerdp* instance, UINT32 data, UINT32 type);
+
+int xf_receive_channel_data(freerdp* instance, int channelId, BYTE* data, int size, int flags, int total_size);
+
+BYTE xf_exit_code_from_disconnect_reason(UINT32 reason);
+
+void* xf_thread_func(void* param);
+
+#endif /* __XF_INTERFACE_H */
