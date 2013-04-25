6bad744ab6784faf7cf7d8da33e0b839d0261ee5
C-o-r-E <can.of.tuna@gmail.com>
2013-04-25 18:57:41 -0400

    xfreerdp-client: refactor xf_input

--- client/X11/xf_interface.c
+++ client/X11/xf_interface.c
@@ -78,6 +78,7 @@
 #include "xf_monitor.h"
 #include "xf_graphics.h"
 #include "xf_keyboard.h"
+#include "xf_input.h"
 
 static int run = 0;
 
@@ -151,98 +152,6 @@ int up_scale_image(
     }
     /*	done	*/
     return 1;
-}
-
-void xf_input_init(rdpContext* context)
-{
-	xfInfo*			xfi;
-	xfi = ((xfContext*) context)->xfi;
-
-	int opcode, event, error;
-	int major = 2, minor = 2;
-
-	XIDeviceInfo *info;
-	int ndevices, i, j;
-
-	XIEventMask eventmask;
-	unsigned char mask[(XI_LASTEVENT + 7)/8];
-
-	ZeroMemory(mask, sizeof(mask));
-
-
-	if (XQueryExtension(xfi->display, "XInputExtension", &opcode, &event, &error))
-	{
-		printf("X Input extension available.\n");
-
-		xfi->XInputOpcode = opcode;
-		printf("Input Opcode = %d\n", opcode);
-
-		XIQueryVersion(xfi->display, &major, &minor);
-		if (!(major * 1000 + minor < 2002))
-		{
-			printf("XI 2.2 supported\n");
-
-
-
-			info = XIQueryDevice(xfi->display, XIAllDevices, &ndevices);
-
-			for (i = 0; i < ndevices; i++)
-			{
-				XIDeviceInfo *dev = &info[i];
-				printf("Device name [id] %s [%d]\n", dev->name, dev->deviceid);
-				for (j = 0; j < dev->num_classes; j++)
-				{
-					XIAnyClassInfo *class = dev->classes[j];
-					XITouchClassInfo *t = (XITouchClassInfo*)class;
-
-					if (class->type != XITouchClass)
-						continue;
-
-					printf("%s touch device, supporting %d touches.\n",
-							(t->mode == XIDirectTouch) ?  "direct" : "dependent",
-									t->num_touches);
-				}
-			}
-
-			////////////////////
-
-			eventmask.deviceid = XIAllDevices; //13;
-			eventmask.mask_len = sizeof(mask); /* always in bytes */
-			eventmask.mask = mask;
-			/* now set the mask */
-			XISetMask(mask, XI_TouchBegin);
-			XISetMask(mask, XI_TouchUpdate);
-			XISetMask(mask, XI_TouchEnd);
-
-			error = XISelectEvents(xfi->display, xfi->window->handle, &eventmask, 1);
-
-			switch (error)
-			{
-			case BadValue:
-				printf("\tBadValue\n");
-				break;
-			case BadWindow:
-				printf("\tBadWindow\n");
-				break;
-
-			default:
-				printf("XISelectEvents() returned %d\n", error);
-				break;
-
-			}
-
-			printf("Should now be able to get touch events\n");
-
-		}
-		else
-		{
-			printf("Server does not support XI 2.2\n");
-		}
-	}
-	else
-	{
-		printf("X Input extension not available!!!\n");
-	}
 }
 
 /*
