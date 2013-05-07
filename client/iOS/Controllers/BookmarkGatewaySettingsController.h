544f5997bc27e9bb381c0b02ccaa7231e72f4f15
Martin Fleisz <mfleisz@thinstuff.at>
2013-05-07 12:05:21 +0200

    iOS: Got rid of device UID

---
+++ client/iOS/Controllers/BookmarkGatewaySettingsController.h
@@ -0,0 +1,13 @@
+//
+//  BookmarkGatewaySettingsController.h
+//  FreeRDP
+//
+//  Created by Thinstuff Developer on 4/30/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BookmarkGatewaySettingsController : UITableViewController
+
+@end
