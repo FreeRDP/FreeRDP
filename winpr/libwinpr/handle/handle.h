23e8af45f0a28330614e7bd0cb44c0ec5543adc4
Marc-André Moreau <marcandre.moreau@gmail.com>
2013-05-16 18:14:37 -0400

    libwinpr-handle: don't use a centralized handle table as it was a major performance bottleneck

--- winpr/libwinpr/handle/handle.h
+++ winpr/libwinpr/handle/handle.h
@@ -20,8 +20,6 @@
 #ifndef WINPR_HANDLE_PRIVATE_H
 #define WINPR_HANDLE_PRIVATE_H
 
-#include <pthread.h>
-
 #include <winpr/handle.h>
 
 #define HANDLE_TYPE_NONE			0
@@ -33,21 +31,8 @@
 #define HANDLE_TYPE_NAMED_PIPE			6
 #define HANDLE_TYPE_ANONYMOUS_PIPE		7
 
-typedef struct _HANDLE_TABLE_ENTRY
-{
-	ULONG Type;
-	PVOID Object;
-} HANDLE_TABLE_ENTRY, *PHANDLE_TABLE_ENTRY;
-
-typedef struct _HANDLE_TABLE
-{
-	LONG Count;
-	LONG MaxCount;
-	PHANDLE_TABLE_ENTRY Entries;
-} HANDLE_TABLE, *PHANDLE_TABLE;
-
 #define WINPR_HANDLE_DEF() \
-	int winpr_Index;
+	ULONG Type;
 
 struct winpr_handle
 {
@@ -55,18 +40,9 @@ struct winpr_handle
 };
 typedef struct winpr_handle WINPR_HANDLE;
 
-#define HandleTable_GetInstance() \
-	if (g_WinPR_HandleTable.MaxCount < 1) \
-		winpr_HandleTable_New()
-
-extern HANDLE_TABLE g_WinPR_HandleTable;
-extern pthread_mutex_t g_WinPR_HandleTable_Mutex;
-
 WINPR_API HANDLE winpr_Handle_Insert(ULONG Type, PVOID Object);
 WINPR_API BOOL winpr_Handle_Remove(HANDLE handle);
 
-WINPR_API ULONG winpr_Handle_GetType(HANDLE handle);
-WINPR_API PVOID winpr_Handle_GetObject(HANDLE handle);
 WINPR_API BOOL winpr_Handle_GetInfo(HANDLE handle, ULONG* pType, PVOID* pObject);
 
 #endif /* WINPR_HANDLE_PRIVATE_H */
--- winpr/libwinpr/handle/table.c
+++ winpr/libwinpr/handle/table.c
@@ -26,178 +26,34 @@
 
 #ifndef _WIN32
 
-#include <pthread.h>
-
 #include "../handle/handle.h"
-
-HANDLE_TABLE g_WinPR_HandleTable = { 0, 0, NULL };
-pthread_mutex_t g_WinPR_HandleTable_Mutex = PTHREAD_MUTEX_INITIALIZER;
-
-void winpr_HandleTable_New()
-{
-	size_t size;
-
-	pthread_mutex_lock(&g_WinPR_HandleTable_Mutex);
-
-	if (g_WinPR_HandleTable.MaxCount < 1)
-	{
-		g_WinPR_HandleTable.Count = 0;
-		g_WinPR_HandleTable.MaxCount = 64;
-
-		size = sizeof(HANDLE_TABLE_ENTRY) * g_WinPR_HandleTable.MaxCount;
-
-		g_WinPR_HandleTable.Entries = (PHANDLE_TABLE_ENTRY) malloc(size);
-		ZeroMemory(g_WinPR_HandleTable.Entries, size);
-	}
-
-	pthread_mutex_unlock(&g_WinPR_HandleTable_Mutex);
-}
-
-void winpr_HandleTable_Grow()
-{
-	size_t size;
-
-	pthread_mutex_lock(&g_WinPR_HandleTable_Mutex);
-
-	g_WinPR_HandleTable.MaxCount *= 2;
-
-	size = sizeof(HANDLE_TABLE_ENTRY) * g_WinPR_HandleTable.MaxCount;
-
-	g_WinPR_HandleTable.Entries = (PHANDLE_TABLE_ENTRY) realloc(g_WinPR_HandleTable.Entries, size);
-	ZeroMemory((void*) &g_WinPR_HandleTable.Entries[g_WinPR_HandleTable.MaxCount / 2], size / 2);
-
-	pthread_mutex_unlock(&g_WinPR_HandleTable_Mutex);
-}
-
-void winpr_HandleTable_Free()
-{
-	pthread_mutex_lock(&g_WinPR_HandleTable_Mutex);
-
-	g_WinPR_HandleTable.Count = 0;
-	g_WinPR_HandleTable.MaxCount = 0;
-
-	free(g_WinPR_HandleTable.Entries);
-	g_WinPR_HandleTable.Entries = NULL;
-
-	pthread_mutex_unlock(&g_WinPR_HandleTable_Mutex);
-}
 
 HANDLE winpr_Handle_Insert(ULONG Type, PVOID Object)
 {
-	int index;
+	WINPR_HANDLE* wHandle;
 
-	HandleTable_GetInstance();
+	wHandle = (WINPR_HANDLE*) Object;
 
-	pthread_mutex_lock(&g_WinPR_HandleTable_Mutex);
+	wHandle->Type = Type;
 
-	for (index = 0; index < (int) g_WinPR_HandleTable.MaxCount; index++)
-	{
-		if (g_WinPR_HandleTable.Entries[index].Object == NULL)
-		{
-			g_WinPR_HandleTable.Count++;
-
-			g_WinPR_HandleTable.Entries[index].Type = Type;
-			g_WinPR_HandleTable.Entries[index].Object = Object;
-
-			((WINPR_HANDLE*) Object)->winpr_Index = index;
-
-			pthread_mutex_unlock(&g_WinPR_HandleTable_Mutex);
-
-			return Object;
-		}
-	}
-
-	pthread_mutex_unlock(&g_WinPR_HandleTable_Mutex);
-
-	/* no available entry was found, the table needs to be grown */
-
-	winpr_HandleTable_Grow();
-
-	/* the next call should now succeed */
-
-	return winpr_Handle_Insert(Type, Object);
+	return (HANDLE) wHandle;
 }
 
 BOOL winpr_Handle_Remove(HANDLE handle)
 {
-	int index;
-
-	HandleTable_GetInstance();
-
-	pthread_mutex_lock(&g_WinPR_HandleTable_Mutex);
-
-	for (index = 0; index < (int) g_WinPR_HandleTable.MaxCount; index++)
-	{
-		if (g_WinPR_HandleTable.Entries[index].Object == handle)
-		{
-			g_WinPR_HandleTable.Entries[index].Type = HANDLE_TYPE_NONE;
-			g_WinPR_HandleTable.Entries[index].Object = NULL;
-			g_WinPR_HandleTable.Count--;
-
-			pthread_mutex_unlock(&g_WinPR_HandleTable_Mutex);
-
-			return TRUE;
-		}
-	}
-
-	pthread_mutex_unlock(&g_WinPR_HandleTable_Mutex);
-
-	return FALSE;
-}
-
-ULONG winpr_Handle_GetType(HANDLE handle)
-{
-	int index;
-
-	HandleTable_GetInstance();
-
-	pthread_mutex_lock(&g_WinPR_HandleTable_Mutex);
-
-	for (index = 0; index < (int) g_WinPR_HandleTable.MaxCount; index++)
-	{
-		if (g_WinPR_HandleTable.Entries[index].Object == handle)
-		{
-			pthread_mutex_unlock(&g_WinPR_HandleTable_Mutex);
-			return g_WinPR_HandleTable.Entries[index].Type;
-		}
-	}
-
-	pthread_mutex_unlock(&g_WinPR_HandleTable_Mutex);
-
-	return HANDLE_TYPE_NONE;
-}
-
-PVOID winpr_Handle_GetObject(HANDLE handle)
-{
-	HandleTable_GetInstance();
-
-	return handle;
+	return TRUE;
 }
 
 BOOL winpr_Handle_GetInfo(HANDLE handle, ULONG* pType, PVOID* pObject)
 {
-	int index;
-	BOOL status = TRUE;
+	WINPR_HANDLE* wHandle;
 
-	HandleTable_GetInstance();
+	wHandle = (WINPR_HANDLE*) handle;
 
-	pthread_mutex_lock(&g_WinPR_HandleTable_Mutex);
+	*pType = wHandle->Type;
+	*pObject = handle;
 
-	index = ((WINPR_HANDLE*) handle)->winpr_Index;
-
-	if ((index >= 0) && (index < g_WinPR_HandleTable.MaxCount))
-	{
-		*pType = g_WinPR_HandleTable.Entries[index].Type;
-		*pObject = g_WinPR_HandleTable.Entries[index].Object;
-	}
-	else
-	{
-		status = FALSE;
-	}
-
-	pthread_mutex_unlock(&g_WinPR_HandleTable_Mutex);
-
-	return status;
+	return TRUE;
 }
 
 #endif
