fb9606ea77bcf9dcbfe6afab4407d8687a92cd26
Vic Lee <llyzs@163.com>
2013-05-23 13:34:34 -0700

    core/nla: fix non-standard C gramma.

--- libfreerdp/core/nla.c
+++ libfreerdp/core/nla.c
@@ -910,6 +910,7 @@ int credssp_write_ts_credentials(rdpCredssp* credssp, wStream* s)
 {
 	int size = 0;
 	int innerSize = credssp_sizeof_ts_credentials(credssp);
+	int passwordSize;
 
 	/* TSCredentials (SEQUENCE) */
 	size += ber_write_sequence_tag(s, innerSize);
@@ -920,7 +921,7 @@ int credssp_write_ts_credentials(rdpCredssp* credssp, wStream* s)
 
 	/* [1] credentials (OCTET STRING) */
 
-	int passwordSize = ber_sizeof_sequence(credssp_sizeof_ts_password_creds(credssp));
+	passwordSize = ber_sizeof_sequence(credssp_sizeof_ts_password_creds(credssp));
 
 	size += ber_write_contextual_tag(s, 1, ber_sizeof_octet_string(passwordSize), TRUE);
 	size += ber_write_octet_string_tag(s, passwordSize);
