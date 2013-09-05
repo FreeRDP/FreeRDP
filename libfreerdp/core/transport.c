9a3b4ac6c22e5f56cd6bb025f0603210b0a5a2e6
Armin Novak <anovak@thinstuff.com>
2013-09-05 17:17:54 +0200

    Fixed invalid event number.

--- libfreerdp/core/transport.c
+++ libfreerdp/core/transport.c
@@ -817,11 +817,11 @@ static void* transport_client_thread(void* arg)
 		events[nCount++] = transport->stopEvent;
 		events[nCount] = transport->connectedEvent;
 
-		status = WaitForMultipleObjects(nCount, events, FALSE, INFINITE);
+		status = WaitForMultipleObjects(nCount + 1, events, FALSE, INFINITE);
 		if (status == WAIT_OBJECT_0)
 			break;
 
-		transport_get_read_handles(transport, (HANDLE*) events, &nCount);
+		transport_get_read_handles(transport, (HANDLE*) &events, &nCount);
 		status = WaitForMultipleObjects(nCount, events, FALSE, INFINITE);
 		if (status == WAIT_OBJECT_0)
 			break;
