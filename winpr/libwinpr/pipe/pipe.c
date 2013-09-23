979ed1b75615859a09d77aee70e2f4f8ea4e9bc2
Marc-André Moreau <marcandre.moreau@gmail.com>
2013-09-23 18:10:21 -0400

    libwinpr-pipe: implement WaitNamedPipe

--- winpr/libwinpr/pipe/pipe.c
+++ winpr/libwinpr/pipe/pipe.c
@@ -225,7 +225,36 @@ BOOL TransactNamedPipe(HANDLE hNamedPipe, LPVOID lpInBuffer, DWORD nInBufferSize
 
 BOOL WaitNamedPipeA(LPCSTR lpNamedPipeName, DWORD nTimeOut)
 {
-	return TRUE;
+	BOOL status;
+	DWORD nWaitTime;
+	char* lpFilePath;
+	DWORD dwSleepInterval;
+
+	if (!lpNamedPipeName)
+		return FALSE;
+
+	lpFilePath = GetNamedPipeUnixDomainSocketFilePathA(lpNamedPipeName);
+
+	if (nTimeOut == NMPWAIT_USE_DEFAULT_WAIT)
+		nTimeOut = 50;
+
+	nWaitTime = 0;
+	status = TRUE;
+	dwSleepInterval = 10;
+
+	while (!PathFileExistsA(lpFilePath))
+	{
+		Sleep(dwSleepInterval);
+		nWaitTime += dwSleepInterval;
+
+		if (nWaitTime >= nTimeOut)
+		{
+			status = FALSE;
+			break;
+		}
+	}
+
+	return status;
 }
 
 BOOL WaitNamedPipeW(LPCWSTR lpNamedPipeName, DWORD nTimeOut)
