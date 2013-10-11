7dd95b9587622e414d17be3dcda7f77d855f371b
Marc-André Moreau <marcandre.moreau@gmail.com>
2013-10-11 04:01:07 -0400

    libwinpr-crypto: improve Schannel unit test

--- winpr/libwinpr/crypto/crypto.c
+++ winpr/libwinpr/crypto/crypto.c
@@ -2,7 +2,7 @@
  * WinPR: Windows Portable Runtime
  * Cryptography API (CryptoAPI)
  *
- * Copyright 2012 Marc-Andre Moreau <marcandre.moreau@gmail.com>
+ * Copyright 2012-2013 Marc-Andre Moreau <marcandre.moreau@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -144,6 +144,6 @@
 
 #ifndef _WIN32
 
-
+#include "crypto.h"
 
 #endif
