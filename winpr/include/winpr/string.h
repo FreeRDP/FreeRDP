dfd39b6d0a8c16c658f5ccfbc6055b08126104b1
Benoît LeBlanc <benoit.leblanc@awakecoding.com>
2013-11-07 13:55:06 -0500

    Fixed macro declaration

--- winpr/include/winpr/string.h
+++ winpr/include/winpr/string.h
@@ -153,7 +153,7 @@ WINPR_API int lstrcmpW(LPCWSTR lpString1, LPCWSTR lpString2);
 #endif
 
 #define	 sprintf_s	snprintf
-#define _scprintf(_fmt, …) snprintf(NULL, 0, _fmt, ## __VAR_ARGS__)
+#define _scprintf(_fmt, ...) snprintf(NULL, 0, _fmt, ## __VA_ARGS__)
 
 /* Unicode Conversion */
 
