7f49c7302ddc81e436bc6d96af026de13f86bf9d
Marc-André Moreau <marcandre.moreau@gmail.com>
2014-02-05 16:25:55 -0500

    libwinpr-winsock: stub more of winsock

--- winpr/libwinpr/winsock/winsock.c
+++ winpr/libwinpr/winsock/winsock.c
@@ -244,6 +244,174 @@ PCSTR inet_ntop(INT Family, PVOID pAddr, PSTR pStringBuf, size_t StringBufSize)
 
 #else /* _WIN32 */
 
+int WSAStartup(WORD wVersionRequired, LPWSADATA lpWSAData)
+{
+	return 0;
+}
 
+int WSACleanup(void)
+{
+	return 0;
+}
+
+void WSASetLastError(int iError)
+{
+
+}
+
+int WSAGetLastError(void)
+{
+	return 0;
+}
+
+SOCKET _accept(SOCKET s, struct sockaddr* addr, int* addrlen)
+{
+	return 0;
+}
+
+int _bind(SOCKET s, const struct sockaddr* addr, int namelen)
+{
+	return 0;
+}
+
+int closesocket(SOCKET s)
+{
+	return 0;
+}
+
+int _connect(SOCKET s, const struct sockaddr* name, int namelen)
+{
+	return 0;
+}
+
+int _ioctlsocket(SOCKET s, long cmd, u_long* argp)
+{
+	return 0;
+}
+
+int _getpeername(SOCKET s, struct sockaddr* name, int* namelen)
+{
+	return 0;
+}
+
+int _getsockname(SOCKET s, struct sockaddr* name, int* namelen)
+{
+	return 0;
+}
+
+int _getsockopt(SOCKET s, int level, int optname, char* optval, int* optlen)
+{
+	return 0;
+}
+
+u_long _htonl(u_long hostlong)
+{
+	return 0;
+}
+
+u_short _htons(u_short hostshort)
+{
+	return 0;
+}
+
+unsigned long _inet_addr(const char* cp)
+{
+	return 0;
+}
+
+char* _inet_ntoa(struct in_addr in)
+{
+	return 0;
+}
+
+int _listen(SOCKET s, int backlog)
+{
+	return 0;
+}
+
+u_long _ntohl(u_long netlong)
+{
+	return 0;
+}
+
+u_short _ntohs(u_short netshort)
+{
+	return 0;
+}
+
+int _recv(SOCKET s, char* buf, int len, int flags)
+{
+	return 0;
+}
+
+int _recvfrom(SOCKET s, char* buf, int len, int flags, struct sockaddr* from, int* fromlen)
+{
+	return 0;
+}
+
+int _select(int nfds, fd_set* readfds, fd_set* writefds, fd_set* exceptfds, const struct timeval* timeout)
+{
+	return 0;
+}
+
+int _send(SOCKET s, const char* buf, int len, int flags)
+{
+	return 0;
+}
+
+int _sendto(SOCKET s, const char* buf, int len, int flags, const struct sockaddr* to, int tolen)
+{
+	return 0;
+}
+
+int _setsockopt(SOCKET s, int level, int optname, const char* optval, int optlen)
+{
+	return 0;
+}
+
+int _shutdown(SOCKET s, int how)
+{
+	return 0;
+}
+
+SOCKET _socket(int af, int type, int protocol)
+{
+	return 0;
+}
+
+struct hostent* _gethostbyaddr(const char* addr, int len, int type)
+{
+	return 0;
+}
+
+struct hostent* _gethostbyname(const char* name)
+{
+	return 0;
+}
+
+int _gethostname(char* name, int namelen)
+{
+	return 0;
+}
+
+struct servent* _getservbyport(int port, const char* proto)
+{
+	return 0;
+}
+
+struct servent* _getservbyname(const char* name, const char* proto)
+{
+	return 0;
+}
+
+struct protoent* _getprotobynumber(int number)
+{
+	return 0;
+}
+
+struct protoent* _getprotobyname(const char* name)
+{
+	return 0;
+}
 
 #endif  /* _WIN32 */
