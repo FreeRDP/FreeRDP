e6efe7c6784f71de43344bd43413a8f3e3cbed60
vworkspace <Mike.McDonald@software.dell.com>
2014-02-07 13:16:41 -0500

    Added creation of rdpei events and added processing of received 'suspend/resume touches' event from the server in the rdpei plugin.

--- libfreerdp/utils/event.c
+++ libfreerdp/utils/event.c
@@ -73,6 +73,30 @@ static wMessage* freerdp_cliprdr_event_new(UINT16 event_type)
 			ZeroMemory(event.v, sizeof(RDP_CB_CLIP_CAPS));
 			event.m->id = MakeMessageId(CliprdrChannel, ClipCaps);
 			break;
+
+		case CliprdrChannel_FileContentsRequest:
+			event.v = malloc(sizeof(RDP_CB_FILE_CONTENTS_REQUEST_EVENT));
+			ZeroMemory(event.v, sizeof(RDP_CB_FILE_CONTENTS_REQUEST_EVENT));
+			event.m->id = MakeMessageId(CliprdrChannel, FileContentsRequest);
+			break;
+
+		case CliprdrChannel_FileContentsResponse:
+			event.v = malloc(sizeof(RDP_CB_FILE_CONTENTS_RESPONSE_EVENT));
+			ZeroMemory(event.v, sizeof(RDP_CB_FILE_CONTENTS_RESPONSE_EVENT));
+			event.m->id = MakeMessageId(CliprdrChannel, FileContentsResponse);
+			break;
+
+		case CliprdrChannel_Capabilities:
+			event.v = malloc(sizeof(RDP_CB_CAPS_EVENT));
+			ZeroMemory(event.v, sizeof(RDP_CB_CAPS_EVENT));
+			event.m->id = MakeMessageId(CliprdrChannel, Capabilities);
+			break;
+
+		case CliprdrChannel_TemporaryDirectory:
+			event.v = malloc(sizeof(RDP_CB_TEMPORARY_DIRECTORY_EVENT));
+			ZeroMemory(event.v, sizeof(RDP_CB_TEMPORARY_DIRECTORY_EVENT));
+			event.m->id = MakeMessageId(CliprdrChannel, TemporaryDirectory);
+			break;
 	}
 
 	return event.v;
@@ -115,6 +139,16 @@ static wMessage* freerdp_rail_event_new(UINT16 event_type)
 	return event;
 }
 
+static wMessage* freerdp_rdpei_event_new(UINT16 event_type)
+{
+	wMessage* event;
+
+	event = (wMessage*) malloc(sizeof(wMessage));
+	ZeroMemory(event, sizeof(wMessage));
+
+	return event;
+}
+
 wMessage* freerdp_event_new(UINT16 event_class, UINT16 event_type,
 	MESSAGE_FREE_FN on_event_free_callback, void* user_data)
 {
@@ -137,6 +171,10 @@ wMessage* freerdp_event_new(UINT16 event_class, UINT16 event_type,
 
 		case RailChannel_Class:
 			event = freerdp_rail_event_new(event_type);
+			break;
+
+		case RdpeiChannel_Class:
+			event = freerdp_rdpei_event_new(event_type);
 			break;
 	}
 
@@ -190,6 +228,11 @@ static void freerdp_rail_event_free(wMessage* event)
 
 }
 
+static void freerdp_rdpei_event_free(wMessage* event)
+{
+
+}
+
 void freerdp_event_free(wMessage* event)
 {
 	if (event)
@@ -209,6 +252,10 @@ void freerdp_event_free(wMessage* event)
 
 			case RailChannel_Class:
 				freerdp_rail_event_free(event);
+				break;
+
+			case RdpeiChannel_Class:
+				freerdp_rdpei_event_free(event);
 				break;
 		}
 
