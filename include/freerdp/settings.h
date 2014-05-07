b65e8ac195fa9b116dd43b4ab6d4b265862389c1
itsmikeeng <michael_eng07@hotmail.com>
2014-05-07 10:50:14 -0700

    Fixed padding

--- include/freerdp/settings.h
+++ include/freerdp/settings.h
@@ -960,7 +960,7 @@ struct rdp_settings
 	ALIGN64 UINT32 PreconnectionId; /* 1154 */
 	ALIGN64 char* PreconnectionBlob; /* 1155 */
 	ALIGN64 BOOL SendPreconnectionPdu; /* 1156 */
-	UINT64 padding1216[1216 - 1057]; /* 1157 */
+	UINT64 padding1216[1216 - 1157]; /* 1157 */
 
 	/* Server Redirection */
 	ALIGN64 UINT32 RedirectionFlags; /* 1216 */
@@ -1029,7 +1029,7 @@ struct rdp_settings
 	ALIGN64 int XPan; /* 1552 */
 	ALIGN64 int YPan; /* 1553 */
 	ALIGN64 double ScalingFactor; /* 1554 */
-	UINT64 padding1600[1600 - 1555]; /* 1555 */
+	UINT64 padding1601[1601 - 1555]; /* 1555 */
 
 	/* Miscellaneous */
 	ALIGN64 BOOL SoftwareGdi; /* 1601 */
@@ -1040,7 +1040,7 @@ struct rdp_settings
 
 	/* Names */
 	ALIGN64 char* ComputerName; /* 1664 */
-	UINT64 padding1728[1728 - 1605]; /* 1665 */
+	UINT64 padding1728[1728 - 1665]; /* 1665 */
 
 	/* Files */
 	ALIGN64 char* ConnectionFile; /* 1728 */
@@ -1097,7 +1097,7 @@ struct rdp_settings
 	ALIGN64 UINT32 RemoteAppNumIconCacheEntries; /* 2123 */
 	ALIGN64 BOOL RemoteAppLanguageBarSupported; /* 2124 */
 	UINT64 padding2176[2176 - 2125]; /* 2125 */
-	UINT64 padding2240[2240 - 2124]; /* 2176 */
+	UINT64 padding2240[2240 - 2176]; /* 2176 */
 
 	/**
 	 * Mandatory Capabilities
@@ -1131,7 +1131,7 @@ struct rdp_settings
 	ALIGN64 BYTE* OrderSupport; /* 2432 */
 	ALIGN64 BOOL BitmapCacheV3Enabled; /* 2433 */
 	ALIGN64 BOOL AltSecFrameMarkerSupport; /* 2434 */
-	UINT64 padding2496[2496 - 2435]; /* 2435 */
+	UINT64 padding2497[2497 - 2435]; /* 2435 */
 
 	/* Bitmap Cache Capabilities */
 	ALIGN64 BOOL BitmapCacheEnabled; /* 2497 */
@@ -1199,7 +1199,7 @@ struct rdp_settings
 	UINT64 padding3264[3264 - 3200]; /* 3200 */
 
 	/* Font Capabilities */
-	UINT64 padding3328[3328 - 3200]; /* 3264 */
+	UINT64 padding3328[3328 - 3264]; /* 3264 */
 
 	/* Multifragment Update Capabilities */
 	ALIGN64 UINT32 MultifragMaxRequestSize; /* 3328 */
@@ -1244,7 +1244,7 @@ struct rdp_settings
 	ALIGN64 UINT32 JpegCodecId; /* 3777 */
 	ALIGN64 UINT32 JpegQuality; /* 3778 */
 	UINT64 padding3840[3840 - 3779]; /* 3779 */
-	UINT64 padding3904[3904 - 3779]; /* 3840 */
+	UINT64 padding3904[3904 - 3840]; /* 3840 */
 
 	/**
 	 * Caches
