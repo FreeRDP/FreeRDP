fae8f6fbf2dabef83091f82df0134c26182bdd1b
Norbert Federa <norbert.federa@thincast.com>
2014-07-24 21:07:44 +0200

    winpr/sync: Added InitOnceExecuteOnce plus CTest

--- winpr/libwinpr/interlocked/interlocked.c
+++ winpr/libwinpr/interlocked/interlocked.c
@@ -234,6 +234,15 @@ LONG InterlockedCompareExchange(LONG volatile *Destination, LONG Exchange, LONG 
 #endif
 }
 
+PVOID InterlockedCompareExchangePointer(PVOID volatile *Destination, PVOID Exchange, PVOID Comperand)
+{
+#ifdef __GNUC__
+	return __sync_val_compare_and_swap(Destination, Comperand, Exchange);
+#else
+	return 0;
+#endif
+}
+
 #endif /* _WIN32 */
 
 #if defined(_WIN32) && !defined(WINPR_INTERLOCKED_COMPARE_EXCHANGE64)
@@ -249,7 +258,7 @@ int static_mutex_lock(volatile HANDLE* static_mutex)
 	if (*static_mutex == NULL)
 	{
 		HANDLE handle = CreateMutex(NULL, FALSE, NULL);
-		
+
 		if (InterlockedCompareExchangePointer((PVOID*) static_mutex, (PVOID) handle, NULL) != NULL)
 			CloseHandle(handle);
 	}
