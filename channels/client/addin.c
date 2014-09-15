2bf2406325a3e987ff10fccd4c61f5ce8d95d602
Armin Novak <armin.novak@thincast.com>
2014-09-15 10:41:05 +0200

    Addind platform specific library prefix when trying to load channel libraries.

--- channels/client/addin.c
+++ channels/client/addin.c
@@ -156,19 +156,23 @@ FREERDP_ADDIN** freerdp_channels_list_dynamic_addins(LPSTR pszName, LPSTR pszSub
 
 	if (pszName && pszSubsystem && pszType)
 	{
-		sprintf_s(pszPattern, cchPattern, "%s-client-%s-%s.%s", pszName, pszSubsystem, pszType, pszExtension);
+		sprintf_s(pszPattern, cchPattern, CMAKE_SHARED_LIBRARY_PREFIX"%s-client-%s-%s.%s",
+				  pszName, pszSubsystem, pszType, pszExtension);
 	}
 	else if (pszName && pszType)
 	{
-		sprintf_s(pszPattern, cchPattern, "%s-client-?-%s.%s", pszName, pszType, pszExtension);
+		sprintf_s(pszPattern, cchPattern, CMAKE_SHARED_LIBRARY_PREFIX"%s-client-?-%s.%s",
+				  pszName, pszType, pszExtension);
 	}
 	else if (pszName)
 	{
-		sprintf_s(pszPattern, cchPattern, "%s-client*.%s", pszName, pszExtension);
+		sprintf_s(pszPattern, cchPattern, CMAKE_SHARED_LIBRARY_PREFIX"%s-client*.%s",
+				  pszName, pszExtension);
 	}
 	else
 	{
-		sprintf_s(pszPattern, cchPattern, "?-client*.%s", pszExtension);
+		sprintf_s(pszPattern, cchPattern, CMAKE_SHARED_LIBRARY_PREFIX"?-client*.%s",
+				  pszExtension);
 	}
 
 	cchPattern = strlen(pszPattern);
