e8b8ae1900f4a34b36dbdb83586cf4453363c765
Mehul Dhorda <mehul@hopto.com>
2014-11-07 14:58:58 -0800

    Added comment to clarify flow control PDU fields.

--- libfreerdp/core/rdp.c
+++ libfreerdp/core/rdp.c
@@ -917,8 +917,13 @@ int rdp_recv_out_of_sequence_pdu(rdpRdp* rdp, wStream* s)
 
 void rdp_read_flow_control_pdu(wStream* s, UINT16* type)
 {
-	/* Read flow control PDU - documented in FlowPDU section in T.128
-	 http://www.itu.int/rec/T-REC-T.128-199802-S/en */
+	/*
+	 * Read flow control PDU - documented in FlowPDU section in T.128
+	 * http://www.itu.int/rec/T-REC-T.128-199802-S/en
+	 * The specification for the PDU has pad8bits listed BEFORE pduTypeFlow.
+	 * However, so far pad8bits has always been observed to arrive AFTER pduTypeFlow.
+	 * Switched the order of these two fields to match this observation.
+	 */
 	UINT8 pduType;
 	Stream_Read_UINT8(s, pduType);	/* pduTypeFlow */
 	*type = pduType;
