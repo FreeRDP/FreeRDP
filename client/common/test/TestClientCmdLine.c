1b18e97d7e640f33fe66a2ea3d54353a45d313f9
Bernhard Miklautz <bernhard.miklautz@thincast.com>
2015-01-16 00:54:45 +0100

    TestClientCmdLine: fix formating

--- client/common/test/TestClientCmdLine.c
+++ client/common/test/TestClientCmdLine.c
@@ -18,78 +18,76 @@
 
 int TestClientCmdLine(int argc, char* argv[])
 {
+	int status;
+	rdpSettings* settings = freerdp_settings_new(0);
 
-  int status;
-  rdpSettings* settings = freerdp_settings_new(0);
+	char* cmd1[] = {"xfreerdp", "--help"};
+	TESTCASE(cmd1, COMMAND_LINE_STATUS_PRINT_HELP);
 
-  /* Test help */
-  char *cmd1[] = {"xfreerdp", "--help"};
-  TESTCASE(cmd1, COMMAND_LINE_STATUS_PRINT_HELP);
+	char* cmd2[] = {"xfreerdp", "/help"};
+	TESTCASE(cmd2, COMMAND_LINE_STATUS_PRINT_HELP);
 
-  char *cmd2[] = {"xfreerdp", "/help"};
-  TESTCASE(cmd2, COMMAND_LINE_STATUS_PRINT_HELP);
+	char* cmd3[] = {"xfreerdp", "-help"};
+	TESTCASE(cmd3, COMMAND_LINE_STATUS_PRINT_HELP);
 
-  char *cmd3[] = {"xfreerdp", "-help"};
-  TESTCASE(cmd3, COMMAND_LINE_STATUS_PRINT_HELP);
+	char* cmd4[] = {"xfreerdp", "--version"};
+	TESTCASE(cmd4, COMMAND_LINE_STATUS_PRINT_VERSION);
 
-  char *cmd4[] = {"xfreerdp", "--version"};
-  TESTCASE(cmd4, COMMAND_LINE_STATUS_PRINT_VERSION);
+	char* cmd5[] = {"xfreerdp", "/version"};
+	TESTCASE(cmd5, COMMAND_LINE_STATUS_PRINT_VERSION);
 
-  char *cmd5[] = {"xfreerdp", "/version"};
-  TESTCASE(cmd5, COMMAND_LINE_STATUS_PRINT_VERSION);
+	char* cmd6[] = {"xfreerdp", "-version"};
+	TESTCASE(cmd6, COMMAND_LINE_STATUS_PRINT_VERSION);
 
-  char *cmd6[] = {"xfreerdp", "-version"};
-  TESTCASE(cmd6, COMMAND_LINE_STATUS_PRINT_VERSION);
-
-  char *cmd7[] = {"xfreerdp", "test.freerdp.com"};
+	char* cmd7[] = {"xfreerdp", "test.freerdp.com"};
 	TESTCASE_SUCCESS(cmd7);
 
-  char *cmd8[] = {"xfreerdp", "-v", "test.freerdp.com"};
-  TESTCASE_SUCCESS(cmd8);
+	char* cmd8[] = {"xfreerdp", "-v", "test.freerdp.com"};
+	TESTCASE_SUCCESS(cmd8);
 
-  char *cmd9[] = {"xfreerdp", "--v", "test.freerdp.com"};
-  TESTCASE_SUCCESS(cmd9);
+	char* cmd9[] = {"xfreerdp", "--v", "test.freerdp.com"};
+	TESTCASE_SUCCESS(cmd9);
 
-  char *cmd10[] = {"xfreerdp", "/v:test.freerdp.com"};
-  TESTCASE_SUCCESS(cmd10);
+	char* cmd10[] = {"xfreerdp", "/v:test.freerdp.com"};
+	TESTCASE_SUCCESS(cmd10);
 
-  char *cmd11[] = {"xfreerdp", "--plugin", "rdpsnd", "--plugin", "rdpdr", "--data", "disk:media:/tmp", "--", "test.freerdp.com" };
-  TESTCASE_SUCCESS(cmd11);
+	char* cmd11[] = {"xfreerdp", "--plugin", "rdpsnd", "--plugin", "rdpdr", "--data", "disk:media:/tmp", "--", "test.freerdp.com" };
+	TESTCASE_SUCCESS(cmd11);
 
-  char *cmd12[] = {"xfreerdp", "/sound", "/drive:media:/tmp", "/v:test.freerdp.com" };
-  TESTCASE_SUCCESS(cmd12);
+	char* cmd12[] = {"xfreerdp", "/sound", "/drive:media:/tmp", "/v:test.freerdp.com" };
+	TESTCASE_SUCCESS(cmd12);
 
 	// password gets overwritten therefore it need to be writeable
-  char *cmd13[6] = {"xfreerdp", "-u", "test", "-p", "test", "test.freerdp.com"};
+	char* cmd13[6] = {"xfreerdp", "-u", "test", "-p", "test", "test.freerdp.com"};
 	cmd13[4] = malloc(5);
 	strncpy(cmd13[4], "test", 4);
-  TESTCASE_SUCCESS(cmd13);
+	TESTCASE_SUCCESS(cmd13);
 	free(cmd13[4]);
 
-  char *cmd14[] = {"xfreerdp", "-u", "test", "-p", "test", "-v", "test.freerdp.com"};
+	char* cmd14[] = {"xfreerdp", "-u", "test", "-p", "test", "-v", "test.freerdp.com"};
 	cmd14[4] = malloc(5);
 	strncpy(cmd14[4], "test", 4);
-  TESTCASE_SUCCESS(cmd14);
+	TESTCASE_SUCCESS(cmd14);
 	free(cmd14[4]);
 
-  char *cmd15[] = {"xfreerdp", "/u:test", "/p:test", "/v:test.freerdp.com"};
+	char* cmd15[] = {"xfreerdp", "/u:test", "/p:test", "/v:test.freerdp.com"};
 	cmd15[2] = malloc(7);
 	strncpy(cmd15[2], "/p:test", 6);
-  TESTCASE_SUCCESS(cmd15);
+	TESTCASE_SUCCESS(cmd15);
 	free(cmd15[2]);
 
-  char *cmd16[] = {"xfreerdp", "-invalid"};
-  TESTCASE(cmd16, COMMAND_LINE_ERROR_NO_KEYWORD);
+	char* cmd16[] = {"xfreerdp", "-invalid"};
+	TESTCASE(cmd16, COMMAND_LINE_ERROR_NO_KEYWORD);
 
-  char *cmd17[] = {"xfreerdp", "--invalid"};
-  TESTCASE(cmd17, COMMAND_LINE_ERROR_NO_KEYWORD);
+	char* cmd17[] = {"xfreerdp", "--invalid"};
+	TESTCASE(cmd17, COMMAND_LINE_ERROR_NO_KEYWORD);
 
-  char *cmd18[] = {"xfreerdp", "/kbd-list"};
-  TESTCASE(cmd18, COMMAND_LINE_STATUS_PRINT);
+	char* cmd18[] = {"xfreerdp", "/kbd-list"};
+	TESTCASE(cmd18, COMMAND_LINE_STATUS_PRINT);
 
-  char *cmd19[] = {"xfreerdp", "/monitor-list"};
-  TESTCASE(cmd19, COMMAND_LINE_STATUS_PRINT);
+	char* cmd19[] = {"xfreerdp", "/monitor-list"};
+	TESTCASE(cmd19, COMMAND_LINE_STATUS_PRINT);
 
-  return 0;
+	return 0;
 }
 
