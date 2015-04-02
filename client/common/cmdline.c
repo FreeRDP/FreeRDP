FreeRDP/FreeRDP

commit 0b7f9d4931540009532d338ddbec29c9a74c7cae

cmdline: fix incorrect usage of realloc.
Signed-off-by: Zhang Zhaolong <zhangzl2013@126.com>

client/common/cmdline.c
@@ -544,10 +544,17 @@ static char** freerdp_command_line_parse_comma_separated_values(char* list, int*
 static char** freerdp_command_line_parse_comma_separated_values_offset(char* list, int* count)
 {
 	char** p;
+	char** t;
+	int s;
 
 	p = freerdp_command_line_parse_comma_separated_values(list, count);
+	if (!p)
+		return NULL;
 
-	p = (char**) realloc(p, sizeof(char*) * (*count + 1));
+	t = (char**) realloc(p, sizeof(char*) * (*count + 1));
+	if (!t)
+		return NULL;
+	p = t;
 	MoveMemory(&p[1], p, sizeof(char*) * *count);
 	(*count)++;
 

commit 0615c13dbbc80b98326d6817c0fa172ef4199084

Fix regression introduced in 0b7f9d4
This fixes problems with command line parsing.

client/common/cmdline.c
@@ -547,14 +547,13 @@ static char** freerdp_command_line_parse_comma_separated_values_offset(char* lis
 	char** t;
 
 	p = freerdp_command_line_parse_comma_separated_values(list, count);
-	if (!p)
-		return NULL;
 
 	t = (char**) realloc(p, sizeof(char*) * (*count + 1));
 	if (!t)
 		return NULL;
 	p = t;
-	MoveMemory(&p[1], p, sizeof(char*) * *count);
+	if (count > 0)
+        MoveMemory(&p[1], p, sizeof(char*) * *count);
 	(*count)++;
 
 	return p;