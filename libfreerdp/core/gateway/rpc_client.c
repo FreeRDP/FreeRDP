FreeRDP/FreeRDP

commit f469e069dc8287f6fcfe0741961022e0cc50170f

stream: Stream_Ensure*Capacity: change return type
Change the return type of Stream_Ensure*Capacity from void to BOOL to be
able to detect realloc problems easily. Otherwise the only way to detect
this was to check if the capacity after the call was >= the required
size.
In case Stream_Ensure*Capacity fails the old memory is still available
and need to freed outside.

This commit also adds checks to most calls of Stream_Ensure*Capacity to
check if the call was successful.

libfreerdp/core/gateway/rpc_client.c
@@ -365,7 +365,8 @@ int rpc_client_recv_fragment(rdpRpc* rpc, wStream* fragment)
 
 		if (call->OpNum != TsProxySetupReceivePipeOpnum)
 		{
-			Stream_EnsureCapacity(pdu->s, header->response.alloc_hint);
+			if(!Stream_EnsureCapacity(pdu->s, header->response.alloc_hint))
+				return -1;
 			Stream_Write(pdu->s, &buffer[StubOffset], StubLength);
 			rpc->StubFragCount++;
 
@@ -402,7 +403,8 @@ int rpc_client_recv_fragment(rdpRpc* rpc, wStream* fragment)
 			pdu->Flags = 0;
 			pdu->Type = header->common.ptype;
 			pdu->CallId = header->common.call_id;
-			Stream_EnsureCapacity(pdu->s, Stream_Length(fragment));
+			if(!Stream_EnsureCapacity(pdu->s, Stream_Length(fragment)))
+				return -1;
 			Stream_Write(pdu->s, buffer, Stream_Length(fragment));
 			Stream_SealLength(pdu->s);
 			rpc_client_recv_pdu(rpc, pdu);
@@ -423,7 +425,8 @@ int rpc_client_recv_fragment(rdpRpc* rpc, wStream* fragment)
 		pdu->Flags = 0;
 		pdu->Type = header->common.ptype;
 		pdu->CallId = header->common.call_id;
-		Stream_EnsureCapacity(pdu->s, Stream_Length(fragment));
+		if(Stream_EnsureCapacity(pdu->s, Stream_Length(fragment)))
+			return -1;
 		Stream_Write(pdu->s, buffer, Stream_Length(fragment));
 		Stream_SealLength(pdu->s);
 		rpc_client_recv_pdu(rpc, pdu);

channels/audin/client/audin_main.c
@@ -176,7 +176,14 @@ static int audin_process_formats(IWTSVirtualChannelCallback* pChannelCallback, w
 			/* Store the agreed format in the corresponding index */
 			callback->formats[callback->formats_count++] = format;
 			/* Put the format to output buffer */
-			Stream_EnsureRemainingCapacity(out, 18 + format.cbSize);
+			if(!Stream_EnsureRemainingCapacity(out, 18 + format.cbSize))
+			{
+				free(callback->formats);
+				Stream_Free(out, TRUE);
+				return 1;
+			}
+
+
 			Stream_Write(out, fm, 18 + format.cbSize);
 		}
 	}

channels/audin/server/audin.c
@@ -118,7 +118,8 @@ static void audin_server_send_formats(audin_server* audin, wStream* s)
 			audin->context.server_formats[i].nChannels *
 			audin->context.server_formats[i].wBitsPerSample / 8;
 
-		Stream_EnsureRemainingCapacity(s, 18);
+		if(!Stream_EnsureRemainingCapacity(s, 18))
+			return;
 
 		Stream_Write_UINT16(s, audin->context.server_formats[i].wFormatTag);
 		Stream_Write_UINT16(s, audin->context.server_formats[i].nChannels);
@@ -130,7 +131,9 @@ static void audin_server_send_formats(audin_server* audin, wStream* s)
 
 		if (audin->context.server_formats[i].cbSize)
 		{
-			Stream_EnsureRemainingCapacity(s, audin->context.server_formats[i].cbSize);
+			if(!Stream_EnsureRemainingCapacity(s, audin->context.server_formats[i].cbSize))
+				return;
+
 			Stream_Write(s, audin->context.server_formats[i].data,
 				audin->context.server_formats[i].cbSize);
 		}
@@ -350,7 +353,8 @@ static void* audin_server_thread_func(void* arg)
 		WTSVirtualChannelRead(audin->audin_channel, 0, NULL, 0, &BytesReturned);
 		if (BytesReturned < 1)
 			continue;
-		Stream_EnsureRemainingCapacity(s, BytesReturned);
+		if (!Stream_EnsureRemainingCapacity(s, BytesReturned))
+			goto out_capacity;
 		if (WTSVirtualChannelRead(audin->audin_channel, 0, (PCHAR) Stream_Buffer(s),
 			Stream_Capacity(s), &BytesReturned) == FALSE)
 		{
@@ -392,8 +396,8 @@ static void* audin_server_thread_func(void* arg)
 		}
 	}
 
+out_capacity:
 	Stream_Free(s, TRUE);
-
 out:
 	WTSVirtualChannelClose(audin->audin_channel);
 	audin->audin_channel = NULL;

channels/cliprdr/client/cliprdr_main.c
@@ -793,7 +793,13 @@ static void cliprdr_virtual_channel_event_data_received(cliprdrPlugin* cliprdr,
 	}
 
 	data_in = cliprdr->data_in;
-	Stream_EnsureRemainingCapacity(data_in, (int) dataLength);
+	if(!Stream_EnsureRemainingCapacity(data_in, (int) dataLength))
+	{
+		Stream_Free(cliprdr->data_in, TRUE);
+		cliprdr->data_in = NULL;
+		return;
+	}
+
 	Stream_Write(data_in, pData, dataLength);
 
 	if (dataFlags & CHANNEL_FLAG_LAST)

channels/cliprdr/server/cliprdr_main.c
@@ -925,7 +925,8 @@ int cliprdr_server_read(CliprdrServerContext* context)
 		Stream_Read_UINT16(s, header.msgFlags); /* msgFlags (2 bytes) */
 		Stream_Read_UINT32(s, header.dataLen); /* dataLen (4 bytes) */
 
-		Stream_EnsureCapacity(s, (header.dataLen + CLIPRDR_HEADER_LENGTH));
+		if(!Stream_EnsureCapacity(s, (header.dataLen + CLIPRDR_HEADER_LENGTH)))
+			return -1;
 		Stream_SetPosition(s, position);
 
 		if (Stream_GetPosition(s) < (header.dataLen + CLIPRDR_HEADER_LENGTH))

channels/drdynvc/client/drdynvc_main.c
@@ -465,6 +465,8 @@ int dvcman_receive_channel_data_first(IWTSVirtualChannelManager* pChannelMgr, UI
 		Stream_Release(channel->dvc_data);
 
 	channel->dvc_data = StreamPool_Take(channel->dvcman->pool, length);
+	if (!channel->dvc_data)
+		return 1;
 	channel->dvc_data_length = length;
 
 	return 0;
@@ -968,7 +970,13 @@ static void drdynvc_virtual_channel_event_data_received(drdynvcPlugin* drdynvc,
 	}
 
 	data_in = drdynvc->data_in;
-	Stream_EnsureRemainingCapacity(data_in, (int) dataLength);
+	if(!Stream_EnsureRemainingCapacity(data_in, (int) dataLength))
+	{
+		Stream_Free(drdynvc->data_in, TRUE);
+		drdynvc->data_in = NULL;
+		return;
+	}
+
 	Stream_Write(data_in, pData, dataLength);
 
 	if (dataFlags & CHANNEL_FLAG_LAST)

channels/drdynvc/server/drdynvc_main.c
@@ -70,7 +70,8 @@ static void* drdynvc_server_thread(void* arg)
 		WTSVirtualChannelRead(context->priv->ChannelHandle, 0, NULL, 0, &BytesReturned);
 		if (BytesReturned < 1)
 			continue;
-		Stream_EnsureRemainingCapacity(s, BytesReturned);
+		if (!Stream_EnsureRemainingCapacity(s, BytesReturned))
+			break;
 		if (!WTSVirtualChannelRead(context->priv->ChannelHandle, 0,
 			(PCHAR) Stream_Buffer(s), Stream_Capacity(s), &BytesReturned))
 		{

channels/drive/client/drive_file.c
@@ -419,8 +419,9 @@ BOOL drive_file_query_information(DRIVE_FILE* file, UINT32 FsInformationClass, w
 	{
 		case FileBasicInformation:
 			/* http://msdn.microsoft.com/en-us/library/cc232094.aspx */
+			if(!Stream_EnsureRemainingCapacity(output, 4 + 36))
+				goto out_fail;
 			Stream_Write_UINT32(output, 36); /* Length */
-			Stream_EnsureRemainingCapacity(output, 36);
 			Stream_Write_UINT64(output, FILE_TIME_SYSTEM_TO_RDP(st.st_mtime)); /* CreationTime */
 			Stream_Write_UINT64(output, FILE_TIME_SYSTEM_TO_RDP(st.st_atime)); /* LastAccessTime */
 			Stream_Write_UINT64(output, FILE_TIME_SYSTEM_TO_RDP(st.st_mtime)); /* LastWriteTime */
@@ -431,8 +432,9 @@ BOOL drive_file_query_information(DRIVE_FILE* file, UINT32 FsInformationClass, w
 
 		case FileStandardInformation:
 			/*  http://msdn.microsoft.com/en-us/library/cc232088.aspx */
+			if(!Stream_EnsureRemainingCapacity(output, 4 + 22))
+				goto out_fail;
 			Stream_Write_UINT32(output, 22); /* Length */
-			Stream_EnsureRemainingCapacity(output, 22);
 			Stream_Write_UINT64(output, st.st_size); /* AllocationSize */
 			Stream_Write_UINT64(output, st.st_size); /* EndOfFile */
 			Stream_Write_UINT32(output, st.st_nlink); /* NumberOfLinks */
@@ -443,17 +445,23 @@ BOOL drive_file_query_information(DRIVE_FILE* file, UINT32 FsInformationClass, w
 
 		case FileAttributeTagInformation:
 			/* http://msdn.microsoft.com/en-us/library/cc232093.aspx */
+			if(!Stream_EnsureRemainingCapacity(output, 4 + 8))
+				goto out_fail;
 			Stream_Write_UINT32(output, 8); /* Length */
-			Stream_EnsureRemainingCapacity(output, 8);
 			Stream_Write_UINT32(output, FILE_ATTR_SYSTEM_TO_RDP(file, st)); /* FileAttributes */
 			Stream_Write_UINT32(output, 0); /* ReparseTag */
 			break;
 
 		default:
+			/* Unhandled FsInformationClass */
 			Stream_Write_UINT32(output, 0); /* Length */
 			return FALSE;
 	}
 	return TRUE;
+
+out_fail:
+	Stream_Write_UINT32(output, 0); /* Length */
+	return FALSE;
 }
 
 int dir_empty(const char *path)
@@ -680,8 +688,9 @@ BOOL drive_file_query_directory(DRIVE_FILE* file, UINT32 FsInformationClass, BYT
 	{
 		case FileDirectoryInformation:
 			/* http://msdn.microsoft.com/en-us/library/cc232097.aspx */
+			if(!Stream_EnsureRemainingCapacity(output, 4 + 64 + length))
+				goto out_fail;
 			Stream_Write_UINT32(output, 64 + length); /* Length */
-			Stream_EnsureRemainingCapacity(output, 64 + length);
 			Stream_Write_UINT32(output, 0); /* NextEntryOffset */
 			Stream_Write_UINT32(output, 0); /* FileIndex */
 			Stream_Write_UINT64(output, FILE_TIME_SYSTEM_TO_RDP(st.st_mtime)); /* CreationTime */
@@ -697,8 +706,9 @@ BOOL drive_file_query_directory(DRIVE_FILE* file, UINT32 FsInformationClass, BYT
 
 		case FileFullDirectoryInformation:
 			/* http://msdn.microsoft.com/en-us/library/cc232068.aspx */
+			if(!Stream_EnsureRemainingCapacity(output, 4 + 68 + length))
+				goto out_fail;
 			Stream_Write_UINT32(output, 68 + length); /* Length */
-			Stream_EnsureRemainingCapacity(output, 68 + length);
 			Stream_Write_UINT32(output, 0); /* NextEntryOffset */
 			Stream_Write_UINT32(output, 0); /* FileIndex */
 			Stream_Write_UINT64(output, FILE_TIME_SYSTEM_TO_RDP(st.st_mtime)); /* CreationTime */
@@ -715,8 +725,9 @@ BOOL drive_file_query_directory(DRIVE_FILE* file, UINT32 FsInformationClass, BYT
 
 		case FileBothDirectoryInformation:
 			/* http://msdn.microsoft.com/en-us/library/cc232095.aspx */
+			if(!Stream_EnsureRemainingCapacity(output, 4 + 93 + length))
+				goto out_fail;
 			Stream_Write_UINT32(output, 93 + length); /* Length */
-			Stream_EnsureRemainingCapacity(output, 93 + length);
 			Stream_Write_UINT32(output, 0); /* NextEntryOffset */
 			Stream_Write_UINT32(output, 0); /* FileIndex */
 			Stream_Write_UINT64(output, FILE_TIME_SYSTEM_TO_RDP(st.st_mtime)); /* CreationTime */
@@ -736,8 +747,9 @@ BOOL drive_file_query_directory(DRIVE_FILE* file, UINT32 FsInformationClass, BYT
 
 		case FileNamesInformation:
 			/* http://msdn.microsoft.com/en-us/library/cc232077.aspx */
+			if(!Stream_EnsureRemainingCapacity(output, 4 + 12 + length))
+				goto out_fail;
 			Stream_Write_UINT32(output, 12 + length); /* Length */
-			Stream_EnsureRemainingCapacity(output, 12 + length);
 			Stream_Write_UINT32(output, 0); /* NextEntryOffset */
 			Stream_Write_UINT32(output, 0); /* FileIndex */
 			Stream_Write_UINT32(output, length); /* FileNameLength */
@@ -745,6 +757,7 @@ BOOL drive_file_query_directory(DRIVE_FILE* file, UINT32 FsInformationClass, BYT
 			break;
 
 		default:
+			/* Unhandled FsInformationClass */
 			Stream_Write_UINT32(output, 0); /* Length */
 			Stream_Write_UINT8(output, 0); /* Padding */
 			ret = FALSE;
@@ -752,8 +765,13 @@ BOOL drive_file_query_directory(DRIVE_FILE* file, UINT32 FsInformationClass, BYT
 	}
 
 	free(ent_path);
-
 	return ret;
+
+out_fail:
+	free(ent_path);
+	Stream_Write_UINT32(output, 0); /* Length */
+	Stream_Write_UINT8(output, 0); /* Padding */
+	return FALSE;
 }
 
 #ifdef _WIN32

commit 3910baf49d7841b321934191715821bc7298622f

GW RPC-HTTP login fix
* f469e069dc8287f6fcfe0741961022e0cc50170f introduced a logic flip
  that broke RPC-HTTP Gateway.  Fix that.

libfreerdp/core/gateway/rpc_client.c
@@ -425,7 +425,7 @@ int rpc_client_recv_fragment(rdpRpc* rpc, wStream* fragment)
 		pdu->Flags = 0;
 		pdu->Type = header->common.ptype;
 		pdu->CallId = header->common.call_id;
-		if (Stream_EnsureCapacity(pdu->s, Stream_Length(fragment)))
+		if (!Stream_EnsureCapacity(pdu->s, Stream_Length(fragment)))
 			return -1;
 		Stream_Write(pdu->s, buffer, Stream_Length(fragment));
 		Stream_SealLength(pdu->s);