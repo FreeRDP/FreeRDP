de1a21eadd4661f78819f7e59eed1afc31d5a47d
Emmanuel Ledoux <eledoux@hp.com>
2015-05-27 18:24:56 +0200

    winpr-thread: added HP's copyright

--- winpr/libwinpr/thread/thread.c
+++ winpr/libwinpr/thread/thread.c
@@ -3,6 +3,7 @@
  * Process Thread Functions
  *
  * Copyright 2012 Marc-Andre Moreau <marcandre.moreau@gmail.com>
+ * Copyright 2015 Hewlett-Packard Development Company, L.P.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
