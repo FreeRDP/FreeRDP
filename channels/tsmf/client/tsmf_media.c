FreeRDP/FreeRDP

commit f8120919af46ac3af1ec8f217e0c71348f69be09

Add checks for some XXX_New and XXX_Add functions
Based on PR #2616

channels/tsmf/client/tsmf_media.c
@@ -279,7 +279,6 @@ TSMF_PRESENTATION* tsmf_presentation_new(const BYTE* guid, IWTSVirtualChannelCal
 		return NULL;
 
 	presentation = (TSMF_PRESENTATION*) calloc(1, sizeof(TSMF_PRESENTATION));
-
 	if (!presentation)
 	{
 		WLog_ERR(TAG, "calloc failed");
@@ -290,11 +289,22 @@ TSMF_PRESENTATION* tsmf_presentation_new(const BYTE* guid, IWTSVirtualChannelCal
 	presentation->channel_callback = pChannelCallback;
 	presentation->volume = 5000; /* 50% */
 	presentation->stream_list = ArrayList_New(TRUE);
+	if (presentation->stream_list)
+		goto error_stream_list;
+
 	ArrayList_Object(presentation->stream_list)->fnObjectFree = (OBJECT_FREE_FN) _tsmf_stream_free;
 
-	ArrayList_Add(presentation_list, presentation);
+	if (ArrayList_Add(presentation_list, presentation) < 0)
+		goto error_add;
 
 	return presentation;
+
+error_add:
+	ArrayList_Free(presentation->stream_list);
+error_stream_list:
+	free(presentation);
+	return NULL;
+
 }
 
 static char* guid_to_string(const BYTE* guid, char* str, size_t len)
@@ -937,7 +947,6 @@ TSMF_STREAM* tsmf_stream_new(TSMF_PRESENTATION* presentation, UINT32 stream_id)
 	}
 
 	stream = (TSMF_STREAM*) calloc(1, sizeof(TSMF_STREAM));
-
 	if (!stream)
 	{
 		WLog_ERR(TAG, "Calloc failed");
@@ -947,17 +956,49 @@ TSMF_STREAM* tsmf_stream_new(TSMF_PRESENTATION* presentation, UINT32 stream_id)
 	stream->stream_id = stream_id;
 	stream->presentation = presentation;
 	stream->stopEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	if (!stream->stopEvent)
+		goto error_stopEvent;
 	stream->ready = CreateEvent(NULL, TRUE, TRUE, NULL);
+	if (!stream->ready)
+		goto error_ready;
 	stream->sample_list = Queue_New(TRUE, -1, -1);
+	if (!stream->sample_list)
+		goto error_sample_list;
 	stream->sample_list->object.fnObjectFree = tsmf_sample_free;
 	stream->sample_ack_list = Queue_New(TRUE, -1, -1);
+	if (!stream->sample_ack_list)
+		goto error_sample_ack_list;
 	stream->sample_ack_list->object.fnObjectFree = tsmf_sample_free;
-	stream->play_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) tsmf_stream_playback_func, stream, 0, NULL);
-	stream->ack_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)tsmf_stream_ack_func, stream, 0, NULL);
 
-	ArrayList_Add(presentation->stream_list, stream);
+	stream->play_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) tsmf_stream_playback_func, stream, 0, NULL);
+	if (!stream->play_thread)
+		goto error_play_thread;
+	stream->ack_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)tsmf_stream_ack_func, stream, 0, NULL);
+	if (!stream->ack_thread)
+		goto error_ack_thread;
+
+	if (ArrayList_Add(presentation->stream_list, stream) < 0)
+		goto error_add;
 
 	return stream;
+
+error_add:
+	SetEvent(stream->stopEvent);
+	WaitForSingleObject(stream->ack_thread, INFINITE);
+error_ack_thread:
+	SetEvent(stream->stopEvent);
+	WaitForSingleObject(stream->play_thread, INFINITE);
+error_play_thread:
+	Queue_Free(stream->sample_ack_list);
+error_sample_ack_list:
+	Queue_Free(stream->sample_list);
+error_sample_list:
+	CloseHandle(stream->ready);
+error_ready:
+	CloseHandle(stream->stopEvent);
+error_stopEvent:
+	free(stream);
+	return NULL;
 }
 
 TSMF_STREAM *tsmf_stream_find_by_id(TSMF_PRESENTATION* presentation, UINT32 stream_id)
@@ -1151,7 +1192,7 @@ static void tsmf_signal_handler(int s)
 
 #endif
 
-void tsmf_media_init(void)
+BOOL tsmf_media_init(void)
 {
 #ifndef _WIN32
 	struct sigaction sigtrap;
@@ -1165,6 +1206,9 @@ void tsmf_media_init(void)
 	if (!presentation_list)
 	{
 		presentation_list = ArrayList_New(TRUE);
+		if (!presentation_list)
+			return FALSE;
 		ArrayList_Object(presentation_list)->fnObjectFree = (OBJECT_FREE_FN) _tsmf_presentation_free;
 	}
+	return TRUE;
 }

channels/encomsp/client/encomsp_main.c
@@ -662,12 +662,16 @@ static void encomsp_process_connect(encomspPlugin* encomsp)
 static wListDictionary* g_InitHandles = NULL;
 static wListDictionary* g_OpenHandles = NULL;
 
-void encomsp_add_init_handle_data(void* pInitHandle, void* pUserData)
+BOOL encomsp_add_init_handle_data(void* pInitHandle, void* pUserData)
 {
 	if (!g_InitHandles)
+	{
 		g_InitHandles = ListDictionary_New(TRUE);
+		if (!g_InitHandles)
+			return FALSE;
+	}
 
-	ListDictionary_Add(g_InitHandles, pInitHandle, pUserData);
+	return ListDictionary_Add(g_InitHandles, pInitHandle, pUserData);
 }
 
 void* encomsp_get_init_handle_data(void* pInitHandle)
@@ -687,14 +691,18 @@ void encomsp_remove_init_handle_data(void* pInitHandle)
 	}
 }
 
-void encomsp_add_open_handle_data(DWORD openHandle, void* pUserData)
+BOOL encomsp_add_open_handle_data(DWORD openHandle, void* pUserData)
 {
 	void* pOpenHandle = (void*) (size_t) openHandle;
 
 	if (!g_OpenHandles)
+	{
 		g_OpenHandles = ListDictionary_New(TRUE);
+		if (!g_OpenHandles)
+			return FALSE;
+	}
 
-	ListDictionary_Add(g_OpenHandles, pOpenHandle, pUserData);
+	return ListDictionary_Add(g_OpenHandles, pOpenHandle, pUserData);
 }
 
 void* encomsp_get_open_handle_data(DWORD openHandle)
@@ -843,7 +851,11 @@ static void encomsp_virtual_channel_event_connected(encomspPlugin* encomsp, LPVO
 	status = encomsp->channelEntryPoints.pVirtualChannelOpen(encomsp->InitHandle,
 		&encomsp->OpenHandle, encomsp->channelDef.name, encomsp_virtual_channel_open_event);
 
-	encomsp_add_open_handle_data(encomsp->OpenHandle, encomsp);
+	if (!encomsp_add_open_handle_data(encomsp->OpenHandle, encomsp))
+	{
+		WLog_ERR(TAG, "%s: unable to register open handle", __FUNCTION__);
+		return;
+	}
 
 	if (status != CHANNEL_RC_OK)
 	{
@@ -853,9 +865,20 @@ static void encomsp_virtual_channel_event_connected(encomspPlugin* encomsp, LPVO
 	}
 
 	encomsp->queue = MessageQueue_New(NULL);
+	if (!encomsp->queue)
+	{
+		WLog_ERR(TAG, "%s: unable to create message queue", __FUNCTION__);
+		return;
+	}
 
 	encomsp->thread = CreateThread(NULL, 0,
 			(LPTHREAD_START_ROUTINE) encomsp_virtual_channel_client_thread, (void*) encomsp, 0, NULL);
+	if (!encomsp->thread)
+	{
+		WLog_ERR(TAG, "%s: unable to create thread", __FUNCTION__);
+		return;
+	}
+
 }
 
 static void encomsp_virtual_channel_event_disconnected(encomspPlugin* encomsp)
@@ -980,7 +1003,5 @@ BOOL VCAPITYPE VirtualChannelEntry(PCHANNEL_ENTRY_POINTS pEntryPoints)
 	encomsp->channelEntryPoints.pInterface = *(encomsp->channelEntryPoints.ppInterface);
 	encomsp->channelEntryPoints.ppInterface = &(encomsp->channelEntryPoints.pInterface);
 
-	encomsp_add_init_handle_data(encomsp->InitHandle, (void*) encomsp);
-
-	return 1;
+	return encomsp_add_init_handle_data(encomsp->InitHandle, (void*) encomsp) ? 1 : -1;
 }

channels/parallel/client/parallel_main.c
@@ -307,7 +307,6 @@ int DeviceServiceEntry(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints)
 	if (name[0] && path[0])
 	{
 		parallel = (PARALLEL_DEVICE*) calloc(1, sizeof(PARALLEL_DEVICE));
-
 		if (!parallel)
 			return -1;
 
@@ -318,6 +317,8 @@ int DeviceServiceEntry(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints)
 
 		length = strlen(name);
 		parallel->device.data = Stream_New(NULL, length + 1);
+		if (!parallel->device.data)
+			goto error_device_data;
 
 		for (i = 0; i <= length; i++)
 			Stream_Write_UINT8(parallel->device.data, name[i] < 0 ? '_' : name[i]);
@@ -325,11 +326,23 @@ int DeviceServiceEntry(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints)
 		parallel->path = path;
 
 		parallel->queue = MessageQueue_New(NULL);
+		if (!parallel->queue)
+			goto error_queue;
 
 		pEntryPoints->RegisterDevice(pEntryPoints->devman, (DEVICE*) parallel);
 
 		parallel->thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) parallel_thread_func, (void*) parallel, 0, NULL);
+		if (!parallel->thread)
+			goto error_thread;
 	}
 
 	return 0;
+
+error_thread:
+	MessageQueue_Free(parallel->queue);
+error_queue:
+	Stream_Free(parallel->device.data, TRUE);
+error_device_data:
+	free(parallel);
+	return -1;
 }

channels/rail/client/rail_main.c
@@ -62,14 +62,14 @@ int rail_send(railPlugin* rail, wStream* s)
 	return status;
 }
 
-void rail_send_channel_data(railPlugin* rail, void* data, size_t length)
+BOOL rail_send_channel_data(railPlugin* rail, void* data, size_t length)
 {
 	wStream* s = NULL;
 
 	s = Stream_New(NULL, length);
 	Stream_Write(s, data, length);
 
-	rail_send(rail, s);
+	return rail_send(rail, s) == CHANNEL_RC_OK;
 }
 
 /**
@@ -96,24 +96,21 @@ int rail_client_execute(RailClientContext* context, RAIL_EXEC_ORDER* exec)
 	rail_string_to_unicode_string(exec->RemoteApplicationWorkingDir, &exec->workingDir); /* ShellWorkingDirectory */
 	rail_string_to_unicode_string(exec->RemoteApplicationArguments, &exec->arguments); /* RemoteApplicationCmdLine */
 
-	rail_send_client_exec_order(rail, exec);
-
-	return 0;
+	return rail_send_client_exec_order(rail, exec) ? 0 : -1;
 }
 
 int rail_client_activate(RailClientContext* context, RAIL_ACTIVATE_ORDER* activate)
 {
 	railPlugin* rail = (railPlugin*) context->handle;
 
-	rail_send_client_activate_order(rail, activate);
-
-	return 0;
+	return rail_send_client_activate_order(rail, activate) ? 0 : -1;
 }
 
-void rail_send_client_sysparam(RailClientContext* context, RAIL_SYSPARAM_ORDER* sysparam)
+BOOL rail_send_client_sysparam(RailClientContext* context, RAIL_SYSPARAM_ORDER* sysparam)
 {
 	wStream* s;
 	int length;
+	BOOL ret;
 	railPlugin* rail = (railPlugin*) context->handle;
 
 	length = RAIL_SYSPARAM_ORDER_LENGTH;
@@ -139,56 +136,63 @@ void rail_send_client_sysparam(RailClientContext* context, RAIL_SYSPARAM_ORDER* 
 	}
 
 	s = rail_pdu_init(RAIL_SYSPARAM_ORDER_LENGTH + 8);
-	rail_write_client_sysparam_order(s, sysparam);
-	rail_send_pdu(rail, s, RDP_RAIL_ORDER_SYSPARAM);
+	if (!s)
+		return FALSE;
+
+	ret = rail_write_client_sysparam_order(s, sysparam) &&
+			rail_send_pdu(rail, s, RDP_RAIL_ORDER_SYSPARAM);
+
 	Stream_Free(s, TRUE);
+	return ret;
 }
 
 int rail_client_system_param(RailClientContext* context, RAIL_SYSPARAM_ORDER* sysparam)
 {
+	BOOL ok = TRUE;
+
 	if (sysparam->params & SPI_MASK_SET_HIGH_CONTRAST)
 	{
 		sysparam->param = SPI_SET_HIGH_CONTRAST;
-		rail_send_client_sysparam(context, sysparam);
+		ok &= rail_send_client_sysparam(context, sysparam);
 	}
 
 	if (sysparam->params & SPI_MASK_TASKBAR_POS)
 	{
 		sysparam->param = SPI_TASKBAR_POS;
-		rail_send_client_sysparam(context, sysparam);
+		ok &= rail_send_client_sysparam(context, sysparam);
 	}
 
 	if (sysparam->params & SPI_MASK_SET_MOUSE_BUTTON_SWAP)
 	{
 		sysparam->param = SPI_SET_MOUSE_BUTTON_SWAP;
-		rail_send_client_sysparam(context, sysparam);
+		ok &= rail_send_client_sysparam(context, sysparam);
 	}
 
 	if (sysparam->params & SPI_MASK_SET_KEYBOARD_PREF)
 	{
 		sysparam->param = SPI_SET_KEYBOARD_PREF;
-		rail_send_client_sysparam(context, sysparam);
+		ok &= rail_send_client_sysparam(context, sysparam);
 	}
 
 	if (sysparam->params & SPI_MASK_SET_DRAG_FULL_WINDOWS)
 	{
 		sysparam->param = SPI_SET_DRAG_FULL_WINDOWS;
-		rail_send_client_sysparam(context, sysparam);
+		ok &= rail_send_client_sysparam(context, sysparam);
 	}
 
 	if (sysparam->params & SPI_MASK_SET_KEYBOARD_CUES)
 	{
 		sysparam->param = SPI_SET_KEYBOARD_CUES;
-		rail_send_client_sysparam(context, sysparam);
+		ok &= rail_send_client_sysparam(context, sysparam);
 	}
 
 	if (sysparam->params & SPI_MASK_SET_WORK_AREA)
 	{
 		sysparam->param = SPI_SET_WORK_AREA;
-		rail_send_client_sysparam(context, sysparam);
+		ok &= rail_send_client_sysparam(context, sysparam);
 	}
 
-	return 0;
+	return ok ? 0 : -1;
 }
 
 int rail_server_system_param(RailClientContext* context, RAIL_SYSPARAM_ORDER* sysparam)
@@ -200,18 +204,14 @@ int rail_client_system_command(RailClientContext* context, RAIL_SYSCOMMAND_ORDER
 {
 	railPlugin* rail = (railPlugin*) context->handle;
 
-	rail_send_client_syscommand_order(rail, syscommand);
-
-	return 0;
+	return rail_send_client_syscommand_order(rail, syscommand) ? 0 : -1;
 }
 
 int rail_client_handshake(RailClientContext* context, RAIL_HANDSHAKE_ORDER* handshake)
 {
 	railPlugin* rail = (railPlugin*) context->handle;
 
-	rail_send_handshake_order(rail, handshake);
-
-	return 0;
+	return rail_send_handshake_order(rail, handshake) ? 0 : -1;
 }
 
 int rail_server_handshake(RailClientContext* context, RAIL_HANDSHAKE_ORDER* handshake)
@@ -223,9 +223,7 @@ int rail_client_handshake_ex(RailClientContext* context, RAIL_HANDSHAKE_EX_ORDER
 {
 	railPlugin* rail = (railPlugin*) context->handle;
 
-	rail_send_handshake_ex_order(rail, handshakeEx);
-
-	return 0;
+	return rail_send_handshake_ex_order(rail, handshakeEx) ? 0 : -1;
 }
 
 int rail_server_handshake_ex(RailClientContext* context, RAIL_HANDSHAKE_EX_ORDER* handshakeEx)
@@ -237,18 +235,14 @@ int rail_client_notify_event(RailClientContext* context, RAIL_NOTIFY_EVENT_ORDER
 {
 	railPlugin* rail = (railPlugin*) context->handle;
 
-	rail_send_client_notify_event_order(rail, notifyEvent);
-
-	return 0;
+	return rail_send_client_notify_event_order(rail, notifyEvent) ? 0 : -1;
 }
 
 int rail_client_window_move(RailClientContext* context, RAIL_WINDOW_MOVE_ORDER* windowMove)
 {
 	railPlugin* rail = (railPlugin*) context->handle;
 
-	rail_send_client_window_move_order(rail, windowMove);
-
-	return 0;
+	return rail_send_client_window_move_order(rail, windowMove) ? 0 : -1;
 }
 
 int rail_server_local_move_size(RailClientContext* context, RAIL_LOCALMOVESIZE_ORDER* localMoveSize)
@@ -265,27 +259,21 @@ int rail_client_information(RailClientContext* context, RAIL_CLIENT_STATUS_ORDER
 {
 	railPlugin* rail = (railPlugin*) context->handle;
 
-	rail_send_client_status_order(rail, clientStatus);
-
-	return 0;
+	return rail_send_client_status_order(rail, clientStatus) ? 0 : -1;
 }
 
 int rail_client_system_menu(RailClientContext* context, RAIL_SYSMENU_ORDER* sysmenu)
 {
 	railPlugin* rail = (railPlugin*) context->handle;
 
-	rail_send_client_sysmenu_order(rail, sysmenu);
-
-	return 0;
+	return rail_send_client_sysmenu_order(rail, sysmenu) ? 0 : -1;
 }
 
 int rail_client_language_bar_info(RailClientContext* context, RAIL_LANGBAR_INFO_ORDER* langBarInfo)
 {
 	railPlugin* rail = (railPlugin*) context->handle;
 
-	rail_send_client_langbar_info_order(rail, langBarInfo);
-
-	return 0;
+	return rail_send_client_langbar_info_order(rail, langBarInfo) ? 0 : -1;
 }
 
 int rail_server_language_bar_info(RailClientContext* context, RAIL_LANGBAR_INFO_ORDER* langBarInfo)
@@ -302,9 +290,7 @@ int rail_client_get_appid_request(RailClientContext* context, RAIL_GET_APPID_REQ
 {
 	railPlugin* rail = (railPlugin*) context->handle;
 
-	rail_send_client_get_appid_req_order(rail, getAppIdReq);
-
-	return 0;
+	return rail_send_client_get_appid_req_order(rail, getAppIdReq) ? 0 : -1;
 }
 
 int rail_server_get_appid_response(RailClientContext* context, RAIL_GET_APPID_RESP_ORDER* getAppIdResp)
@@ -317,12 +303,16 @@ int rail_server_get_appid_response(RailClientContext* context, RAIL_GET_APPID_RE
 static wListDictionary* g_InitHandles = NULL;
 static wListDictionary* g_OpenHandles = NULL;
 
-void rail_add_init_handle_data(void* pInitHandle, void* pUserData)
+BOOL rail_add_init_handle_data(void* pInitHandle, void* pUserData)
 {
 	if (!g_InitHandles)
+	{
 		g_InitHandles = ListDictionary_New(TRUE);
+		if (!g_InitHandles)
+			return FALSE;
+	}
 
-	ListDictionary_Add(g_InitHandles, pInitHandle, pUserData);
+	return ListDictionary_Add(g_InitHandles, pInitHandle, pUserData);
 }
 
 void* rail_get_init_handle_data(void* pInitHandle)
@@ -342,14 +332,18 @@ void rail_remove_init_handle_data(void* pInitHandle)
 	}
 }
 
-void rail_add_open_handle_data(DWORD openHandle, void* pUserData)
+BOOL rail_add_open_handle_data(DWORD openHandle, void* pUserData)
 {
 	void* pOpenHandle = (void*) (size_t) openHandle;
 
 	if (!g_OpenHandles)
+	{
 		g_OpenHandles = ListDictionary_New(TRUE);
+		if (!g_OpenHandles)
+			return FALSE;
+	}
 
-	ListDictionary_Add(g_OpenHandles, pOpenHandle, pUserData);
+	return ListDictionary_Add(g_OpenHandles, pOpenHandle, pUserData);
 }
 
 void* rail_get_open_handle_data(DWORD openHandle)
@@ -387,10 +381,19 @@ static void rail_virtual_channel_event_data_received(railPlugin* rail,
 			Stream_Free(rail->data_in, TRUE);
 
 		rail->data_in = Stream_New(NULL, totalLength);
+		if (!rail->data_in)
+		{
+			WLog_ERR(TAG, "%s: unable to allocate data_in", __FUNCTION__);
+			return;
+		}
 	}
 
 	data_in = rail->data_in;
-	Stream_EnsureRemainingCapacity(data_in, (int) dataLength);
+	if (!Stream_EnsureRemainingCapacity(data_in, (int) dataLength))
+	{
+		WLog_ERR(TAG, "%s: unable to grow data_in to %d", __FUNCTION__, dataLength);
+		return;
+	}
 	Stream_Write(data_in, pData, dataLength);
 
 	if (dataFlags & CHANNEL_FLAG_LAST)
@@ -455,7 +458,11 @@ static void* rail_virtual_channel_client_thread(void* arg)
 			if (message.id == 0)
 			{
 				data = (wStream*) message.wParam;
-				rail_order_recv(rail, data);
+				if (!rail_order_recv(rail, data))
+				{
+					WLog_ERR(TAG, "%s: invalid message, exiting", __FUNCTION__);
+					break;
+				}
 			}
 		}
 	}
@@ -471,7 +478,11 @@ static void rail_virtual_channel_event_connected(railPlugin* rail, LPVOID pData,
 	status = rail->channelEntryPoints.pVirtualChannelOpen(rail->InitHandle,
 		&rail->OpenHandle, rail->channelDef.name, rail_virtual_channel_open_event);
 
-	rail_add_open_handle_data(rail->OpenHandle, rail);
+	if (!rail_add_open_handle_data(rail->OpenHandle, rail))
+	{
+		WLog_ERR(TAG,  "%s: unable to register open handle", __FUNCTION__);
+		return;
+	}
 
 	if (status != CHANNEL_RC_OK)
 	{
@@ -481,6 +492,11 @@ static void rail_virtual_channel_event_connected(railPlugin* rail, LPVOID pData,
 	}
 
 	rail->queue = MessageQueue_New(NULL);
+	if (!rail->queue)
+	{
+		WLog_ERR(TAG, "%s: unable to create a message queue", __FUNCTION__);
+		return;
+	}
 
 	rail->thread = CreateThread(NULL, 0,
 			(LPTHREAD_START_ROUTINE) rail_virtual_channel_client_thread, (void*) rail, 0, NULL);
@@ -574,6 +590,8 @@ BOOL VCAPITYPE VirtualChannelEntry(PCHANNEL_ENTRY_POINTS pEntryPoints)
 			(pEntryPointsEx->MagicNumber == FREERDP_CHANNEL_MAGIC_NUMBER))
 	{
 		context = (RailClientContext*) calloc(1, sizeof(RailClientContext));
+		if (!context)
+			return FALSE;
 
 		context->handle = (void*) rail;
 		context->custom = NULL;
@@ -623,7 +641,5 @@ BOOL VCAPITYPE VirtualChannelEntry(PCHANNEL_ENTRY_POINTS pEntryPoints)
 	rail->channelEntryPoints.pInterface = *(rail->channelEntryPoints.ppInterface);
 	rail->channelEntryPoints.ppInterface = &(rail->channelEntryPoints.pInterface);
 
-	rail_add_init_handle_data(rail->InitHandle, (void*) rail);
-
-	return 1;
+	return rail_add_init_handle_data(rail->InitHandle, (void*) rail) ? 1 : -1;
 }

channels/rail/client/rail_main.h
@@ -51,6 +51,6 @@ struct rail_plugin
 typedef struct rail_plugin railPlugin;
 
 RailClientContext* rail_get_client_interface(railPlugin* rail);
-void rail_send_channel_data(railPlugin* rail, void* data, size_t length);
+BOOL rail_send_channel_data(railPlugin* rail, void* data, size_t length);
 
 #endif /* FREERDP_CHANNEL_CLIENT_RAIL_MAIN_H */

commit 8e70012d65dec5c83789db89fa92c7cefb4cedfc

tsmf: fix invalid check and a potential segfault
This resuscitates tsmf which was accidently broken in commit f8120919

channels/tsmf/client/tsmf_media.c
@@ -288,8 +288,7 @@ TSMF_PRESENTATION* tsmf_presentation_new(const BYTE* guid, IWTSVirtualChannelCal
 	CopyMemory(presentation->presentation_id, guid, GUID_SIZE);
 	presentation->channel_callback = pChannelCallback;
 	presentation->volume = 5000; /* 50% */
-	presentation->stream_list = ArrayList_New(TRUE);
-	if (presentation->stream_list)
+	if (!(presentation->stream_list = ArrayList_New(TRUE)))
 		goto error_stream_list;
 
 	ArrayList_Object(presentation->stream_list)->fnObjectFree = (OBJECT_FREE_FN) _tsmf_stream_free;
@@ -828,6 +827,9 @@ void tsmf_presentation_set_geometry_info(TSMF_PRESENTATION* presentation,
 	UINT32 count;
 	TSMF_STREAM* stream;
 	void *tmp_rects;
+
+	if (num_rects < 1 || !rects)
+		return;
 
 	/* The server may send messages with invalid width / height.
 	 * Ignore those messages. */