0965608e80c9b5864fec2fc27d03e4ebd3a224b6
Martin Haimberger <martin.haimberger@thincast.com>
2015-06-08 08:51:01 -0700

    channel printer hardend

--- channels/printer/client/printer_main.c
+++ channels/printer/client/printer_main.c
@@ -3,6 +3,8 @@
  * Print Virtual Channel
  *
  * Copyright 2010-2011 Vic Lee
+ * Copyright 2015 Thincast Technologies GmbH
+ * Copyright 2015 DI (FH) Martin Haimberger <martin.haimberger@thincast.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,6 +45,10 @@
 #include "printer_win.h"
 #endif
 
+#include <freerdp/channels/log.h>
+
+#define TAG CHANNELS_TAG("printer.client")
+
 typedef struct _PRINTER_DEVICE PRINTER_DEVICE;
 struct _PRINTER_DEVICE
 {
@@ -58,7 +64,7 @@ struct _PRINTER_DEVICE
 	HANDLE thread;
 };
 
-static void printer_process_irp_create(PRINTER_DEVICE* printer_dev, IRP* irp)
+static WIN32ERROR printer_process_irp_create(PRINTER_DEVICE* printer_dev, IRP* irp)
 {
 	rdpPrintJob* printjob = NULL;
 
@@ -75,10 +81,10 @@ static void printer_process_irp_create(PRINTER_DEVICE* printer_dev, IRP* irp)
 		irp->IoStatus = STATUS_PRINT_QUEUE_FULL;
 	}
 
-	irp->Complete(irp);
+	return irp->Complete(irp);
 }
 
-static void printer_process_irp_close(PRINTER_DEVICE* printer_dev, IRP* irp)
+static WIN32ERROR printer_process_irp_close(PRINTER_DEVICE* printer_dev, IRP* irp)
 {
 	rdpPrintJob* printjob = NULL;
 
@@ -96,14 +102,15 @@ static void printer_process_irp_close(PRINTER_DEVICE* printer_dev, IRP* irp)
 
 	Stream_Zero(irp->output, 4); /* Padding(4) */
 
-	irp->Complete(irp);
+	return irp->Complete(irp);
 }
 
-static void printer_process_irp_write(PRINTER_DEVICE* printer_dev, IRP* irp)
+static WIN32ERROR printer_process_irp_write(PRINTER_DEVICE* printer_dev, IRP* irp)
 {
 	UINT32 Length;
 	UINT64 Offset;
 	rdpPrintJob* printjob = NULL;
+	WIN32ERROR error = CHANNEL_RC_OK;
 
 	Stream_Read_UINT32(irp->input, Length);
 	Stream_Read_UINT64(irp->input, Offset);
@@ -119,46 +126,70 @@ static void printer_process_irp_write(PRINTER_DEVICE* printer_dev, IRP* irp)
 	}
 	else
 	{
-		printjob->Write(printjob, Stream_Pointer(irp->input), Length);
+		error = printjob->Write(printjob, Stream_Pointer(irp->input), Length);
+	}
+
+	if (error)
+	{
+		WLog_ERR(TAG, "printjob->Write failed with error %lu!", error);
+		return error;
 	}
 
 	Stream_Write_UINT32(irp->output, Length);
 	Stream_Write_UINT8(irp->output, 0); /* Padding */
 
-	irp->Complete(irp);
+	return irp->Complete(irp);
 }
 
-static void printer_process_irp_device_control(PRINTER_DEVICE* printer_dev, IRP* irp)
+static WIN32ERROR printer_process_irp_device_control(PRINTER_DEVICE* printer_dev, IRP* irp)
 {
 	Stream_Write_UINT32(irp->output, 0); /* OutputBufferLength */
-	irp->Complete(irp);
+	return irp->Complete(irp);
 }
 
-static void printer_process_irp(PRINTER_DEVICE* printer_dev, IRP* irp)
+static WIN32ERROR printer_process_irp(PRINTER_DEVICE* printer_dev, IRP* irp)
 {
+	WIN32ERROR error;
 	switch (irp->MajorFunction)
 	{
 		case IRP_MJ_CREATE:
-			printer_process_irp_create(printer_dev, irp);
+			if ((error = printer_process_irp_create(printer_dev, irp)))
+			{
+				WLog_ERR(TAG, "printer_process_irp_create failed with error %lu!", error);
+				return error;
+			}
 			break;
 
 		case IRP_MJ_CLOSE:
-			printer_process_irp_close(printer_dev, irp);
+			if ((error = printer_process_irp_close(printer_dev, irp)))
+			{
+				WLog_ERR(TAG, "printer_process_irp_close failed with error %lu!", error);
+				return error;
+			}
 			break;
 
 		case IRP_MJ_WRITE:
-			printer_process_irp_write(printer_dev, irp);
+			if ((error = printer_process_irp_write(printer_dev, irp)))
+			{
+				WLog_ERR(TAG, "printer_process_irp_write failed with error %lu!", error);
+				return error;
+			}
 			break;
 
 		case IRP_MJ_DEVICE_CONTROL:
-			printer_process_irp_device_control(printer_dev, irp);
+			if ((error = printer_process_irp_device_control(printer_dev, irp)))
+			{
+				WLog_ERR(TAG, "printer_process_irp_device_control failed with error %lu!", error);
+				return error;
+			}
 			break;
 
 		default:
 			irp->IoStatus = STATUS_NOT_SUPPORTED;
-			irp->Complete(irp);
+			return irp->Complete(irp);
 			break;
 	}
+	return CHANNEL_RC_OK;
 }
 
 static void* printer_thread_func(void* arg)
@@ -166,6 +197,7 @@ static void* printer_thread_func(void* arg)
 	IRP* irp;
 	PRINTER_DEVICE* printer_dev = (PRINTER_DEVICE*) arg;
 	HANDLE obj[] = {printer_dev->event, printer_dev->stopEvent};
+	WIN32ERROR error = CHANNEL_RC_OK;
 
 	while (1)
 	{
@@ -181,23 +213,32 @@ static void* printer_thread_func(void* arg)
 		irp = (IRP*) InterlockedPopEntrySList(printer_dev->pIrpList);
 
 		if (irp == NULL)
+		{
+			WLog_ERR(TAG, "InterlockedPopEntrySList failed!");
+			error = ERROR_INTERNAL_ERROR;
 			break;
+		}
 
-		printer_process_irp(printer_dev, irp);
+		if ((error = printer_process_irp(printer_dev, irp)))
+		{
+			WLog_ERR(TAG, "printer_process_irp failed with error %d!", error);
+			break;
+		}
 	}
 
-	ExitThread(0);
+	ExitThread((DWORD) error);
 
 	return NULL;
 }
 
-static void printer_irp_request(DEVICE* device, IRP* irp)
+static WIN32ERROR printer_irp_request(DEVICE* device, IRP* irp)
 {
 	PRINTER_DEVICE* printer_dev = (PRINTER_DEVICE*) device;
 
 	InterlockedPushEntrySList(printer_dev->pIrpList, &(irp->ItemEntry));
 
 	SetEvent(printer_dev->event);
+	return CHANNEL_RC_OK;
 }
 
 static void printer_free(DEVICE* device)
@@ -225,7 +266,7 @@ static void printer_free(DEVICE* device)
 	free(printer_dev);
 }
 
-void printer_register(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints, rdpPrinter* printer)
+WIN32ERROR printer_register(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints, rdpPrinter* printer)
 {
 	char* port;
 	UINT32 Flags;
@@ -236,12 +277,23 @@ void printer_register(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints, rdpPrinter* pri
 	UINT32 CachedFieldsLen;
 	BYTE* CachedPrinterConfigData;
 	PRINTER_DEVICE* printer_dev;
+	WIN32ERROR error;
 
 	port = malloc(10);
+	if (!port)
+	{
+		WLog_ERR(TAG, "malloc failed!");
+		return CHANNEL_RC_NO_MEMORY;
+	}
 	snprintf(port, 10, "PRN%d", printer->id);
 
-	printer_dev = (PRINTER_DEVICE*) malloc(sizeof(PRINTER_DEVICE));
-	ZeroMemory(printer_dev, sizeof(PRINTER_DEVICE));
+	printer_dev = (PRINTER_DEVICE*) calloc(1, sizeof(PRINTER_DEVICE));
+	if (!printer_dev)
+	{
+		WLog_ERR(TAG, "calloc failed!");
+		free(port);
+		return CHANNEL_RC_NO_MEMORY;
+	}
 
 	printer_dev->device.type = RDPDR_DTYP_PRINT;
 	printer_dev->device.name = port;
@@ -262,6 +314,14 @@ void printer_register(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints, rdpPrinter* pri
 	PrintNameLen = ConvertToUnicode(CP_UTF8, 0, printer->name, -1, &PrintName, 0) * 2;
 
 	printer_dev->device.data = Stream_New(NULL, 28 + DriverNameLen + PrintNameLen + CachedFieldsLen);
+	if (!printer_dev->device.data)
+	{
+		WLog_ERR(TAG, "calloc failed!");
+		error = CHANNEL_RC_NO_MEMORY;
+		free(DriverName);
+		free(PrintName);
+		goto error_out;
+	}
 
 	Stream_Write_UINT32(printer_dev->device.data, Flags);
 	Stream_Write_UINT32(printer_dev->device.data, 0); /* CodePage, reserved */
@@ -283,22 +343,57 @@ void printer_register(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints, rdpPrinter* pri
 	free(PrintName);
 
 	printer_dev->pIrpList = (PSLIST_HEADER) _aligned_malloc(sizeof(SLIST_HEADER), MEMORY_ALLOCATION_ALIGNMENT);
+	if (!printer_dev->pIrpList)
+	{
+		WLog_ERR(TAG, "_aligned_malloc failed!");
+		error = CHANNEL_RC_NO_MEMORY;
+		goto error_out;
+	}
 	InitializeSListHead(printer_dev->pIrpList);
 
-	printer_dev->event = CreateEvent(NULL, TRUE, FALSE, NULL);
-	printer_dev->stopEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	if (!(printer_dev->event = CreateEvent(NULL, TRUE, FALSE, NULL)))
+	{
+		WLog_ERR(TAG, "CreateEvent failed!");
+		error = ERROR_INTERNAL_ERROR;
+		goto error_out;
+	}
+	if (!(printer_dev->stopEvent = CreateEvent(NULL, TRUE, FALSE, NULL)))
+	{
+		WLog_ERR(TAG, "CreateEvent failed!");
+		error = ERROR_INTERNAL_ERROR;
+		goto error_out;
+	}
 
-	pEntryPoints->RegisterDevice(pEntryPoints->devman, (DEVICE*) printer_dev);
+	if ((error = pEntryPoints->RegisterDevice(pEntryPoints->devman, (DEVICE*) printer_dev)))
+	{
+		WLog_ERR(TAG, "RegisterDevice failed with error %d!", error);
+		goto error_out;
+	}
 
-	printer_dev->thread = CreateThread(NULL, 0,
-			(LPTHREAD_START_ROUTINE) printer_thread_func, (void*) printer_dev, 0, NULL);
+	if (!(printer_dev->thread = CreateThread(NULL, 0,
+			(LPTHREAD_START_ROUTINE) printer_thread_func, (void*) printer_dev, 0, NULL)))
+	{
+		WLog_ERR(TAG, "CreateThread failed!");
+		error = ERROR_INTERNAL_ERROR;
+		goto error_out;
+	}
+
+	return CHANNEL_RC_OK;
+error_out:
+	CloseHandle(printer_dev->stopEvent);
+	CloseHandle(printer_dev->event);
+	_aligned_free(printer_dev->pIrpList);
+	Stream_Free(printer_dev->device.data, TRUE);
+	free(printer_dev);
+	free(port);
+	return error;
 }
 
 #ifdef STATIC_CHANNELS
 #define DeviceServiceEntry	printer_DeviceServiceEntry
 #endif
 
-int DeviceServiceEntry(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints)
+WIN32ERROR DeviceServiceEntry(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints)
 {
 	int i;
 	char* name;
@@ -307,6 +402,7 @@ int DeviceServiceEntry(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints)
 	rdpPrinter** printers;
 	RDPDR_PRINTER* device;
 	rdpPrinterDriver* driver = NULL;
+	WIN32ERROR error;
 
 #ifdef WITH_CUPS
 	driver = printer_cups_get_driver();
@@ -318,7 +414,8 @@ int DeviceServiceEntry(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints)
 
 	if (!driver)
 	{
-		return 1;
+		WLog_ERR(TAG, "Could not get a printer driver!");
+		return CHANNEL_RC_INITIALIZATION_ERROR;
 	}
 
 	device = (RDPDR_PRINTER*) pEntryPoints->device;
@@ -330,12 +427,19 @@ int DeviceServiceEntry(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints)
 		printer = driver->GetPrinter(driver, name);
 
 		if (!printer)
-			return 1;
+		{
+			WLog_ERR(TAG, "Could not get printer %s!", name);
+			return CHANNEL_RC_INITIALIZATION_ERROR;
+		}
 
 		if (driver_name && driver_name[0])
 			printer->driver = driver_name;
 
-		printer_register(pEntryPoints, printer);
+		if ((error = printer_register(pEntryPoints, printer)))
+		{
+			WLog_ERR(TAG, "printer_register failed with error %lu!", error);
+			return error;
+		}
 	}
 	else
 	{
@@ -344,11 +448,17 @@ int DeviceServiceEntry(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints)
 		for (i = 0; printers[i]; i++)
 		{
 			printer = printers[i];
-			printer_register(pEntryPoints, printer);
+			if ((error = printer_register(pEntryPoints, printer)))
+			{
+				WLog_ERR(TAG, "printer_register failed with error %lu!", error);
+				free(printers);
+				return error;
+
+			}
 		}
 
 		free(printers);
 	}
 
-	return 0;
+	return CHANNEL_RC_OK;
 }
