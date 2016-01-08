6fa36081112c46951096cddfacc0f760e4728b25
Bernhard Miklautz <bernhard.miklautz@thincast.com>
2016-01-08 14:07:35 +0100

    cleanup cmake exports and pkg-config files
    With this commit the "exported" components (usable with pkg-config and
    cmake find module package)
    * winpr - winpr library and headers
    * freerdp - core library and headers
    * freerdp-client - client specific library
    * freerdp-server - server specific library
    * rdtk - rdtk headers and library
    
    To allow the installation of multiple different version (different major
    number) the include files were moved into the respective sub folder:
    freerdp -> freerdp{MAJOR}/freerdp (currently freerdp2/freerdp/)
    winpr -> winpr{MAJOR}/winpr (currently winrp1/winpr/)
    rdtk -> rdpk{MAJOR}/rdtk (currently rdtk0/rdtk/
    
    The generated pkg-config and cmake find modules now also include the major
    version number. Currently the following pkg-config are generated and
    installed.
    * winpr1
    * freerdp2
    * freerdp-server2
    * freerdp-client2
    * rdtk0
    
    As cmake is able to handle multiple versions out of the box the
    following can be used to find a specific module:
    
    find_package(WinPR)
    find_package(FreeRDP)
    find_package(FreeRDP-Server)
    find_package(FreeRDP-Client)
    find_package(RdTk)
    
    As cmake doesn't automatically resolve dependencies for packages it is
    necessary to manually include the requirements. For example if
    FreeRDP-Client is required WinPR and FreeRDP need to be included
    (find_package) as well.
    
    This commit also fixes the installation when STATIC_CHANNELS are built.
    WITH STATIC_CHANNELS all channels are linked into libfreerdp-client, for
    this all channels are generated as linker archive and linked together in
    the final step. Before the intermediate linker archives were, although
    not required and useful, installed. Same applies for server side
    channels.

--- channels/server/channels.c
+++ channels/server/channels.c
@@ -47,6 +47,8 @@
 #include <freerdp/server/rdpdr.h>
 #include <freerdp/server/rdpei.h>
 #include <freerdp/server/drdynvc.h>
+#include <freerdp/server/remdesk.h>
+#include <freerdp/server/encomsp.h>
 
 void freerdp_channels_dummy() 
 {
@@ -70,6 +72,13 @@ void freerdp_channels_dummy()
 
 	rdpei_server_context_new(NULL);
 	rdpei_server_context_free(NULL);
+
+	remdesk_server_context_new(NULL);
+	remdesk_server_context_free(NULL);
+
+	encomsp_server_context_new(NULL);
+	encomsp_server_context_free(NULL);
+
 }
 
 /**
