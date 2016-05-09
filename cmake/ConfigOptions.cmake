2c0e73b0751d1b8299a352f0ef57c88992401b9a
Bernhard Miklautz <bernhard.miklautz@thincast.com>
2016-05-09 10:25:31 +0200

    client: disable the client interface per default
    libxfreerdp-client doesn't necessarily provide a stable interface
    therefore it isn't built and installed anymore per default.
    
    To archive the same behavior as before this change -
    libxfreerdp-client.so built and a library version set - use
    -DWITH_CLIENT_INTERFACE=ON -DCLIENT_INTERFACE_SHARED=ON
    when running cmake.
    
    This also fixes the build on windows without WITH_CLIENT_INTERFACE
    enabled.

--- cmake/ConfigOptions.cmake
+++ cmake/ConfigOptions.cmake
@@ -81,7 +81,7 @@ endif()
 
 option(WITH_THIRD_PARTY "Build third-party components" OFF)
 
-option(WITH_CLIENT_INTERFACE "Build clients as a library with an interface" ON)
+option(WITH_CLIENT_INTERFACE "Build clients as a library with an interface" OFF)
 option(WITH_SERVER_INTERFACE "Build servers as a library with an interface" ON)
 
 option(WITH_DEBUG_ALL "Print all debug messages." OFF)
