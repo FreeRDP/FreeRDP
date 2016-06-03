341eed138f97304801ac84f6d70ac33b080c3298
Norbert Federa <norbert.federa@thincast.com>
2016-06-03 18:56:36 +0200

    winpr/synch: rewrite barrier implementation & test
    The synchronization barrier test as well as the actual WinPR
    implementation were completely broken.

--- winpr/libwinpr/synch/test/TestSynchBarrier.c
+++ winpr/libwinpr/synch/test/TestSynchBarrier.c
@@ -2,123 +2,142 @@
 #include <winpr/crt.h>
 #include <winpr/synch.h>
 #include <winpr/thread.h>
+#include <winpr/interlocked.h>
 
 #include "../synch.h"
 
-static int g_Count;
-static HANDLE g_Event;
-static CRITICAL_SECTION g_Lock;
-static SYNCHRONIZATION_BARRIER g_Barrier;
+static SYNCHRONIZATION_BARRIER gBarrier;
+static HANDLE gStartEvent = NULL;
 
-static void* test_synch_barrier_thread_func(void* arg)
+static LONG gThreadCount = 0;
+static LONG gTrueCount   = 0;
+static LONG gFalseCount  = 0;
+static LONG gErrorCount  = 0;
+
+#define LOOP_COUNT      200
+#define THREAD_COUNT    32
+#define MAX_SLEEP_MS    16
+
+#define EXPECTED_TRUE_COUNT  LOOP_COUNT
+#define EXPECTED_FALSE_COUNT (LOOP_COUNT * (THREAD_COUNT - 1))
+
+
+DWORD WINAPI test_synch_barrier_thread(LPVOID lpParam)
 {
-	BOOL status;
-  int count;
+	BOOL status = FALSE;
+	DWORD i, tnum = InterlockedIncrement(&gThreadCount) - 1;
 
-	EnterCriticalSection(&g_Lock);
-  count = g_Count++;
-	LeaveCriticalSection(&g_Lock);
-	status = EnterSynchronizationBarrier(&g_Barrier, 0);
+	printf("Thread #%03u entered.\n", tnum);
 
-	printf("Thread #%d status: %s\n", count,
-			status ? "TRUE" : "FALSE");
-
-	if (status)
+	/* wait for start event from main */
+	if (WaitForSingleObject(gStartEvent, INFINITE) != WAIT_OBJECT_0)
 	{
-		SetEvent(g_Event);
+		InterlockedIncrement(&gErrorCount);
+		goto out;
 	}
 
-	return NULL;
+	printf("Thread #%03u unblocked.\n", tnum);
+
+	for (i = 0; i < LOOP_COUNT && gErrorCount == 0; i++)
+	{
+		/* simulate different execution times before the barrier */
+		Sleep(rand() % MAX_SLEEP_MS);
+		status = EnterSynchronizationBarrier(&gBarrier, 0);
+		//printf("Thread #%03u status: %s\n", tnum, status ? "TRUE" : "FALSE");
+		if (status)
+			InterlockedIncrement(&gTrueCount);
+		else
+			InterlockedIncrement(&gFalseCount);
+	}
+out:
+	printf("Thread #%03u leaving.\n", tnum);
+	return 0;
 }
 
-static void* barrier_deleter_thread_func(void* arg)
-{
-	/* Blocks until all threads are released from the barrier. */
-	DeleteSynchronizationBarrier(&g_Barrier);
-
-	return NULL;
-}
 
 int TestSynchBarrier(int argc, char* argv[])
 {
-	int index;
-	HANDLE threads[5];
-	HANDLE deleter_thread = NULL;
+	HANDLE threads[THREAD_COUNT];
+	DWORD dwStatus;
+	int i;
 
-	g_Count = 0;
-
-	if (!(g_Event = CreateEvent(NULL, TRUE, FALSE, NULL)))
+	/* Test invalid parameters */
+	if (InitializeSynchronizationBarrier(&gBarrier, 0, -1))
 	{
-		printf("%s: CreateEvent failed. GetLastError() = 0x%08x", __FUNCTION__, GetLastError());
+		printf("%s: InitializeSynchronizationBarrier unecpectedly succeeded with lTotalThreads = 0\n", __FUNCTION__);
 		return -1;
 	}
 
-	if (!InitializeCriticalSectionAndSpinCount(&g_Lock, 4000))
+	if (InitializeSynchronizationBarrier(&gBarrier, -1, -1))
 	{
-		printf("%s: InitializeCriticalSectionAndSpinCount failed. GetLastError() = 0x%08x", __FUNCTION__, GetLastError());
-		CloseHandle(g_Event);
+		printf("%s: InitializeSynchronizationBarrier unecpectedly succeeded with lTotalThreads = -1\n", __FUNCTION__);
 		return -1;
 	}
 
-	if (!InitializeSynchronizationBarrier(&g_Barrier, 5, -1))
+	if (InitializeSynchronizationBarrier(&gBarrier, 1, -2))
+	{
+		printf("%s: InitializeSynchronizationBarrier unecpectedly succeeded with lSpinCount = -2\n", __FUNCTION__);
+		return -1;
+	}
+
+
+	/* Functional test */
+	if (!InitializeSynchronizationBarrier(&gBarrier, THREAD_COUNT, -1))
 	{
 		printf("%s: InitializeSynchronizationBarrier failed. GetLastError() = 0x%08x", __FUNCTION__, GetLastError());
-		DeleteCriticalSection(&g_Lock);
-		CloseHandle(g_Event);
+		DeleteSynchronizationBarrier(&gBarrier);
 		return -1;
 	}
 
-	for (index = 0; index < 5; index++)
+	if (!(gStartEvent = CreateEvent(NULL, TRUE, FALSE, NULL)))
 	{
-		if (!(threads[index] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)
-				test_synch_barrier_thread_func, NULL, 0, NULL)))
-		{
-			printf("%s: CreateThread failed for thread #%d. GetLastError() = 0x%08x\n", __FUNCTION__, index, GetLastError());
-			while (index)
-				CloseHandle(threads[--index]);
-			CloseHandle(deleter_thread);
-			DeleteCriticalSection(&g_Lock);
-			CloseHandle(g_Event);
-			return -1;
-		}
-
-		if (index == 0)
-		{
-			/* Make sure first thread has already entered the barrier... */
-			while (((WINPR_BARRIER*) g_Barrier.Reserved3[0])->count == 0)
-				Sleep(100);
-
-			/* Now spawn the deleter thread. */
-			if (!(deleter_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)
-					barrier_deleter_thread_func, NULL, 0, NULL)))
-			{
-				printf("%s: CreateThread failed for deleter thread. GetLastError() = 0x%08x\n", __FUNCTION__, GetLastError());
-				while (index)
-					CloseHandle(threads[--index]);
-				DeleteCriticalSection(&g_Lock);
-				CloseHandle(g_Event);
-				return -1;
-			}
-		}
-	}
-
-	WaitForSingleObject(g_Event, INFINITE);
-
-	if (g_Count != 5)
+		printf("%s: CreateEvent failed with error 0x%08x", __FUNCTION__, GetLastError());
+		DeleteSynchronizationBarrier(&gBarrier);
 		return -1;
-
-	printf("All threads have reached the barrier\n");
-
-	for (index = 0; index < 5; index++)
-	{
-		CloseHandle(threads[index]);
 	}
 
-	CloseHandle(deleter_thread);
+	for (i = 0; i < THREAD_COUNT; i++)
+	{
+		if (!(threads[i] = CreateThread(NULL, 0, test_synch_barrier_thread, NULL, 0, NULL)))
+		{
+			printf("%s: CreateThread failed for thread #%u with error 0x%08x\n", __FUNCTION__, i, GetLastError());
+			InterlockedIncrement(&gErrorCount);
+			break;
+		}
+	}
 
-	DeleteCriticalSection(&g_Lock);
+	if (i > 0)
+	{
+		SetEvent(gStartEvent);
 
-	CloseHandle(g_Event);
+		if (WAIT_OBJECT_0 != (dwStatus = WaitForMultipleObjects(i, threads, TRUE, INFINITE)))
+		{
+			printf("%s: WaitForMultipleObjects unexpectedly returned %u (error = 0x%08x)\n",
+				__FUNCTION__, dwStatus, GetLastError());
+			gErrorCount++;
+		}
+	}
+
+	CloseHandle(gStartEvent);
+	DeleteSynchronizationBarrier(&gBarrier);
+
+	if (gTrueCount != EXPECTED_TRUE_COUNT)
+		InterlockedIncrement(&gErrorCount);
+
+	if (gFalseCount != EXPECTED_FALSE_COUNT)
+		InterlockedIncrement(&gErrorCount);
+
+	printf("%s: gErrorCount: %d (expected 0)\n", __FUNCTION__, gErrorCount);
+	printf("%s: gTrueCount:  %d (expected %d)\n", __FUNCTION__, gTrueCount, LOOP_COUNT);
+	printf("%s: gFalseCount: %d (expected %d)\n", __FUNCTION__, gFalseCount, LOOP_COUNT * (THREAD_COUNT - 1));
+
+	if (gErrorCount > 0)
+	{
+		printf("%s: Error test failed with %d reported errors\n", __FUNCTION__, gErrorCount);
+		return -1;
+	}
+
+	printf("%s: Test successfully completed\n", __FUNCTION__);
 
 	return 0;
 }
