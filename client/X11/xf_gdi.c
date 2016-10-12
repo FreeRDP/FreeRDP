25c7372fe68508b0baed79936e1d0fd9487a7667
Armin Novak <armin.novak@thincast.com>
2016-10-12 09:32:05 +0200

    Fixed offset and stride for X11 surface bits.

--- client/X11/xf_gdi.c
+++ client/X11/xf_gdi.c
@@ -984,11 +984,16 @@ static BOOL xf_gdi_update_screen(xfContext* xfc,
 	XSetFillStyle(xfc->display, xfc->gc, FillSolid);
 	image = XCreateImage(xfc->display, xfc->visual, xfc->depth, ZPixmap, 0,
 	                     (char*) pSrcData, cmd->width, cmd->height, xfc->scanline_pad, 0);
-	XPutImage(xfc->display, xfc->primary, xfc->gc, image, 0, 0,
-	          cmd->destLeft, cmd->destTop, cmd->width, cmd->height);
-	XFree(image);
-	ret = xf_gdi_surface_update_frame(xfc, cmd->destLeft, cmd->destTop, cmd->width,
-	                                  cmd->height);
+
+	if (image)
+	{
+		XPutImage(xfc->display, xfc->primary, xfc->gc, image, 0, 0,
+		          cmd->destLeft, cmd->destTop, cmd->width, cmd->height);
+		XFree(image);
+		ret = xf_gdi_surface_update_frame(xfc, cmd->destLeft, cmd->destTop, cmd->width,
+		                                  cmd->height);
+	}
+
 	XSetClipMask(xfc->display, xfc->gc, None);
 	return ret;
 }
@@ -1000,7 +1005,14 @@ static BOOL xf_gdi_surface_bits(rdpContext* context,
 	xfContext* xfc = (xfContext*) context;
 	BOOL ret = FALSE;
 	DWORD format;
-	rdpGdi* gdi = context->gdi;
+	DWORD stride;
+	rdpGdi* gdi;
+
+	if (!context || !cmd || !context->gdi)
+		return FALSE;
+
+	gdi = context->gdi;
+	stride = cmd->width * GetBytesPerPixel(gdi->dstFormat);
 	xf_lock_x11(xfc, FALSE);
 
 	switch (cmd->codecID)
@@ -1010,12 +1022,11 @@ static BOOL xf_gdi_surface_bits(rdpContext* context,
 
 			if (!rfx_process_message(context->codecs->rfx, cmd->bitmapData,
 			                         format, cmd->bitmapDataLength,
-			                         cmd->destLeft, cmd->destTop,
-			                         gdi->primary_buffer, gdi->dstFormat, gdi->stride,
+			                         0, 0,
+			                         gdi->primary_buffer, gdi->dstFormat, stride,
 			                         gdi->height, NULL))
 				goto fail;
 
-			ret = xf_gdi_update_screen(xfc, cmd, gdi->primary_buffer);
 			break;
 
 		case RDP_CODEC_ID_NSCODEC:
@@ -1023,32 +1034,31 @@ static BOOL xf_gdi_surface_bits(rdpContext* context,
 
 			if (!nsc_process_message(context->codecs->nsc, cmd->bpp, cmd->width,
 			                         cmd->height, cmd->bitmapData, cmd->bitmapDataLength,
-			                         gdi->primary_buffer, format, gdi->stride,
-			                         cmd->destLeft, cmd->destTop, cmd->width, cmd->height))
+			                         gdi->primary_buffer, format, stride,
+			                         0, 0, cmd->width, cmd->height))
 				goto fail;
 
-			ret = xf_gdi_update_screen(xfc, cmd, gdi->primary_buffer);
 			break;
 
 		case RDP_CODEC_ID_NONE:
 			pSrcData = cmd->bitmapData;
 			format = PIXEL_FORMAT_BGRX32_VF;
 
-			if (!freerdp_image_copy(gdi->primary_buffer, gdi->dstFormat, gdi->stride,
-			                        cmd->destLeft, cmd->destTop,
+			if (!freerdp_image_copy(gdi->primary_buffer, gdi->dstFormat, stride,
+			                        0, 0,
 			                        cmd->width, cmd->height, pSrcData,
 			                        format, 0, 0, 0, &xfc->context.gdi->palette))
 				goto fail;
 
-			ret = xf_gdi_update_screen(xfc, cmd, gdi->primary_buffer);
 			break;
 
 		default:
 			WLog_ERR(TAG, "Unsupported codecID %d", cmd->codecID);
 			ret = TRUE;
-			break;
+			goto fail;
 	}
 
+	ret = xf_gdi_update_screen(xfc, cmd, gdi->primary_buffer);
 fail:
 	xf_unlock_x11(xfc, FALSE);
 	return ret;
