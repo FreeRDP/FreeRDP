43755c28eb9df6c0fbcf868606dd01b276512492
Armin Novak <armin.novak@thincast.com>
2016-10-17 10:24:30 +0200

    Android: fixed visibility, reformatted

--- client/Android/android_event.h
+++ client/Android/android_event.h
@@ -12,6 +12,7 @@
 #ifndef FREERDP_ANDROID_EVENT_H
 #define FREERDP_ANDROID_EVENT_H
 #include <freerdp/freerdp.h>
+#include <freerdp/api.h>
 
 #define EVENT_TYPE_KEY			1
 #define EVENT_TYPE_CURSOR		2
@@ -55,24 +56,27 @@ struct _ANDROID_EVENT_QUEUE
 	int size;
 	int count;
 	HANDLE isSet;
-	ANDROID_EVENT **events;
+	ANDROID_EVENT** events;
 };
 typedef struct _ANDROID_EVENT_QUEUE ANDROID_EVENT_QUEUE;
 
-BOOL android_push_event(freerdp * inst, ANDROID_EVENT* event);
+FREERDP_LOCAL BOOL android_push_event(freerdp* inst, ANDROID_EVENT* event);
 
-HANDLE android_get_handle(freerdp* inst);
-BOOL android_check_handle(freerdp * inst);
+FREERDP_LOCAL HANDLE android_get_handle(freerdp* inst);
+FREERDP_LOCAL BOOL android_check_handle(freerdp* inst);
 
-ANDROID_EVENT_KEY* android_event_key_new(int flags, UINT16 scancode);
-ANDROID_EVENT_KEY* android_event_unicodekey_new(UINT16 key);
-ANDROID_EVENT_CURSOR* android_event_cursor_new(UINT16 flags, UINT16 x, UINT16 y);
-ANDROID_EVENT* android_event_disconnect_new(void);
-ANDROID_EVENT_CLIPBOARD* android_event_clipboard_new(void* data, int data_length);
+FREERDP_LOCAL ANDROID_EVENT_KEY* android_event_key_new(int flags,
+        UINT16 scancode);
+FREERDP_LOCAL ANDROID_EVENT_KEY* android_event_unicodekey_new(UINT16 key);
+FREERDP_LOCAL ANDROID_EVENT_CURSOR* android_event_cursor_new(UINT16 flags,
+        UINT16 x, UINT16 y);
+FREERDP_LOCAL ANDROID_EVENT* android_event_disconnect_new(void);
+FREERDP_LOCAL ANDROID_EVENT_CLIPBOARD* android_event_clipboard_new(void* data,
+        int data_length);
 
-void android_event_free(ANDROID_EVENT* event);
+FREERDP_LOCAL void android_event_free(ANDROID_EVENT* event);
 
-BOOL android_event_queue_init(freerdp * inst);
-void android_event_queue_uninit(freerdp * inst);
+FREERDP_LOCAL BOOL android_event_queue_init(freerdp* inst);
+FREERDP_LOCAL void android_event_queue_uninit(freerdp* inst);
 
 #endif /* FREERDP_ANDROID_EVENT_H */
