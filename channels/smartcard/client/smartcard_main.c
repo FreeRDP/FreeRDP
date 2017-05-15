03723c27380806206d0007308f6ec150ac7fec7b
dodo040 <dorian.ducournau@gmail.com>
2017-05-15 17:17:08 +0200

    fix wrong declaration

--- channels/smartcard/client/smartcard_main.c
+++ channels/smartcard/client/smartcard_main.c
@@ -695,6 +695,7 @@ UINT DeviceServiceEntry(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints)
 	int ck;
 	RDPDR_SMARTCARD* device;
 	SMARTCARD_DEVICE* smartcard;
+	LONG status;
 	UINT error = CHANNEL_RC_NO_MEMORY;
 	device = (RDPDR_SMARTCARD*) pEntryPoints->device;
 	name = device->Name;
@@ -739,7 +740,6 @@ UINT DeviceServiceEntry(PDEVICE_SERVICE_ENTRY_POINTS pEntryPoints)
 			smartcard->name = name;
 	}
 
-	LONG status;
 	status = SCardAddReaderName(&smartcard->thread, (LPSTR) name);
 
 	if (status != SCARD_S_SUCCESS)
