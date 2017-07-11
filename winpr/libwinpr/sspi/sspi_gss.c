695043599675bc424e84b294ee3c215ca18ea4c2
dodo040 <dorian.ducournau@gmail.com>
2017-07-11 17:32:28 +0200

    use SSIZE_T instead of ssize_t

--- winpr/libwinpr/sspi/sspi_gss.c
+++ winpr/libwinpr/sspi/sspi_gss.c
@@ -976,7 +976,7 @@ UINT32 SSPI_GSSAPI sspi_gss_pseudo_random(
     sspi_gss_ctx_id_t context,
     int prf_key,
     const sspi_gss_buffer_t prf_in,
-    ssize_t desired_output_len,
+    SSIZE_T desired_output_len,
     sspi_gss_buffer_t prf_out)
 {
 	SECURITY_STATUS status;
