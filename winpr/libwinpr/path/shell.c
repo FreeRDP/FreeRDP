ee7b5460b5e2cc88cda3d4f3b7222313dbe3aa5a
Richard Markiewicz <rmarkiewicz@devolutions.net>
2017-10-06 14:55:34 -0700

    freerdp - fix iOS directory paths

--- winpr/libwinpr/path/shell.c
+++ winpr/libwinpr/path/shell.c
@@ -117,7 +117,7 @@ static char* GetPath_XDG_DATA_HOME(void)
 {
 	size_t size;
 	char* path = NULL;
-#if defined(WIN32)
+#if defined(WIN32) || defined(__IOS__)
 	path = GetPath_XDG_CONFIG_HOME();
 #else
 	char* home = NULL;
@@ -170,7 +170,7 @@ static char* GetPath_XDG_CONFIG_HOME(void)
 	}
 
 #elif defined(__IOS__)
-	path = GetCombinedPath(GetPath_HOME(), ".freerdp");
+	path = ios_get_data();
 #else
 	char* home = NULL;
 	/**
@@ -226,6 +226,8 @@ static char* GetPath_XDG_CACHE_HOME(void)
 	}
 
 	free(home);
+#elif defined(__IOS__)
+	path = ios_get_cache();
 #else
 	/**
 	 * There is a single base directory relative to which user-specific non-essential (cached) data should be written.
