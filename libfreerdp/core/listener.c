FreeRDP/FreeRDP

commit 884e87fde4ece132c3b7bdf947de044ecb3d4b14

Unlink file after binding to it.
When unlinking the file before binding, a new entry is created
in the file system after binding. This is not desireable, so
unlink it after binding to remove the temporary file after the process
closes.

libfreerdp/core/listener.c
@@ -185,9 +185,9 @@ static BOOL freerdp_listener_open_local(freerdp_listener* instance, const char* 
 
 	addr.sun_family = AF_UNIX;
 	strncpy(addr.sun_path, path, sizeof(addr.sun_path));
-	unlink(path);
 
 	status = _bind(sockfd, (struct sockaddr*) &addr, sizeof(addr));
+	unlink(path);
 
 	if (status != 0)
 	{

commit 02e4f1f256aa45725b839ba56d227b863a3eae61

Do not delete the listener socket right after creating it.
The listener server socket file needs to be deleted before we bind it
otherwise it's an "address already in use" error. But it was getting
deleted after the bind, causing the file to disappear, and preventing
anyone from connecting to the listener socket since the socket stops
existing.

This is caused by commit 884e87fde4ece132c3b7bdf947de044ecb3d4b14

libfreerdp/core/listener.c
@@ -186,8 +186,8 @@ static BOOL freerdp_listener_open_local(freerdp_listener* instance, const char* 
 	addr.sun_family = AF_UNIX;
 	strncpy(addr.sun_path, path, sizeof(addr.sun_path));
 
-	status = _bind(sockfd, (struct sockaddr*) &addr, sizeof(addr));
 	unlink(path);
+	status = _bind(sockfd, (struct sockaddr*) &addr, sizeof(addr));
 
 	if (status != 0)
 	{