b86c0ba5488a4e83e859849f87202fa4c371bce1
Armin Novak <armin.novak@thincast.com>
2017-11-08 11:30:32 +0100

    Fixed NLA default error to FREERDP_ERROR_AUTHENTICATION_FAILED

--- libfreerdp/core/nla.c
+++ libfreerdp/core/nla.c
@@ -1668,7 +1668,7 @@ int nla_recv_pdu(rdpNla* nla, wStream* s)
 
 		    default:
 			    WLog_ERR(TAG, "SPNEGO failed with NTSTATUS: 0x%08"PRIX32"", nla->errorCode);
-				code = FREERDP_ERROR_CONNECT_FAILED;
+				code = FREERDP_ERROR_AUTHENTICATION_FAILED;
 			    break;
 		}
 		freerdp_set_last_error(nla->instance->context, code);
