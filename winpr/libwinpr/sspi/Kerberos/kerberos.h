1611ec16b16932a2d08e200105de55a599a2bcc7
Armin Novak <armin.novak@thincast.com>
2018-01-16 10:58:30 +0100

    Refactored kerberos SSPI * Functions static where approrpriate * Variables static const where appropriate

--- winpr/libwinpr/sspi/Kerberos/kerberos.h
+++ winpr/libwinpr/sspi/Kerberos/kerberos.h
@@ -32,52 +32,6 @@
 #include <gssapi.h>
 #endif
 
-struct _KRB_CONTEXT
-{
-	CtxtHandle context;
-	SSPI_CREDENTIALS* credentials;
-	SEC_WINNT_AUTH_IDENTITY identity;
-
-	/* GSSAPI */
-	UINT32 major_status;
-	UINT32 minor_status;
-	UINT32 actual_time;
-	sspi_gss_cred_id_t cred;
-	sspi_gss_ctx_id_t gss_ctx;
-	sspi_gss_name_t target_name;
-};
 typedef struct _KRB_CONTEXT KRB_CONTEXT;
-
-const SecPkgInfoA KERBEROS_SecPkgInfoA =
-{
-	0x000F3BBF,		/* fCapabilities */
-	1,			/* wVersion */
-	0x0010,			/* wRPCID */
-	0x0000BB80,		/* cbMaxToken : 48k bytes maximum for Windows Server 2012 */
-	"Kerberos",		/* Name */
-	"Kerberos Security Package" /* Comment */
-};
-
-WCHAR KERBEROS_SecPkgInfoW_Name[] = { 'K', 'e', 'r', 'b', 'e', 'r', 'o', 's', '\0' };
-
-WCHAR KERBEROS_SecPkgInfoW_Comment[] =
-{
-	'K', 'e', 'r', 'b', 'e', 'r', 'o', 's', ' ',
-	'S', 'e', 'c', 'u', 'r', 'i', 't', 'y', ' ',
-	'P', 'a', 'c', 'k', 'a', 'g', 'e', '\0'
-};
-
-const SecPkgInfoW KERBEROS_SecPkgInfoW =
-{
-	0x000F3BBF,		/* fCapabilities */
-	1,			/* wVersion */
-	0x0010,			/* wRPCID */
-	0x0000BB80,		/* cbMaxToken : 48k bytes maximum for Windows Server 2012 */
-	KERBEROS_SecPkgInfoW_Name,	/* Name */
-	KERBEROS_SecPkgInfoW_Comment	/* Comment */
-};
-
-
-void krb_ContextFree(KRB_CONTEXT* context);
 
 #endif /* FREERDP_SSPI_KERBEROS_PRIVATE_H */
