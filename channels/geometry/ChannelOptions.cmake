731ef4f8c06e40d857cb595ca9a3544574bdb193
David Fort <contact@hardening-consulting.com>
2018-01-30 11:35:23 +0100

    geometry: drop server-side option for geometry

--- channels/geometry/ChannelOptions.cmake
+++ channels/geometry/ChannelOptions.cmake
@@ -9,4 +9,3 @@ define_channel_options(NAME "geometry" TYPE "dynamic"
 	DEFAULT ${OPTION_DEFAULT})
 
 define_channel_client_options(${OPTION_CLIENT_DEFAULT})
-define_channel_server_options(${OPTION_SERVER_DEFAULT})
