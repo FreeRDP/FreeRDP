2c98d85a345793c36ba1d2b6b9033763f170c712
Armin Novak <armin.novak@thincast.com>
2018-04-03 12:08:01 +0200

    Fixed function pointer typedef formatting.

--- include/freerdp/client/audin.h
+++ include/freerdp/client/audin.h
@@ -30,22 +30,22 @@
  * Subsystem Interface
  */
 
-typedef UINT(*AudinReceive)(const AUDIO_FORMAT* format,
-                            const BYTE* data, size_t size, void* userData);
+typedef UINT (*AudinReceive)(const AUDIO_FORMAT* format,
+                             const BYTE* data, size_t size, void* userData);
 
 typedef struct _IAudinDevice IAudinDevice;
 struct _IAudinDevice
 {
-	UINT(*Open)(IAudinDevice* devplugin, AudinReceive receive, void* userData);
+	UINT (*Open)(IAudinDevice* devplugin, AudinReceive receive, void* userData);
 	BOOL (*FormatSupported)(IAudinDevice* devplugin, const AUDIO_FORMAT* format);
-	UINT(*SetFormat)(IAudinDevice* devplugin, const AUDIO_FORMAT* format, UINT32 FramesPerPacket);
-	UINT(*Close)(IAudinDevice* devplugin);
-	UINT(*Free)(IAudinDevice* devplugin);
+	UINT (*SetFormat)(IAudinDevice* devplugin, const AUDIO_FORMAT* format, UINT32 FramesPerPacket);
+	UINT (*Close)(IAudinDevice* devplugin);
+	UINT (*Free)(IAudinDevice* devplugin);
 };
 
 #define AUDIN_DEVICE_EXPORT_FUNC_NAME "freerdp_audin_client_subsystem_entry"
 
-typedef UINT(*PREGISTERAUDINDEVICE)(IWTSPlugin* plugin, IAudinDevice* device);
+typedef UINT (*PREGISTERAUDINDEVICE)(IWTSPlugin* plugin, IAudinDevice* device);
 
 struct _FREERDP_AUDIN_DEVICE_ENTRY_POINTS
 {
@@ -57,7 +57,7 @@ struct _FREERDP_AUDIN_DEVICE_ENTRY_POINTS
 typedef struct _FREERDP_AUDIN_DEVICE_ENTRY_POINTS FREERDP_AUDIN_DEVICE_ENTRY_POINTS;
 typedef FREERDP_AUDIN_DEVICE_ENTRY_POINTS* PFREERDP_AUDIN_DEVICE_ENTRY_POINTS;
 
-typedef UINT(*PFREERDP_AUDIN_DEVICE_ENTRY)(PFREERDP_AUDIN_DEVICE_ENTRY_POINTS pEntryPoints);
+typedef UINT (*PFREERDP_AUDIN_DEVICE_ENTRY)(PFREERDP_AUDIN_DEVICE_ENTRY_POINTS pEntryPoints);
 
 #endif /* FREERDP_CHANNEL_AUDIN_CLIENT_AUDIN_H */
 
