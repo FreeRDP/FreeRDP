f1e51f69e1c7dd92e851b19fd4794d30cef3657c
Pascal J. Bourguignon <pjb@informatimago.com>
2018-08-21 13:50:59 +0200

    Removed object orientation for WCHAR vs. CHAR strings functions.

--- channels/smartcard/client/str.c
+++ channels/smartcard/client/str.c
@@ -21,41 +21,48 @@
 #include "config.h"
 #endif
 
+#include <string.h>
 #include <winpr/string.h>
-
 #include <freerdp/log.h>
 
 #include "str.h"
 
 #define TAG FREERDP_TAG("str")
 
+/* Note WCHAR are 2-byte characters. wchar_t may be bigger! */
+
+
 WCHAR * towide(BYTE* string)
 {
 	WCHAR * wstring = 0;
-	ConvertToUnicode(CP_UTF8, 0, string, -1, &wstring, 0, NULL, NULL);
+	ConvertToUnicode(CP_UTF8, 0, (LPCSTR)string, -1, (LPWSTR*)&wstring, 0);
 	return wstring;
 }
 
-
-
-int compareA(BYTE* string, BYTE* other_string)
+char*	tochar(WCHAR* string)
 {
-	return strcmp((char *)string, (char *)other_string);
+	char*	utf8 = 0;
+	ConvertFromUnicode(CP_UTF8, 0, string, -1, (CHAR**) &utf8, 0, NULL, NULL);
+	return utf8;
 }
 
-int compareW(BYTE* string, BYTE* other_string)
-{
-	return strcmp((char *)string, (char *)other_string);
-}
 
-int compare(BOOL widechar, BYTE* string, BYTE* other_string)
+int compare(BOOL widechar, void* string, void* other_string)
 {
 	if (widechar)
 	{
 		WCHAR * wother = towide(other_string);
 		if (wother)
 		{
-			int result =  compareW((WCHAR *)string, wother);
+			WCHAR * a = (WCHAR *)string;
+			WCHAR * b = wother;
+			int result;
+			while ((*a != 0) && (*a == *b))
+			{
+				a ++;
+				b ++;
+			}
+			result = (*a == *b)? 0 : ((*a < *b)? -1 : +1);
 			free(wother);
 			return result;
 		}
@@ -66,28 +73,28 @@ int compare(BOOL widechar, BYTE* string, BYTE* other_string)
 	}
 	else
 	{
-		return compareA(string, other_string);
+		int result = strcmp((char *)string, (char *)other_string);
+		return (result == 0)? 0 : ((result < 0)? -1 : +1);
 	}
 }
 
-int ncompareA(BYTE* string, BYTE* other_string, int max)
-{
-	return strncmp((char *)string, (char *)other_string, max);
-}
-
-int ncompareW(WCHAR * string, WCHAR * other_string, int max)
-{
-	return wcsncmp(string, other_string, max;)
-}
-
-int ncompare(BOOL widechar, BYTE* string, BYTE* other_string, int max)
+int ncompare(BOOL widechar, void* string, void* other_string, int max)
 {
 	if (widechar)
 	{
 		WCHAR * wother = towide(other_string);
 		if (wother)
 		{
-			int result = ncompareW((WCHAR *)string, wother, max);
+			WCHAR * a = (WCHAR *)string;
+			WCHAR * b = wother;
+			int result;
+			while ((0 < max) && (*a != 0) && (*a == *b))
+			{
+				a ++;
+				b ++;
+				max --;
+			}
+			result = ((max == 0) || (*a == *b)) ? 0 : ((*a < *b)? -1 : +1);
 			free(wother);
 			return result;
 		}
@@ -98,64 +105,69 @@ int ncompare(BOOL widechar, BYTE* string, BYTE* other_string, int max)
 	}
 	else
 	{
-		return ncompareA(string, other_string, max);
+		int result = strncmp((char *)string, (char *)other_string, max);
+		return (result == 0)? 0 : ((result < 0)? -1 : +1);
 	}
 }
 
-BOOL containsA(BYTE* string, BYTE* substring)
+BOOL contains(BOOL widechar, void* string, char* substring)
 {
-	int wlen = funs->len(string);
-	int slen = strlen((char*)substring);
-	int end = wlen - slen;
-	int i = 0;
-
-	for (i = 0; i <= end; i ++)
+	if (widechar)
 	{
-		if (ncompare(funs, funs->inc(string, i), substring, slen) == 0)
+		BOOL result = false;
+		WCHAR * s = (WCHAR *)string;
+		int l = strlen(substring);
+		int e = lstrlenW((LPCWSTR)string) - l;
+		int i = 0;
+		while (i <= e)
 		{
-			return TRUE;
+			if (ncompare(widechar, s + i, substring, l) == 0)
+			{
+				result = true;
+				break;
+			}
+			i ++ ;
+		}
+		return result;
+	}
+	else
+	{
+		return 0 != strstr((char *)string, (char *)substring);
+	}
+}
+
+
+void ncopy(BOOL widechar, void* destination, void* source, int count)
+{
+	if (widechar)
+	{
+		WCHAR * d = (WCHAR *)destination;
+		WCHAR * s = (WCHAR *)source;
+		while (0 < count)
+		{
+			* d ++  = * s ++ ;
+			count -- ;
 		}
 	}
-
-	return FALSE;
-}
-
-BOOL contains(BOOL widechar, BYTE* string, BYTE* substring)
-{
-	int wlen = funs->len(string);
-	int slen = strlen((char*)substring);
-	int end = wlen - slen;
-	int i = 0;
-
-	for (i = 0; i <= end; i ++)
+	else
 	{
-		if (ncompare(funs, funs->inc(string, i), substring, slen) == 0)
+		char * d = (char *)destination;
+		char * s = (char *)source;
+		while (0 < count)
 		{
-			return TRUE;
+			* d ++  = * s ++ ;
+			count -- ;
 		}
 	}
-
-	return FALSE;
 }
 
-
-void ncopy(BOOL widechar, BYTE* destination, BYTE* source, int count)
-{
-	int i;
-
-	for (i = 0; i < count; i ++)
-	{
-		funs->set(destination, i, funs->ref(source, i));
-	}
-}
-
-BOOL LinkedList_StringHasSubstring(BOOL widechar, BYTE* string, wLinkedList* list)
+BOOL LinkedList_StringHasSubstring(BOOL widechar, void* string, wLinkedList* list)
 {
 	LinkedList_Enumerator_Reset(list);
 
 	while (LinkedList_Enumerator_MoveNext(list))
 	{
-		if (contains(funs, string, LinkedList_Enumerator_Current(list)))
+		if (contains(widechar, string, LinkedList_Enumerator_Current(list)))
 		{
 			return TRUE;
 		}
@@ -164,28 +176,52 @@ BOOL LinkedList_StringHasSubstring(BOOL widechar, BYTE* string, wLinkedList* lis
 	return FALSE;
 }
 
-void mszFilterStrings(BOOL widechar, void*   mszStrings, DWORD* cchReaders, wLinkedList* substrings)
+void mszFilterStrings(BOOL widechar, void*  mszStrings, DWORD* cchReaders, wLinkedList* substrings)
 {
-	BOOL widechar = & string_funs[widechar ? 1 : 0];
-	BYTE* current = (BYTE*)mszStrings;
-	BYTE* destination = current;
-
-	while (funs->ref(current, 0))
+	if (widechar)
 	{
-		int size = funs->len(current) + 1;
+		WCHAR* current = mszStrings;
+		WCHAR* destination = current;
 
-		if (LinkedList_StringHasSubstring(funs, current, substrings))
+		while (* current != 0)
 		{
-			/* Keep it */
-			ncopy(funs, destination, current, size);
-			destination = funs->inc(destination, size);
+			int size = lstrlenW(current) + 1;
+
+			if (LinkedList_StringHasSubstring(widechar, (BYTE *)current, substrings))
+			{
+				/* Keep it */
+				ncopy(widechar, destination, current, size);
+				destination += size;
+			}
+
+			current += size;
 		}
 
-		current = funs->inc(current, size);
+		ncopy(widechar, destination, current, 1);
+		* cchReaders = 1 + (destination - (WCHAR*)mszStrings);
 	}
+	else
+	{
+		char* current = (char*)mszStrings;
+		char* destination = current;
 
-	ncopy(funs, destination, current, 1);
-	* cchReaders = 1 + ((BYTE*)destination - (BYTE*)mszStrings) / funs->size();
+		while (* current != 0)
+		{
+			int size = strlen(current) + 1;
+
+			if (LinkedList_StringHasSubstring(widechar, (BYTE *)current, substrings))
+			{
+				/* Keep it */
+				ncopy(widechar, destination, current, size);
+				destination += size;
+			}
+
+			current += size;
+		}
+
+		ncopy(widechar, destination, current, 1);
+		* cchReaders = 1 + (destination - (char*)mszStrings);
+	}
 }
 
 void mszStrings_Enumerator_Reset(mszStrings_Enumerator* enumerator, BOOL widechar, void* mszStrings)
@@ -197,18 +233,30 @@ void mszStrings_Enumerator_Reset(mszStrings_Enumerator* enumerator, BOOL widecha
 
 BOOL mszStrings_Enumerator_MoveNext(mszStrings_Enumerator*  enumerator)
 {
-	struct string_funs*   funs = &string_funs[enumerator->widechar ? 1 : 0];
-
-	if (enumerator->state == 0)
+	if (enumerator->widechar)
 	{
-		enumerator->state = enumerator->mszStrings;
+		if (enumerator->state == 0)
+		{
+			enumerator->state = enumerator->mszStrings;
+		}
+		else
+		{
+			enumerator->state = ((WCHAR *)enumerator->state) + wcslen(enumerator->state) + 1;
+		}
+		return *((WCHAR *)enumerator->state);
 	}
 	else
 	{
-		enumerator->state = funs->inc(enumerator->state, funs->len(enumerator->state) + 1);
+		if (enumerator->state == 0)
+		{
+			enumerator->state = enumerator->mszStrings;
+		}
+		else
+		{
+			enumerator->state = ((char *)enumerator->state) + strlen(enumerator->state) + 1;
+		}
+		return *((char *)enumerator->state);
 	}
-
-	return funs->ref(enumerator->state, 0);
 }
 
 void* mszStrings_Enumerator_Current(mszStrings_Enumerator*  enumerator)
@@ -218,37 +266,49 @@ void* mszStrings_Enumerator_Current(mszStrings_Enumerator*  enumerator)
 
 void mszStringsPrint(FILE* output, BOOL widechar, void* mszStrings)
 {
-	BOOL widechar = & string_funs[widechar ? 1 : 0];
 	mszStrings_Enumerator enumerator;
 	mszStrings_Enumerator_Reset(&enumerator, widechar, mszStrings);
 
 	while (mszStrings_Enumerator_MoveNext(& enumerator))
 	{
 		void* 	current = mszStrings_Enumerator_Current(& enumerator);
-		char* printable = funs->convert(current);
-		fprintf(output, "%s\n", printable);
-		free(printable);
+
+		if (widechar)
+		{
+			char* printable = tochar(current);
+			fprintf(output, "%s\n", printable);
+			free(printable);
+		}
+		else
+		{
+			fprintf(output, "%s\n", current);
+		}
 	}
 }
 
 void mszStringsLog(const char* prefix, BOOL widechar, void* mszStrings)
 {
-	BOOL widechar = & string_funs[widechar ? 1 : 0];
-	mszStrings_Enumerator enumerator;
-
 	if (prefix == 0)
 	{
 		prefix = "";
 	}
 
+	mszStrings_Enumerator enumerator;
 	mszStrings_Enumerator_Reset(&enumerator, widechar, mszStrings);
 
 	while (mszStrings_Enumerator_MoveNext(& enumerator))
 	{
 		void* current = mszStrings_Enumerator_Current(& enumerator);
-		char* printable = funs->convert(current);
-		WLog_DBG(TAG, "%s%s", prefix, printable);
-		free(printable);
+		if (widechar)
+		{
+			char* printable = tochar(current);
+			WLog_DBG(TAG, "%s%s", prefix, printable);
+			free(printable);
+		}
+		else
+		{
+			WLog_DBG(TAG, "%s%s", prefix, current);
+		}
 	}
 }
 
@@ -258,11 +318,23 @@ int mszSize(BOOL widechar, void* mszStrings)
 	mszStrings_Enumerator enumerator;
 	mszStrings_Enumerator_Reset(&enumerator, widechar, mszStrings);
 
-	while (mszStrings_Enumerator_MoveNext(& enumerator))
+	if (widechar)
 	{
-		size += funs->len(mszStrings_Enumerator_Current(& enumerator)) + 1;
-	}
+		while (mszStrings_Enumerator_MoveNext(& enumerator))
+		{
+			size += wcslen(mszStrings_Enumerator_Current(& enumerator)) + 1;
+		}
 
-	return size * funs->size();
+		return size * sizeof(WCHAR);
+	}
+	else
+	{
+		while (mszStrings_Enumerator_MoveNext(& enumerator))
+		{
+			size += strlen(mszStrings_Enumerator_Current(& enumerator)) + 1;
+		}
+
+		return size;
+	}
 }
 
