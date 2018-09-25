dab5770fed1aa1147fd50eee21ba0b9227fbbed8
Armin Novak <armin.novak@thincast.com>
2018-09-25 11:13:32 +0200

    Added microphone support to shadow server.

--- include/freerdp/server/audin.h
+++ include/freerdp/server/audin.h
@@ -28,13 +28,15 @@
 
 typedef struct _audin_server_context audin_server_context;
 
-typedef UINT (*psAudinServerSelectFormat)(audin_server_context* context, int client_format_index);
+typedef UINT(*psAudinServerSelectFormat)(audin_server_context* context, int client_format_index);
 typedef BOOL (*psAudinServerOpen)(audin_server_context* context);
+typedef BOOL (*psAudinServerIsOpen)(audin_server_context* context);
 typedef BOOL (*psAudinServerClose)(audin_server_context* context);
 
-typedef UINT (*psAudinServerOpening)(audin_server_context* context);
-typedef UINT (*psAudinServerOpenResult)(audin_server_context* context, UINT32 result);
-typedef UINT (*psAudinServerReceiveSamples)(audin_server_context* context, const void* buf, int nframes);
+typedef UINT(*psAudinServerOpening)(audin_server_context* context);
+typedef UINT(*psAudinServerOpenResult)(audin_server_context* context, UINT32 result);
+typedef UINT(*psAudinServerReceiveSamples)(audin_server_context* context, const void* buf,
+        int nframes);
 
 struct _audin_server_context
 {
@@ -68,6 +70,9 @@ struct _audin_server_context
 	 * Open the audio input stream.
 	 */
 	psAudinServerOpen Open;
+
+	psAudinServerIsOpen IsOpen;
+
 	/**
 	 * Close the audio stream.
 	 */
