dd80a097816f83af6549689190468d9d421f8452
Armin Novak <armin.novak@thincast.com>
2018-10-18 17:23:06 +0200

    Allow window orders with AllowUnanouncedOrdersFromServer

--- libfreerdp/core/window.c
+++ libfreerdp/core/window.c
@@ -409,24 +409,27 @@ static BOOL window_order_supported(const rdpSettings* settings, UINT32 fieldFlag
 {
 	const UINT32 mask = (WINDOW_ORDER_FIELD_CLIENT_AREA_SIZE || WINDOW_ORDER_FIELD_RP_CONTENT ||
 	                     WINDOW_ORDER_FIELD_ROOT_PARENT);
+	BOOL dresult;
 
 	if (!settings)
 		return FALSE;
 
 	/* See [MS-RDPERP] 2.2.1.1.2 Window List Capability Set */
+	dresult = settings->AllowUnanouncedOrdersFromServer;
+
 	switch (settings->RemoteWndSupportLevel)
 	{
 		case WINDOW_LEVEL_SUPPORTED_EX:
 			return TRUE;
 
 		case WINDOW_LEVEL_SUPPORTED:
-			return (fieldFlags & mask) == 0;
+			return ((fieldFlags & mask) == 0) || dresult;
 
 		case WINDOW_LEVEL_NOT_SUPPORTED:
-			return FALSE;
+			return dresult;
 
 		default:
-			return FALSE;
+			return dresult;
 	}
 }
 
