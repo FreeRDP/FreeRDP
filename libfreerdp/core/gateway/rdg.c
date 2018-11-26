263cc74580a904a6e9d4e92dfe20acc967feaa35
Armin Novak <armin.novak@thincast.com>
2018-11-26 14:11:29 +0100

    Fixed buffer reset in fields_present_to_string

--- libfreerdp/core/gateway/rdg.c
+++ libfreerdp/core/gateway/rdg.c
@@ -190,6 +190,7 @@ static const char* fields_present_to_string(UINT16 fieldsPresent, const t_err_ma
 	size_t x = 0;
 	static char buffer[1024] = { 0 };
 	char fields[12];
+	memset(buffer, 0, sizeof(buffer));
 
 	for (x = 0; x < elements; x++)
 	{
