4c3626340cecf191af560a257d81eebc38d097af
Armin Novak <armin.novak@thincast.com>
2018-12-03 16:54:56 +0100

    Set minimum deployment target to 10.0

--- cmake/iOSToolchain.cmake
+++ cmake/iOSToolchain.cmake
@@ -37,7 +37,7 @@ set (APPLE True)
 set (IOS True)
 
 # Required as of cmake 2.8.10
-set (CMAKE_OSX_DEPLOYMENT_TARGET "" CACHE STRING "Force unset of the deployment target for iOS" FORCE)
+#set (CMAKE_OSX_DEPLOYMENT_TARGET "" CACHE STRING "Force unset of the deployment target for iOS" FORCE)
 
 # Determine the cmake host system version so we know where to find the iOS SDKs
 find_program (CMAKE_UNAME uname /bin /usr/bin /usr/local/bin)
