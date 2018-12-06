53a8787675f4f37a5f2b4974927f50aeae6aea2c
Pascal J. Bourguignon <pjb@informatimago.com>
2018-12-06 15:33:05 +0100

    Autoformated.

--- libfreerdp/core/nla.c
+++ libfreerdp/core/nla.c
@@ -551,7 +551,6 @@ static int nla_client_init_smartcard_logon(rdpNla* nla)
 {
 	rdpSettings* settings = nla->settings;
 	nla->cred_type = settings->CredentialsType;
-
 #if defined(WITH_PKCS11H) && defined(WITH_GSSAPI)
 
 	/* gets the UPN settings->UserPrincipalName */
