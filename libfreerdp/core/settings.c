7610917a48e2ea4f1e1065bd226643120cbce4e5
Armin Novak <armin.novak@thincast.com>
2018-12-14 11:51:50 +0100

    Disabled offscreen and bitmap cache by default.

--- libfreerdp/core/settings.c
+++ libfreerdp/core/settings.c
@@ -430,9 +430,6 @@ rdpSettings* freerdp_settings_new(DWORD flags)
 	settings->DrawAllowDynamicColorFidelity = FALSE;
 	settings->FrameMarkerCommandEnabled = TRUE;
 	settings->SurfaceFrameMarkerEnabled = TRUE;
-	settings->BitmapCacheV3Enabled = FALSE;
-	settings->BitmapCacheEnabled = TRUE;
-	settings->BitmapCachePersistEnabled = FALSE;
 	settings->AllowCacheWaitingList = TRUE;
 	settings->BitmapCacheV2NumCells = 5;
 	settings->BitmapCacheV2CellInfo = (BITMAP_CACHE_V2_CELL_INFO*) malloc(sizeof(
@@ -487,7 +484,7 @@ rdpSettings* freerdp_settings_new(DWORD flags)
 	settings->GlyphCache[9].cacheMaximumCellSize = 256;
 	settings->FragCache->cacheEntries = 256;
 	settings->FragCache->cacheMaximumCellSize = 256;
-	settings->OffscreenSupportLevel = TRUE;
+	settings->OffscreenSupportLevel = FALSE;
 	settings->OffscreenCacheSize = 7680;
 	settings->OffscreenCacheEntries = 2000;
 	settings->DrawNineGridCacheSize = 2560;
