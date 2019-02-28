136a4f95069697296d23f44a8c5a3eda7cfb61dd
Mati Shabtay <matishabtay@gmail.com>
2019-02-28 11:07:17 +0200

    server/proxy: Move context definition to pf_context

---
+++ server/proxy/pf_context.c
@@ -0,0 +1,64 @@
+/**
+ * FreeRDP: A Remote Desktop Protocol Implementation
+ * FreeRDP Proxy Server
+ *
+ * Copyright 2019 Mati Shabtay <matishabtay@gmail.com>
+ * Copyright 2019 Kobi Mizrachi <kmizrachi18@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "pf_client.h"
+#include "pf_context.h"
+
+/* Proxy context initialization callback */
+BOOL client_to_proxy_context_new(freerdp_peer* client, clientToProxyContext* context)
+{
+	context->vcm = WTSOpenServerA((LPSTR) client->context);
+
+	if (!context->vcm || context->vcm == INVALID_HANDLE_VALUE)
+		goto fail_open_server;
+
+	return TRUE;
+fail_open_server:
+	context->vcm = NULL;
+	return FALSE;
+}
+
+/* Proxy context free callback */
+void client_to_proxy_context_free(freerdp_peer* client, clientToProxyContext* context)
+{
+	if (context)
+	{
+		WTSCloseServer((HANDLE) context->vcm);
+	}
+}
+
+rdpContext* proxy_to_server_context_create(rdpContext* peerContext, char* host, 
+                                           DWORD port, char* username, char* password)
+{
+	RDP_CLIENT_ENTRY_POINTS clientEntryPoints;
+	rdpContext* context;
+	RdpClientEntry(&clientEntryPoints);
+	context = freerdp_client_context_new(&clientEntryPoints);
+
+	if (!context)
+		return NULL;
+
+	context->settings->RedirectClipboard = FALSE;
+	context->settings->ServerHostname = host;
+	context->settings->ServerPort = port;
+	context->settings->Username = username;
+	context->settings->Password = password;
+	return context;
+}
---
+++ server/proxy/pf_context.h
@@ -0,0 +1,80 @@
+/**
+ * FreeRDP: A Remote Desktop Protocol Implementation
+ * FreeRDP Proxy Server
+ *
+ * Copyright 2019 Mati Shabtay <matishabtay@gmail.com>
+ * Copyright 2019 Kobi Mizrachi <kmizrachi18@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FREERDP_SERVER_PROXY_PFCONTEXT_H
+#define FREERDP_SERVER_PROXY_PFCONTEXT_H
+
+#include <freerdp/freerdp.h>
+#include <freerdp/channels/wtsvc.h>
+#include <freerdp/client/encomsp.h>
+#include <freerdp/client/rdpei.h>
+#include <freerdp/client/rdpgfx.h>
+
+/**
+ * Proxy context wraps a peer's context, and holds a reference to the other end
+ * of the connection. This lets one side of the proxy to forward events to the
+ * other.
+ */
+struct proxy_context
+{
+	/* Context of client or server */
+	rdpContext _context;
+
+	/**
+	 * Context of peer to which the proxy is connected.
+	 * Events from the first context are forwarded to this one.
+	 */
+	rdpContext* peerContext;
+};
+typedef struct proxy_context proxyContext;
+
+/**
+ * Context used for the client's connection to the proxy.
+ */
+struct client_to_proxy_context
+{
+	proxyContext _context;
+
+	HANDLE vcm;
+};
+typedef struct client_to_proxy_context clientToProxyContext;
+
+BOOL client_to_proxy_context_new(freerdp_peer* client,
+                                 clientToProxyContext* context);
+void client_to_proxy_context_free(freerdp_peer* client,
+                                  clientToProxyContext* context);
+
+/**
+ * Context used for the proxy's connection to the target server.
+ */
+struct proxy_to_server_context
+{
+	proxyContext _context;
+
+	RdpeiClientContext* rdpei;
+	RdpgfxClientContext* gfx;
+	EncomspClientContext* encomsp;
+};
+typedef struct proxy_to_server_context proxyToServerContext;
+
+rdpContext* proxy_to_server_context_create(rdpContext* peerContext, char* host,
+                                        DWORD port, char* username, char* password);
+
+#endif /* FREERDP_SERVER_PROXY_PFCONTEXT_H */
