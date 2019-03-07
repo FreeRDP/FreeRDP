f6bc7f24092519dc91880679058d9497fadc92cb
kubistika <kmizrachi18@gmail.com>
2019-03-07 10:07:38 +0200

    server/proxy: Move common function  to pf_common.c

--- server/proxy/pf_common.h
+++ server/proxy/pf_common.h
@@ -1,4 +1,9 @@
+#ifndef FREERDP_SERVER_PROXY_PFCOMMON_H
+#define FREERDP_SERVER_PROXY_PFCOMMON_H
+
 #include <freerdp/freerdp.h>
 #include "pf_context.h"
 
 BOOL pf_common_connection_aborted_by_peer(proxyContext* context);
+
+#endif /* FREERDP_SERVER_PROXY_PFCOMMON_H */
