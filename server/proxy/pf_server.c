e0c157deec76510ba7a6f918e0cfdcd66344968f
Mati Shabtay <matishabtay@gmail.com>
2019-03-12 11:47:08 +0200

    pf_server: Fix double freed credentials

--- server/proxy/pf_server.c
+++ server/proxy/pf_server.c
@@ -275,11 +275,8 @@ BOOL pf_peer_post_connect(freerdp_peer* client)
 		return FALSE;
 	}
 
-	if(!freerdp_client_settings_post_process(client.settings)) {
-		return FALSE
-	}
-	char* username = client->settings->Username;
-	char* password = client->settings->Password;
+	char* username = _strdup(client->settings->Username);
+	char* password = _strdup(client->settings->Password);
 	
 	/* Start a proxy's client in it's own thread */
 	rdpContext* sContext = proxy_to_server_context_create(client->context,
