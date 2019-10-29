b06efc78ecb688bd172a7d9da9e84e091c9a053e
Armin Novak <armin.novak@thincast.com>
2019-10-29 12:57:19 +0100

    Fixed warnings from TestEnvironmentGetEnvironmentStrings

--- winpr/libwinpr/environment/test/TestEnvironmentGetEnvironmentStrings.c
+++ winpr/libwinpr/environment/test/TestEnvironmentGetEnvironmentStrings.c
@@ -6,9 +6,13 @@
 
 int TestEnvironmentGetEnvironmentStrings(int argc, char* argv[])
 {
+	int r = -1;
 	TCHAR* p;
-	int length;
+	size_t length;
 	LPTCH lpszEnvironmentBlock;
+
+	WINPR_UNUSED(argc);
+	WINPR_UNUSED(argv);
 
 	lpszEnvironmentBlock = GetEnvironmentStrings();
 
@@ -16,13 +20,19 @@ int TestEnvironmentGetEnvironmentStrings(int argc, char* argv[])
 
 	while (p[0] && p[1])
 	{
-		_tprintf(_T("%s\n"), p);
+		const int rc = _tprintf(_T("%s\n"), p);
+		if (rc < 1)
+			goto fail;
 		length = _tcslen(p);
+		if (length != (size_t)(rc - 1))
+			goto fail;
 		p += (length + 1);
 	}
 
+	r = 0;
+fail:
 	FreeEnvironmentStrings(lpszEnvironmentBlock);
 
-	return 0;
+	return r;
 }
 
