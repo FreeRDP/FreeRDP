d63319587982c2e888cfcf93cad21447fc0912a9
Armin Novak <armin.novak@thincast.com>
2020-02-25 16:38:31 +0100

    Promoted usb, printer and smartcard dependencies to recommended

--- channels/urbdrc/ChannelOptions.cmake
+++ channels/urbdrc/ChannelOptions.cmake
@@ -1,6 +1,6 @@
 
-set(OPTION_DEFAULT OFF)
-set(OPTION_CLIENT_DEFAULT OFF)
+set(OPTION_DEFAULT ON)
+set(OPTION_CLIENT_DEFAULT ON)
 set(OPTION_SERVER_DEFAULT OFF)
 
 define_channel_options(NAME "urbdrc" TYPE "dynamic"
