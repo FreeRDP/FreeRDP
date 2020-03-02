27f116419215c0225566acc6cecf33f778ce787b
Sergey Bronnikov <estetus@gmail.com>
2020-03-02 18:55:05 +0300

    add certificate_data_free to revent memleak

--- libfreerdp/crypto/test/TestFuzzCryptoCertificateDataMatch.c
+++ libfreerdp/crypto/test/TestFuzzCryptoCertificateDataMatch.c
@@ -9,6 +9,7 @@ extern int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
 	settings = freerdp_settings_new(0);
 	certificate_store = certificate_store_new(settings);
 	certificate_data_print(certificate_store, (rdpCertificateData*)Data);
+	certificate_data_free(certificate_store);
 	freerdp_settings_free(settings);
 
 	return EXIT_SUCCESS;
