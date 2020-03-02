456cff541017b26ee688b16f8ed59f48af1fc92f
Sergey Bronnikov <estetus@gmail.com>
2020-03-02 17:03:49 +0300

    stdin = 1

--- libfreerdp/utils/test/TestFuzzUtilsReadPassphrase.c
+++ libfreerdp/utils/test/TestFuzzUtilsReadPassphrase.c
@@ -2,6 +2,6 @@
 
 int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
 {
-	freerdp_passphrase_read(NULL, (char*)Data, Size, 0);
+	freerdp_passphrase_read(NULL, (char*)Data, Size, 1);
 	return 0;
 }
