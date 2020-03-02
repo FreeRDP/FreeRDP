287484efd4c3401676c13dd50c25eb2cb2f3b191
Sergey Bronnikov <estetus@gmail.com>
2020-03-02 19:17:25 +0300

    limit data size in crypto_base64_decode()

--- libfreerdp/crypto/test/TestFuzzCryptoBase64Decode.c
+++ libfreerdp/crypto/test/TestFuzzCryptoBase64Decode.c
@@ -4,7 +4,11 @@ extern int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
 {
 	int ntlmTokenLength = 0;
 	BYTE* ntlmTokenData = NULL;
-	crypto_base64_decode((char*)Data, (int)Size, &ntlmTokenData, &ntlmTokenLength);
+	char* data = (char*)Data;
+	if (Size > UINTPTR_MAX) {
+	    data[UINTPTR_MAX] = '\0';
+	}
+	crypto_base64_decode(data, UINTPTR_MAX, &ntlmTokenData, &ntlmTokenLength);
 
 	return EXIT_SUCCESS;
 }
