e2d21bb93a75b8b4ca3bb5bc6f081ed34358ec1f
Sergey Bronnikov <estetus@gmail.com>
2020-03-02 17:31:45 +0300

    Add fuzzing data to certificate_data_{match,print}

--- libfreerdp/crypto/test/TestFuzzCryptoCertificateDataPrint.c
+++ libfreerdp/crypto/test/TestFuzzCryptoCertificateDataPrint.c
@@ -4,13 +4,11 @@ extern int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
 {
 	rdpCertificate* certificate = NULL;
 	rdpCertificateStore* certificate_store = NULL;
-	rdpCertificateData* certificate_data = NULL;
 	rdpSettings* settings;
 
 	settings = freerdp_settings_new(0);
 	certificate_store = certificate_store_new(settings);
-	certificate_data = certificate_data_new("someurl", 3389, "subject", "issuer", "ff:11:22:dd");
-	certificate_data_match(certificate_store, certificate_data);
+	certificate_data_match(certificate_store, (rdpCertificateData*)Data);
 	freerdp_settings_free(settings);
 
 	return EXIT_SUCCESS;
