8afa27c936b95ddfdcfc353ecf6eba832c62ccc3
Armin Novak <armin.novak@thincast.com>
2020-03-10 08:50:55 +0100

    Fixed broken NULL check.

--- winpr/tools/makecert/makecert.c
+++ winpr/tools/makecert/makecert.c
@@ -563,12 +563,12 @@ int makecert_context_output_certificate_file(MAKECERT_CONTEXT* context, char* pa
 					goto out_fail;
 
 				x509_str = makecert_read_str(bio, &offset);
-				if (x509_str)
+				if (!x509_str)
 					goto out_fail;
 
 				length = offset;
 
-				if (fwrite((void*)x509_str, length, 1, fp) != 1)
+				if (fwrite(x509_str, length, 1, fp) != 1)
 					goto out_fail;
 			}
 		}
