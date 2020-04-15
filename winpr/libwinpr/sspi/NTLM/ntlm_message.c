8fa38359634a9910b91719818ab02f23c320dbae
akallabeth <akallabeth@posteo.net>
2020-04-15 16:48:50 +0200

    Fixed oob read in ntlm_read_NegotiateMessage

--- winpr/libwinpr/sspi/NTLM/ntlm_message.c
+++ winpr/libwinpr/sspi/NTLM/ntlm_message.c
@@ -219,6 +219,11 @@ SECURITY_STATUS ntlm_read_NegotiateMessage(NTLM_CONTEXT* context, PSecBuffer buf
 		return SEC_E_INVALID_TOKEN;
 	}
 
+	if (Stream_GetRemainingLength(s) < 4)
+	{
+		Stream_Free(s, FALSE);
+		return SEC_E_INVALID_TOKEN;
+	}
 	Stream_Read_UINT32(s, message->NegotiateFlags); /* NegotiateFlags (4 bytes) */
 
 	if (!((message->NegotiateFlags & NTLMSSP_REQUEST_TARGET) &&
