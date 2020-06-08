FreeRDP/FreeRDP

commit 722790f4cadc3f2d551bf42702a99b3a3a69e5a3

Always require aligned memory for interleaved codec.

libfreerdp/codec/interleaved.c
@@ -272,11 +272,7 @@ static INLINE void write_pixel_16(BYTE* _buf, UINT16 _pix)
 #define WHITE_PIXEL 0xFFFF
 #define DESTWRITEPIXEL(_buf, _pix) write_pixel_16(_buf, _pix)
 #define DESTREADPIXEL(_pix, _buf) _pix = ((UINT16*)(_buf))[0]
-#ifdef HAVE_ALIGNED_REQUIRED
 #define SRCREADPIXEL(_pix, _buf) _pix = (_buf)[0] | ((_buf)[1] << 8)
-#else
-#define SRCREADPIXEL(_pix, _buf) _pix = ((UINT16*)(_buf))[0]
-#endif
 #define DESTNEXTPIXEL(_buf) _buf += 2
 #define SRCNEXTPIXEL(_buf) _buf += 2
 #define WRITEFGBGIMAGE WriteFgBgImage16to16

CMakeLists.txt
@@ -936,10 +936,6 @@ if(MBEDTLS_FOUND)
 	add_definitions("-DWITH_MBEDTLS")
 endif()
 
-if (TARGET_ARCH MATCHES "sparc")
-	set(HAVE_ALIGNED_REQUIRED 1)
-endif()
-
 if (WITH_X264 OR WITH_OPENH264 OR WITH_MEDIA_FOUNDATION OR WITH_FFMPEG)
 	set(WITH_GFX_H264 ON)
 else()

config.h.in
@@ -26,7 +26,6 @@
 #cmakedefine HAVE_EXECINFO_H
 
 /* Features */
-#cmakedefine HAVE_ALIGNED_REQUIRED
 #cmakedefine SWRESAMPLE_FOUND
 #cmakedefine AVRESAMPLE_FOUND
 

commit 47d65a0006b8bfdcabfa3406c2f59f45d7e9007e

Write pixel value as little-endian in write_pixel_16
Fixes: 722790f4cadc3f2d551bf42702a99b3a3a69e5a3
Closes: https://github.com/FreeRDP/FreeRDP/issues/6267

libfreerdp/codec/interleaved.c
@@ -232,7 +232,8 @@ static INLINE void write_pixel_24(BYTE* _buf, UINT32 _pix)
 
 static INLINE void write_pixel_16(BYTE* _buf, UINT16 _pix)
 {
-	*(UINT16*)_buf = _pix;
+	(_buf)[0] = (BYTE)(_pix);
+	(_buf)[1] = (BYTE)((_pix) >> 8);
 }
 
 #undef DESTWRITEPIXEL