37a5a7a1575b292a31bb0974d6c60143fd5e1027
akallabeth <akallabeth@posteo.net>
2020-07-03 15:47:53 +0200

    Replaced memset in urbdrc

--- channels/urbdrc/client/urbdrc_main.c
+++ channels/urbdrc/client/urbdrc_main.c
@@ -86,7 +86,7 @@ static IWTSVirtualChannel* get_channel(IUDEVMAN* idevman)
 static int func_container_id_generate(IUDEVICE* pdev, char* strContainerId)
 {
 	char *p, *path;
-	UINT8 containerId[17];
+	UINT8 containerId[17] = { 0 };
 	UINT16 idVendor, idProduct;
 	idVendor = (UINT16)pdev->query_device_descriptor(pdev, ID_VENDOR);
 	idProduct = (UINT16)pdev->query_device_descriptor(pdev, ID_PRODUCT);
@@ -97,7 +97,6 @@ static int func_container_id_generate(IUDEVICE* pdev, char* strContainerId)
 	else
 		p = path;
 
-	ZeroMemory(containerId, sizeof(containerId));
 	sprintf_s((char*)containerId, sizeof(containerId), "%04" PRIX16 "%04" PRIX16 "%s", idVendor,
 	          idProduct, p);
 	/* format */
