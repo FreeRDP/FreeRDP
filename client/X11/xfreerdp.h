e24c95f90e96c3578b461281de128314c9def66f
Nathan Loewen <loewen.nathan@gmail.com>
2020-09-08 10:12:27 -0500

    X11: Scale cursor when SmartSizing is on

--- client/X11/xfreerdp.h
+++ client/X11/xfreerdp.h
@@ -28,6 +28,10 @@
 
 typedef struct xf_context xfContext;
 
+#ifdef WITH_XCURSOR
+#include <X11/Xcursor/Xcursor.h>
+#endif
+
 #include <freerdp/api.h>
 
 #include "xf_window.h"
@@ -47,6 +51,14 @@ typedef struct xf_context xfContext;
 #include <freerdp/codec/h264.h>
 #include <freerdp/codec/progressive.h>
 #include <freerdp/codec/region.h>
+
+#if !defined(XcursorUInt)
+typedef unsigned int XcursorUInt;
+#endif
+
+#if !defined(XcursorPixel)
+typedef XcursorUInt XcursorPixel;
+#endif
 
 struct xf_FullscreenMonitors
 {
@@ -69,6 +81,12 @@ typedef struct xf_WorkArea xfWorkArea;
 struct xf_pointer
 {
 	rdpPointer pointer;
+	XcursorPixel* cursorPixels;
+	UINT32 nCursors;
+	UINT32 mCursors;
+	UINT32* cursorWidths;
+	UINT32* cursorHeights;
+	Cursor* cursors;
 	Cursor cursor;
 };
 typedef struct xf_pointer xfPointer;
