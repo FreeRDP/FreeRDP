268bc2e8ef7b76887d14d50702bb8446ce15a4c1
Armin Novak <armin.novak@thincast.com>
2021-05-20 14:11:43 +0200

    Updated RDPEI channel API
    * Added new function pointers to cancel a pending operation
    * Added new function pointers to send custom event flags
    * Added exposed feature mask to disable channel features

--- include/freerdp/client/rdpei.h
+++ include/freerdp/client/rdpei.h
@@ -32,21 +32,23 @@ typedef struct _rdpei_client_context RdpeiClientContext;
 
 typedef UINT32 (*pcRdpeiGetVersion)(RdpeiClientContext* context);
 typedef UINT32 (*pcRdpeiGetFeatures)(RdpeiClientContext* context);
+
 typedef UINT (*pcRdpeiAddContact)(RdpeiClientContext* context,
                                   const RDPINPUT_CONTACT_DATA* contact);
 
-typedef UINT (*pcRdpeiTouchBegin)(RdpeiClientContext* context, INT32 externalId, INT32 x, INT32 y,
+typedef UINT (*pcRdpeiTouchEvent)(RdpeiClientContext* context, INT32 externalId, INT32 x, INT32 y,
                                   INT32* contactId);
-typedef UINT (*pcRdpeiTouchUpdate)(RdpeiClientContext* context, INT32 externalId, INT32 x, INT32 y,
-                                   INT32* contactId);
-typedef UINT (*pcRdpeiTouchEnd)(RdpeiClientContext* context, INT32 externalId, INT32 x, INT32 y,
-                                INT32* contactId);
+typedef UINT (*pcRdpeiTouchRawEvent)(RdpeiClientContext* context, INT32 externalId, INT32 x,
+                                     INT32 y, INT32* contactId, UINT32 contactFlags,
+                                     UINT32 fieldFlags, ...);
 
 typedef UINT (*pcRdpeiAddPen)(RdpeiClientContext* context, INT32 externalId,
                               const RDPINPUT_PEN_CONTACT* contact);
 
 typedef UINT (*pcRdpeiPen)(RdpeiClientContext* context, INT32 externalId, UINT32 fieldFlags,
                            INT32 x, INT32 y, ...);
+typedef UINT (*pcRdpeiPenRawEvent)(RdpeiClientContext* context, INT32 externalId,
+                                   UINT32 contactFlags, UINT32 fieldFlags, INT32 x, INT32 y, ...);
 
 typedef UINT (*pcRdpeiSuspendTouch)(RdpeiClientContext* context);
 typedef UINT (*pcRdpeiResumeTouch)(RdpeiClientContext* context);
@@ -61,9 +63,9 @@ struct _rdpei_client_context
 
 	pcRdpeiAddContact AddContact;
 
-	pcRdpeiTouchBegin TouchBegin;
-	pcRdpeiTouchUpdate TouchUpdate;
-	pcRdpeiTouchEnd TouchEnd;
+	pcRdpeiTouchEvent TouchBegin;
+	pcRdpeiTouchEvent TouchUpdate;
+	pcRdpeiTouchEvent TouchEnd;
 
 	pcRdpeiAddPen AddPen;
 
@@ -73,6 +75,14 @@ struct _rdpei_client_context
 
 	pcRdpeiSuspendTouch SuspendTouch;
 	pcRdpeiResumeTouch ResumeTouch;
+
+	pcRdpeiTouchEvent TouchCancel;
+	pcRdpeiTouchRawEvent TouchRawEvent;
+
+	pcRdpeiPen PenCancel;
+	pcRdpeiPenRawEvent PenRawEvent;
+
+	UINT32 clientFeaturesMask;
 };
 
 #endif /* FREERDP_CHANNEL_RDPEI_CLIENT_RDPEI_H */
