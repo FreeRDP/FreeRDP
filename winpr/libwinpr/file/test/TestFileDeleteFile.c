04ff6b8824b5467a661d32ee6a03334ce2b66252
Armin Novak <armin.novak@thincast.com>
2021-07-29 10:38:41 +0200

    Implemented TestFileDeleteFile

--- winpr/libwinpr/file/test/TestFileDeleteFile.c
+++ winpr/libwinpr/file/test/TestFileDeleteFile.c
@@ -1,10 +1,50 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <winpr/crt.h>
 #include <winpr/file.h>
 #include <winpr/windows.h>
 
 int TestFileDeleteFile(int argc, char* argv[])
 {
+	BOOL rc;
+	int fd;
+	char validA[] = "/tmp/valid-test-file-XXXXXX";
+	char validW[] = "/tmp/valid-test-file-XXXXXX";
+	WCHAR* validWW = NULL;
+	const char* invalidA = "/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+	const WCHAR invalidW[] = { '/', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+		                       'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+		                       'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+		                       'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', '\0' };
+
+	WINPR_UNUSED(argc);
+	WINPR_UNUSED(argv);
+
+	rc = DeleteFileA(invalidA);
+	if (rc)
+		return -1;
+
+	rc = DeleteFileW(invalidW);
+	if (rc)
+		return -1;
+
+	fd = mkstemp(validA);
+	if (fd < 0)
+		return -1;
+
+	rc = DeleteFileA(validA);
+	if (!rc)
+		return -1;
+
+	fd = mkstemp(validW);
+	if (fd < 0)
+		return -1;
+
+	ConvertToUnicode(CP_UTF8, 0, validW, -1, &validWW, 0);
+	rc = DeleteFileW(validWW);
+	free(validWW);
+	if (!rc)
+		return -1;
 	return 0;
 }
