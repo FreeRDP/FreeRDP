10ee86527a32f28a84ef44194433bb84bf3b360e
Fabian Vogt <fvogt@suse.de>
2021-12-02 15:50:08 +0100

    Make H.264 codec optional during runtime
    It's possible that FreeRDP was built against FFmpeg, but it doesn't support
    H.264. In that case, just continue without H.264 support instead of failing
    hard before even trying to connect.
    
    This is especially useful for Linux distributions which can't ship H.264
    support in FFmpeg out of the box (patent issues), but allow enabling H.264
    later by installing a version of FFmpeg which has it enabled.

--- libfreerdp/core/codecs.c
+++ libfreerdp/core/codecs.c
@@ -142,10 +142,7 @@ BOOL freerdp_client_codecs_prepare(rdpCodecs* codecs, UINT32 flags, UINT32 width
 	{
 		if (!(codecs->h264 = h264_context_new(FALSE)))
 		{
-			WLog_ERR(TAG, "Failed to create h264 codec context");
-#ifndef WITH_OPENH264_LOADING
-			return FALSE;
-#endif
+			WLog_WARN(TAG, "Failed to create h264 codec context");
 		}
 	}
 #endif
