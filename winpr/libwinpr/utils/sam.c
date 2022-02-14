b369517e98360706d9252d1ca3a030af56a1cb42
Armin Novak <armin.novak@thincast.com>
2022-02-14 10:05:28 +0100

    Fixed HexStrToBin, respect const input

--- winpr/libwinpr/utils/sam.c
+++ winpr/libwinpr/utils/sam.c
@@ -173,23 +173,29 @@ static void SamLookupFinish(WINPR_SAM* sam)
 static void HexStrToBin(const char* str, BYTE* bin, size_t length)
 {
 	size_t i;
-	CharUpperBuffA(str, length * 2);
 
 	for (i = 0; i < length; i++)
 	{
-		bin[i] = 0;
+		char cur = str[2 * i];
+		char curNext = str[2 * i + 1];
+		BYTE* curBin = &bin[i];
 
-		if ((str[i * 2] >= '0') && (str[i * 2] <= '9'))
-			bin[i] |= (str[i * 2] - '0') << 4;
+		CharUpperBuffA(&cur, 1);
+		CharUpperBuffA(&curNext, 1);
 
-		if ((str[i * 2] >= 'A') && (str[i * 2] <= 'F'))
-			bin[i] |= (str[i * 2] - 'A' + 10) << 4;
+		*curBin = 0;
 
-		if ((str[i * 2 + 1] >= '0') && (str[i * 2 + 1] <= '9'))
-			bin[i] |= (str[i * 2 + 1] - '0');
+		if ((cur >= '0') && (cur <= '9'))
+			*curBin |= (cur - '0') << 4;
 
-		if ((str[i * 2 + 1] >= 'A') && (str[i * 2 + 1] <= 'F'))
-			bin[i] |= (str[i * 2 + 1] - 'A' + 10);
+		if ((cur >= 'A') && (cur <= 'F'))
+			*curBin |= (cur - 'A' + 10) << 4;
+
+		if ((curNext >= '0') && (curNext <= '9'))
+			*curBin |= (curNext - '0');
+
+		if ((curNext >= 'A') && (curNext <= 'F'))
+			*curBin |= (curNext - 'A' + 10);
 	}
 }
 
