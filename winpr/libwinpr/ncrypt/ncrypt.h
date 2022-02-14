5a9528bba9883e68230b14697da9ae84422fd54e
Armin Novak <armin.novak@thincast.com>
2022-02-14 13:05:59 +0100

    Warning fixes and assert

--- winpr/libwinpr/ncrypt/ncrypt.h
+++ winpr/libwinpr/ncrypt/ncrypt.h
@@ -81,7 +81,7 @@ typedef struct
 SECURITY_STATUS checkNCryptHandle(NCRYPT_HANDLE handle, NCryptHandleType matchType);
 void* ncrypt_new_handle(NCryptHandleType kind, size_t len, NCryptGetPropertyFn getProp,
                         NCryptReleaseFn dtor);
-SECURITY_STATUS winpr_NCryptDefault_dtor(NCryptBaseHandle* h);
+SECURITY_STATUS winpr_NCryptDefault_dtor(NCRYPT_HANDLE handle);
 
 #if defined(WITH_PKCS11)
 SECURITY_STATUS NCryptOpenP11StorageProviderEx(NCRYPT_PROV_HANDLE* phProvider,
