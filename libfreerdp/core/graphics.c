cfb62bb1d232cd7383d7081e46a3e8efc4250d43
Dan Boanta <danut.boanta@uipath.com>
2022-02-17 20:05:48 +0200

    Fix memory leak

--- libfreerdp/core/graphics.c
+++ libfreerdp/core/graphics.c
@@ -106,13 +106,24 @@ rdpPointer* Pointer_Alloc(rdpContext* context)
 }
 
 static BOOL Pointer_New(rdpContext* context, rdpPointer* pointer)
-{
+{	
 	rdpPointer* proto;
 
 	if (!context || !context->graphics || !context->graphics->Pointer_Prototype)
 		return FALSE;
 
 	proto = context->graphics->Pointer_Prototype;
+	if (pointer->xorMaskData)
+	{
+		free(pointer->xorMaskData);
+		pointer->xorMaskData = NULL;
+	}
+
+	if (pointer->andMaskData)
+	{
+		free(pointer->andMaskData);
+		pointer->andMaskData = NULL;
+	}
 	*pointer = *proto;
 	return TRUE;
 }
