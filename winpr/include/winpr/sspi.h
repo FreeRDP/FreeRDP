f568d70d5281adadaafc1fef789cc835c2aec533
akallabeth <akallabeth@posteo.net>
2022-02-17 16:37:14 +0100

    Fixed guards

--- winpr/include/winpr/sspi.h
+++ winpr/include/winpr/sspi.h
@@ -484,12 +484,14 @@ typedef SecPkgCredentials_NamesW* PSecPkgCredentials_NamesW;
 
 #endif
 
-#if !defined(_WIN32) || defined(_UWP) || !defined(__MINGW32__)
+#if !defined(_WIN32) || defined(_UWP)
+#if !defined(__MINGW32__)
 typedef struct
 {
 	UINT32 BindingsLength;
 	SEC_CHANNEL_BINDINGS* Bindings;
 } SecPkgContext_Bindings;
+#endif
 #endif
 
 /* InitializeSecurityContext Flags */
