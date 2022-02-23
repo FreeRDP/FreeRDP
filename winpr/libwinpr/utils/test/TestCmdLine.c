ceaff16f8c77f5194c4ff854212daef713a16a39
Armin Novak <armin.novak@thincast.com>
2022-02-23 14:49:03 +0100

    Added quoted argument support to CommandLineParseCommaSeparatedValuesEx
    * Argument quoting support
    * Empty list element detection
    * Unit test for argument parser

--- winpr/libwinpr/utils/test/TestCmdLine.c
+++ winpr/libwinpr/utils/test/TestCmdLine.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 #include <winpr/crt.h>
+#include <winpr/assert.h>
 #include <winpr/tchar.h>
 #include <winpr/cmdline.h>
 #include <winpr/strlst.h>
@@ -17,6 +18,82 @@ static const char* testArgv[] = { "mstsc.exe",
 	                              "/valuelist:value1,value2",
 	                              "/valuelist-empty:",
 	                              0 };
+
+static const char testListAppName[] = "test app name";
+static const char* testListArgs[] = { "a,b,c,d", "a:,\"b:xxx, yyy\",c", "a:,,,b",
+	                                  "a:,\",b", "\"a,b,c,d d d,fff\"", "",
+	                                  NULL };
+
+static const char* testListArgs1[] = { testListAppName, "a", "b", "c", "d" };
+static const char* testListArgs2[] = { testListAppName, "a:", "b:xxx, yyy", "c" };
+// static const char* testListArgs3[] = {};
+// static const char* testListArgs4[] = {};
+static const char* testListArgs5[] = { testListAppName, "a", "b", "c", "d d d", "fff" };
+static const char* testListArgs6[] = { testListAppName };
+static const char* testListArgs7[] = { testListAppName };
+
+static const char** testListArgsResult[] = {
+	testListArgs1, testListArgs2, NULL /* testListArgs3 */, NULL /* testListArgs4 */, testListArgs5,
+	testListArgs6, testListArgs7
+};
+static const size_t testListArgsCount[] = {
+	ARRAYSIZE(testListArgs1),         ARRAYSIZE(testListArgs2), 0 /* ARRAYSIZE(testListArgs3) */,
+	0 /* ARRAYSIZE(testListArgs4) */, ARRAYSIZE(testListArgs5), ARRAYSIZE(testListArgs6),
+	ARRAYSIZE(testListArgs7)
+};
+
+static BOOL checkResult(size_t index, char** actual, size_t actualCount)
+{
+	const char** result = testListArgsResult[index];
+	const size_t resultCount = testListArgsCount[index];
+
+	if (resultCount != actualCount)
+		return FALSE;
+
+	if (actualCount == 0)
+	{
+		return (actual == NULL);
+	}
+	else
+	{
+		size_t x;
+
+		if (!actual)
+			return FALSE;
+
+		for (x = 0; x < actualCount; x++)
+		{
+			const char* a = result[x];
+			const char* b = actual[x];
+
+			if (strcmp(a, b) != 0)
+				return FALSE;
+		}
+	}
+
+	return TRUE;
+}
+
+static BOOL TestCommandLineParseCommaSeparatedValuesEx(void)
+{
+	size_t x;
+
+	WINPR_ASSERT(ARRAYSIZE(testListArgs) == ARRAYSIZE(testListArgsResult));
+	WINPR_ASSERT(ARRAYSIZE(testListArgs) == ARRAYSIZE(testListArgsCount));
+
+	for (x = 0; x < ARRAYSIZE(testListArgs); x++)
+	{
+		const char* list = testListArgs[x];
+		size_t count = 42;
+		char** ptr = CommandLineParseCommaSeparatedValuesEx(testListAppName, list, &count);
+		BOOL valid = checkResult(x, ptr, count);
+		free(ptr);
+		if (!valid)
+			return FALSE;
+	}
+
+	return TRUE;
+}
 
 int TestCmdLine(int argc, char* argv[])
 {
@@ -250,5 +327,8 @@ int TestCmdLine(int argc, char* argv[])
 
 out:
 	string_list_free(command_line);
+
+	if (!TestCommandLineParseCommaSeparatedValuesEx())
+		return -1;
 	return ret;
 }
