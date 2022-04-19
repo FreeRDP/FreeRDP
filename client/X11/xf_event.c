0e71056d49cf952a5426f5f52530a09af8bafe32
2fly2 <wjatchd@163.com>
2022-04-19 15:21:38 +0800

    fix : restore a maximized window that is minimized back to the maximized state
    Signed-off-by: 2fly2 <wjatchd@163.com>

--- client/X11/xf_event.c
+++ client/X11/xf_event.c
@@ -949,8 +949,8 @@ static BOOL xf_event_PropertyNotify(xfContext* xfc, const XPropertyEvent* event,
 
 		if (app)
 		{
-			if (maxVert && maxHorz && !minimized &&
-			    (appWindow->rail_state != WINDOW_SHOW_MAXIMIZED))
+			if (maxVert && maxHorz && !minimized /*&&
+			    (appWindow->rail_state != WINDOW_SHOW_MAXIMIZED)*/)
 			{
 				appWindow->rail_state = WINDOW_SHOW_MAXIMIZED;
 				xf_rail_send_client_system_command(xfc, appWindow->windowId, SC_MAXIMIZE);
