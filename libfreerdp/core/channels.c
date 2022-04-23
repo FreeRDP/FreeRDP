FreeRDP/FreeRDP

commit 73cdcdfe0931842b41b15b450a99e16b19d8d50c

Logging and parser fixes (#7796)
* Fixed remdesk settings pointer

* Fixed sign warnings in display_write_monitor_layout_pdu

* Use freerdp_abort_connect_context and freerdp_shall_disconnect_context

* Added and updates settings

* info assert/dynamic timezone

* mcs assert/log/flags

* Fixed and added assertions for wStream

* Unified stream length checks

* Added new function to check for lenght and log
* Replace all usages with this new function

* Cleaned up PER, added parser logging

* Cleaned up BER, added parser logging

* log messages

* Modified Stream_CheckAndLogRequiredLengthEx

* Allow custom format and options
* Add Stream_CheckAndLogRequiredLengthExVa for prepared va_list

* Improved Stream_CheckAndLogRequiredLength

* Now have log level adjustable
* Added function equivalents for existing logger
* Added a backtrace in case of a failure is detected

* Fixed public API input checks

libfreerdp/core/channels.c
@@ -117,7 +117,7 @@ BOOL freerdp_channel_process(freerdp* instance, wStream* s, UINT16 channelId, si
 	}
 	packetLength -= 8;
 
-	if (Stream_GetRemainingLength(s) < 8)
+	if (!Stream_CheckAndLogRequiredLength(TAG, s, 8))
 		return FALSE;
 
 	/* [MS-RDPBCGR] 3.1.5.2.2 Processing of Virtual Channel PDU
@@ -134,7 +134,7 @@ BOOL freerdp_channel_process(freerdp* instance, wStream* s, UINT16 channelId, si
 		         chunkLength);
 		return FALSE;
 	}
-	if (length < chunkLength)
+	if (!Stream_CheckAndLogRequiredLength(TAG, s, length))
 	{
 		WLog_ERR(TAG, "Expected %" PRIu32 " bytes, but have %" PRIdz, length, chunkLength);
 		return FALSE;
@@ -156,7 +156,10 @@ BOOL freerdp_channel_peer_process(freerdp_peer* client, wStream* s, UINT16 chann
 	UINT32 flags;
 	size_t chunkLength;
 
-	if (Stream_GetRemainingLength(s) < 8)
+	WINPR_ASSERT(client);
+	WINPR_ASSERT(s);
+
+	if (!Stream_CheckAndLogRequiredLength(TAG, s, 8))
 		return FALSE;
 
 	Stream_Read_UINT32(s, length);
@@ -199,7 +202,13 @@ BOOL freerdp_channel_peer_process(freerdp_peer* client, wStream* s, UINT16 chann
 		if (!rc)
 			return FALSE;
 	}
-	return Stream_SafeSeek(s, chunkLength);
+	if (!Stream_SafeSeek(s, chunkLength))
+	{
+		WLog_WARN(TAG, "Short PDU, need %" PRIuz " bytes, got %" PRIuz, chunkLength,
+		          Stream_GetRemainingLength(s));
+		return FALSE;
+	}
+	return TRUE;
 }
 
 static const WtsApiFunctionTable FreeRDP_WtsApiFunctionTable = {

commit f1a8872bc06b7a30815636cb55b5f9d7721d55df

Fixed #7821: Revert length check broken by #7796

libfreerdp/core/channels.c
@@ -134,7 +134,7 @@ BOOL freerdp_channel_process(freerdp* instance, wStream* s, UINT16 channelId, si
 		         chunkLength);
 		return FALSE;
 	}
-	if (!Stream_CheckAndLogRequiredLength(TAG, s, length))
+	if (length < chunkLength)
 	{
 		WLog_ERR(TAG, "Expected %" PRIu32 " bytes, but have %" PRIdz, length, chunkLength);
 		return FALSE;