46b44daf9921b036083afe6d75811ab6ddacd04b
Armin Novak <armin.novak@thincast.com>
2022-04-27 08:26:57 +0200

    Fix argument for winpr_RAND and winpr_RAND_pseudo
    Using void* eliminates all assumptions of which type the buffer is.
    This eliminates unnecessary warnings.

--- winpr/libwinpr/crypto/rand.c
+++ winpr/libwinpr/crypto/rand.c
@@ -34,7 +34,7 @@
 #include <mbedtls/hmac_drbg.h>
 #endif
 
-int winpr_RAND(BYTE* output, size_t len)
+int winpr_RAND(void* output, size_t len)
 {
 #if defined(WITH_OPENSSL)
 	if (len > INT_MAX)
@@ -53,7 +53,7 @@ int winpr_RAND(BYTE* output, size_t len)
 	return 0;
 }
 
-int winpr_RAND_pseudo(BYTE* output, size_t len)
+int winpr_RAND_pseudo(void* output, size_t len)
 {
 	return winpr_RAND(output, len);
 }
