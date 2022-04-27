4ae6b21ab6c38c6a6b4ff3b79ed7d41780fba059
Armin Novak <armin.novak@thincast.com>
2022-04-27 16:07:12 +0200

    Fixed use after free

--- libfreerdp/core/tscredentials.c
+++ libfreerdp/core/tscredentials.c
@@ -1,6 +1,6 @@
 /* ============================================================================================================
  * this file has been generated using
- * tools/asn_parser_generator.py --input=libfreerdp/core/credssp.asn1 --output-kind=impls
+ * ./tools/asn_parser_generator.py --input=libfreerdp/core/credssp.asn1 --output-kind=impls
  * --output=libfreerdp/core/tscredentials.c
  *
  * /!\ If you want to modify this file you'd probably better change asn_parser_generator.py or the
@@ -844,7 +844,9 @@ BOOL ber_read_nla_TSRemoteGuardPackageCred_array(wStream* s, TSRemoteGuardPackag
 			free(retItems);
 			return FALSE;
 		}
+		retItems = tmpRet;
 
+		retItems = tmpRet;
 		memcpy(&retItems[ret], item, sizeof(*item));
 		free(item);
 		ret++;
