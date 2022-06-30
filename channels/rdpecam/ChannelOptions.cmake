a59abddb12476fbd4ad08aa651d5bdbf063a02b5
akallabeth <akallabeth@posteo.net>
2022-06-30 11:11:29 +0200

    Fix rdpecam server channel naming

--- channels/rdpecam/ChannelOptions.cmake
+++ channels/rdpecam/ChannelOptions.cmake
@@ -1,14 +1,9 @@
 
-set(OPTION_DEFAULT OFF)
+set(OPTION_DEFAULT ON)
 set(OPTION_CLIENT_DEFAULT OFF)
 set(OPTION_SERVER_DEFAULT ON)
 
-define_channel_options(NAME "camera-device" TYPE "dynamic"
-	DESCRIPTION "Video Capture Virtual Channel Extension"
-	SPECIFICATIONS "[MS-RDPECAM]"
-	DEFAULT ${OPTION_DEFAULT})
-
-define_channel_options(NAME "camera-device-enumerator" TYPE "dynamic"
+define_channel_options(NAME "rdpecam" TYPE "dynamic"
 	DESCRIPTION "Video Capture Virtual Channel Extension"
 	SPECIFICATIONS "[MS-RDPECAM]"
 	DEFAULT ${OPTION_DEFAULT})
