f299c01cc09af893b4ac9b42f526d2dedb7a3e7e
Pascal Nowack <Pascal.Nowack@gmx.de>
2022-07-05 11:38:52 +0200

    freerdp/peer: Add APIs to get OS major and minor type strings

--- include/freerdp/peer.h
+++ include/freerdp/peer.h
@@ -183,6 +183,9 @@ extern "C"
 	FREERDP_API BOOL freerdp_peer_context_new_ex(freerdp_peer* client, const rdpSettings* settings);
 	FREERDP_API void freerdp_peer_context_free(freerdp_peer* client);
 
+	FREERDP_API const char* freerdp_peer_os_major_type_string(freerdp_peer* client);
+	FREERDP_API const char* freerdp_peer_os_minor_type_string(freerdp_peer* client);
+
 	FREERDP_API freerdp_peer* freerdp_peer_new(int sockfd);
 	FREERDP_API void freerdp_peer_free(freerdp_peer* client);
 	FREERDP_API BOOL freerdp_peer_set_local_and_hostname(freerdp_peer* client,
