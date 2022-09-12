33d22b6a2895f50e8cf17ae836cafeff973eeb22
garbb <garbb@users.noreply.github.com>
2022-09-12 15:10:17 -0700

    Update wf_event.c

--- client/Windows/wf_event.c
+++ client/Windows/wf_event.c
@@ -707,6 +707,8 @@ LRESULT CALLBACK wf_event_proc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
 		case WM_SETFOCUS:
 			DEBUG_KBD("getting focus %X", hWnd);
 
+			freerdp_settings_set_bool(wfc->common.context.settings, FreeRDP_SuspendInput, FALSE);
+
 			if (alt_ctrl_down())
 				g_flipping_in = TRUE;
 
@@ -715,6 +717,8 @@ LRESULT CALLBACK wf_event_proc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
 			break;
 
 		case WM_KILLFOCUS:
+			freerdp_settings_set_bool(wfc->common.context.settings, FreeRDP_SuspendInput, TRUE);
+
 			if (g_focus_hWnd == hWnd && wfc && !wfc->fullscreen)
 			{
 				DEBUG_KBD("loosing focus %X", hWnd);
