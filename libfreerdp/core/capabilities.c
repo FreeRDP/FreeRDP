8ff9af8f4d00588b6fc8d7262ea5583585b754b3
Armin Novak <armin.novak@thincast.com>
2022-09-14 11:29:20 +0200

    Fixed undefined shift behaviour
    BOOL must be cast to UINT32 before shifting to avoid undefined
    behaviour

--- libfreerdp/core/capabilities.c
+++ libfreerdp/core/capabilities.c
@@ -1806,7 +1806,7 @@ static void rdp_write_bitmap_cache_cell_info(wStream* s, BITMAP_CACHE_V2_CELL_IN
 	 * numEntries is in the first 31 bits, while the last bit (k)
 	 * is used to indicate a persistent bitmap cache.
 	 */
-	info = (cellInfo->numEntries | (cellInfo->persistent << 31));
+	info = (cellInfo->numEntries | (((UINT32)cellInfo->persistent) << 31));
 	Stream_Write_UINT32(s, info);
 }
 
