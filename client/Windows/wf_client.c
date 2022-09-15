c4dbb0f06421500987ada557b564afa071335706
garbb <e456h@yahoo.com>
2022-09-14 20:55:18 -0700

    add windows client assistance request control menu item

--- client/Windows/wf_client.c
+++ client/Windows/wf_client.c
@@ -272,10 +272,24 @@ static BOOL wf_pre_connect(freerdp* instance)
 	return TRUE;
 }
 
+static void wf_append_item_to_system_menu(HMENU hMenu, UINT fMask, UINT wID, const wchar_t* text,
+                                          wfContext* wfc)
+{
+	MENUITEMINFO item_info = { 0 };
+	item_info.fMask = fMask;
+	item_info.cbSize = sizeof(MENUITEMINFO);
+	item_info.wID = wID;
+	item_info.fType = MFT_STRING;
+	item_info.dwTypeData = _wcsdup(text);
+	item_info.cch = (UINT)_wcslen(text);
+	if (wfc)
+		item_info.dwItemData = (ULONG_PTR)wfc;
+	InsertMenuItem(hMenu, wfc->systemMenuInsertPosition++, TRUE, &item_info);
+}
+
 static void wf_add_system_menu(wfContext* wfc)
 {
 	HMENU hMenu;
-	MENUITEMINFO item_info;
 
 	if (wfc->fullscreen && !wfc->fullscreen_toggle)
 	{
@@ -288,20 +302,19 @@ static void wf_add_system_menu(wfContext* wfc)
 	}
 
 	hMenu = GetSystemMenu(wfc->hwnd, FALSE);
-	ZeroMemory(&item_info, sizeof(MENUITEMINFO));
-	item_info.fMask = MIIM_CHECKMARKS | MIIM_FTYPE | MIIM_ID | MIIM_STRING | MIIM_DATA;
-	item_info.cbSize = sizeof(MENUITEMINFO);
-	item_info.wID = SYSCOMMAND_ID_SMARTSIZING;
-	item_info.fType = MFT_STRING;
-	item_info.dwTypeData = _wcsdup(_T("Smart sizing"));
-	item_info.cch = (UINT)_wcslen(_T("Smart sizing"));
-	item_info.dwItemData = (ULONG_PTR)wfc;
-	InsertMenuItem(hMenu, 6, TRUE, &item_info);
+
+	wf_append_item_to_system_menu(hMenu,
+	                              MIIM_CHECKMARKS | MIIM_FTYPE | MIIM_ID | MIIM_STRING | MIIM_DATA,
+	                              SYSCOMMAND_ID_SMARTSIZING, L"Smart sizing", wfc);
 
 	if (wfc->common.context.settings->SmartSizing)
 	{
 		CheckMenuItem(hMenu, SYSCOMMAND_ID_SMARTSIZING, MF_CHECKED);
 	}
+
+	if (wfc->common.context.settings->RemoteAssistanceMode)
+		wf_append_item_to_system_menu(hMenu, MIIM_FTYPE | MIIM_ID | MIIM_STRING,
+		                              SYSCOMMAND_ID_REQUEST_CONTROL, L"Request control", wfc);
 }
 
 static WCHAR* wf_window_get_title(rdpSettings* settings)
@@ -1355,6 +1368,13 @@ static int wfreerdp_client_start(rdpContext* context)
 	hWndParent = (HWND)context->settings->ParentWindowId;
 	context->settings->EmbeddedWindow = (hWndParent) ? TRUE : FALSE;
 	wfc->hWndParent = hWndParent;
+
+	/* initial windows system item position where we will insert new menu item
+	 * after default 5 items (restore, move, size, minimize, maximize)
+	 * gets incremented each time wf_append_item_to_system_menu is called
+	 * or maybe could use GetMenuItemCount() to get initial item count ? */
+	wfc->systemMenuInsertPosition = 6;
+
 	wfc->hInstance = hInstance;
 	wfc->cursor = LoadCursor(NULL, IDC_ARROW);
 	wfc->icon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
