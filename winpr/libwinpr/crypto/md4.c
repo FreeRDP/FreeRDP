389817cb57da2a8c7323765622781fa1f8eb238d
akallabeth <akallabeth@posteo.net>
2022-11-14 10:33:08 +0100

    [winpr,crypto] fixed const correctness of md4/5

--- winpr/libwinpr/crypto/md4.c
+++ winpr/libwinpr/crypto/md4.c
@@ -72,7 +72,7 @@
  * their own translation unit avoids the problem.
  */
 #if defined(__i386__) || defined(__x86_64__) || defined(__vax__)
-#define SET(n) (*(winpr_MD4_u32plus*)&ptr[(n)*4])
+#define SET(n) (*(const winpr_MD4_u32plus*)&ptr[(n)*4])
 #define GET(n) SET(n)
 #else
 #define SET(n)                                                                                    \
