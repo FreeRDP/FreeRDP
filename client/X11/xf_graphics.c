30a056835f2c272c5cb43ce3004ab3ca78d8762b
Armin Novak <armin.novak@thincast.com>
2022-11-16 11:58:02 +0100

    [client,x11] Fixed #8397: Clamp scaled cursor sizes
    If a cursor is scaled, ensure the result size is at least 1x1 pixel
    wide.
    
    (cherry picked from commit 0ebc468d12a8d71cf68df92787e8b8fcf9208ba8)

--- client/X11/xf_graphics.c
+++ client/X11/xf_graphics.c
@@ -258,8 +258,8 @@ static BOOL xf_Pointer_GetCursorForCurrentScale(rdpContext* context, const rdpPo
 
 	xscale = (settings->SmartSizing ? xfc->scaledWidth / (double)settings->DesktopWidth : 1);
 	yscale = (settings->SmartSizing ? xfc->scaledHeight / (double)settings->DesktopHeight : 1);
-	xTargetSize = pointer->width * xscale;
-	yTargetSize = pointer->height * yscale;
+	xTargetSize = MAX(1, pointer->width * xscale);
+	yTargetSize = MAX(1, pointer->height * yscale);
 
 	WLog_DBG(TAG, "%s: scaled: %" PRIu32 "x%" PRIu32 ", desktop: %" PRIu32 "x%" PRIu32, __func__,
 	         xfc->scaledWidth, xfc->savedHeight, settings->DesktopWidth, settings->DesktopHeight);
