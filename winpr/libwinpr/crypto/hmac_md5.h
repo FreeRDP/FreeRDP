a40ade5abc6bccd6a97d4ee462b4efc65538edce
Armin Novak <armin.novak@thincast.com>
2022-11-21 08:52:46 +0100

    Fixed -Wincompatible-pointer-types

--- winpr/libwinpr/crypto/hmac_md5.h
+++ winpr/libwinpr/crypto/hmac_md5.h
@@ -12,8 +12,8 @@ typedef struct
 	WINPR_MD5_CTX ocontext;
 } WINPR_HMAC_MD5_CTX;
 
-void hmac_md5_init(WINPR_HMAC_MD5_CTX* ctx, unsigned char* key, size_t key_len);
-void hmac_md5_update(WINPR_HMAC_MD5_CTX* ctx, unsigned char* text, size_t text_len);
+void hmac_md5_init(WINPR_HMAC_MD5_CTX* ctx, const unsigned char* key, size_t key_len);
+void hmac_md5_update(WINPR_HMAC_MD5_CTX* ctx, const unsigned char* text, size_t text_len);
 void hmac_md5_finalize(WINPR_HMAC_MD5_CTX* ctx, unsigned char* hmac);
 
 #endif
