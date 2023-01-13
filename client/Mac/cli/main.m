a691b48bc0e0039684a09bf5fdd267e5461b0288
amazingfate <liujianfeng1994@gmail.com>
2023-01-13 18:09:49 +0800

    hide password for mac client

--- client/Mac/cli/main.m
+++ client/Mac/cli/main.m
@@ -10,5 +10,15 @@
 
 int main(int argc, const char *argv[])
 {
+	const char *ctemp;
+	for (int i = 0; i < argc; i++)
+	{
+		ctemp = argv[i];
+		if (memcmp(ctemp, "/p:", 3) == 0 || memcmp(ctemp, "-p:", 3) == 0)
+		{
+			memset((char *)ctemp + 3, '*', strlen(ctemp) - 3);
+		}
+	}
+
 	return NSApplicationMain(argc, argv);
 }
