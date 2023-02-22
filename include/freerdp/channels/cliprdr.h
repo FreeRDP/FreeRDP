303ad89377c7b18fe5cecee804f96e1b9ae35818
Armin Novak <anovak@thincast.com>
2023-02-22 07:13:38 +0100

    [clipboard] cleanup definitions
    * remove definitions for clipboard formats that are dynamically registered
    * fix length of converted clipboard string

--- include/freerdp/channels/cliprdr.h
+++ include/freerdp/channels/cliprdr.h
@@ -28,18 +28,6 @@
 
 #define CLIPRDR_SVC_CHANNEL_NAME "cliprdr"
 
-/**
- * Clipboard Formats
- */
-
-typedef enum
-{
-	CB_FORMAT_HTML = 0xD010,
-	CB_FORMAT_PNG = 0xD011,
-	CB_FORMAT_JPEG = 0xD012,
-	CB_FORMAT_GIF = 0xD013
-} CliprdrFormatType;
-
 /* CLIPRDR_HEADER.msgType */
 typedef enum
 {
