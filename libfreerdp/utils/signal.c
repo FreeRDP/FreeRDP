f357312584a5d0de10f1d9c5ce515f64a0edba89
Armin Novak <armin.novak@thincast.com>
2023-02-22 10:13:18 +0100

    [utils] term signal cleanup handlers
    add functions to register/unregister termination cleanup handlers

--- libfreerdp/utils/signal.c
+++ libfreerdp/utils/signal.c
@@ -28,7 +28,7 @@
 #include <freerdp/utils/signal.h>
 #include <freerdp/log.h>
 
-#define TAG FREERDP_TAG("utils")
+#define TAG FREERDP_TAG("utils.signal")
 
 #ifdef _WIN32
 
@@ -38,6 +38,15 @@ int freerdp_handle_signals(void)
 	return -1;
 }
 
+BOOL freerdp_add_signal_cleanup_handler(void* context, void (*fkt)(void*))
+{
+	return FALSE;
+}
+
+BOOL freerdp_del_signal_cleanup_handler(void* context, void (*fkt)(void*))
+{
+	return FALSE;
+}
 #else
 
 #include <pthread.h>
@@ -45,8 +54,62 @@ int freerdp_handle_signals(void)
 
 volatile sig_atomic_t terminal_needs_reset = 0;
 int terminal_fildes = 0;
-struct termios orig_flags;
-struct termios new_flags;
+struct termios orig_flags = { 0 };
+struct termios new_flags = { 0 };
+
+static BOOL handlers_registered = FALSE;
+static pthread_mutex_t signal_handler_lock = PTHREAD_MUTEX_INITIALIZER;
+
+typedef struct
+{
+	void* context;
+	freerdp_signal_handler_t handler;
+} cleanup_handler_t;
+
+static size_t cleanup_handler_count = 0;
+static cleanup_handler_t cleanup_handlers[20] = { 0 };
+
+static void lock(void)
+{
+	const int rc = pthread_mutex_lock(&signal_handler_lock);
+	if (rc != 0)
+		WLog_ERR(TAG, "[pthread_mutex_lock] failed with %s [%d]", strerror(rc), rc);
+}
+
+static void unlock(void)
+{
+	const int rc = pthread_mutex_unlock(&signal_handler_lock);
+	if (rc != 0)
+		WLog_ERR(TAG, "[pthread_mutex_lock] failed with %s [%d]", strerror(rc), rc);
+}
+
+static void term_handler(int signum)
+{
+	struct sigaction default_sigaction;
+	sigset_t this_mask;
+	static BOOL recursive = FALSE;
+
+	if (!recursive)
+	{
+		recursive = TRUE;
+		WLog_ERR(TAG, "Caught signal '%s' [%d]", strsignal(signum), signum);
+	}
+
+	if (terminal_needs_reset)
+		tcsetattr(terminal_fildes, TCSAFLUSH, &orig_flags);
+
+	lock();
+	for (size_t x = 0; x < cleanup_handler_count; x++)
+	{
+		const cleanup_handler_t empty = { 0 };
+		cleanup_handler_t* cur = &cleanup_handlers[x];
+		if (cur->handler)
+			cur->handler(signum, strsignal(signum), cur->context);
+		*cur = empty;
+	}
+	cleanup_handler_count = 0;
+	unlock();
+}
 
 static void fatal_handler(int signum)
 {
@@ -74,9 +137,10 @@ static void fatal_handler(int signum)
 	raise(signum);
 }
 
+static const int term_signals[] = { SIGINT, SIGKILL, SIGQUIT, SIGSTOP, SIGTERM };
+
 static const int fatal_signals[] = { SIGABRT,   SIGALRM, SIGBUS,  SIGFPE,  SIGHUP,  SIGILL,
-	                                 SIGINT,    SIGKILL, SIGQUIT, SIGSEGV, SIGSTOP, SIGTERM,
-	                                 SIGTSTP,   SIGTTIN, SIGTTOU, SIGUSR1, SIGUSR2,
+	                                 SIGSEGV,   SIGTSTP, SIGTTIN, SIGTTOU, SIGUSR1, SIGUSR2,
 #ifdef SIGPOLL
 	                                 SIGPOLL,
 #endif
@@ -92,34 +156,108 @@ static const int fatal_signals[] = { SIGABRT,   SIGALRM, SIGBUS,  SIGFPE,  SIGHU
 #endif
 	                                 SIGXCPU,   SIGXFSZ };
 
-int freerdp_handle_signals(void)
+static BOOL register_handlers(const int* signals, size_t count, void (*handler)(int))
 {
-	size_t signal_index;
-	sigset_t orig_set;
-	struct sigaction orig_sigaction;
-	struct sigaction fatal_sigaction;
-	WLog_DBG(TAG, "Registering signal hook...");
-	sigfillset(&(fatal_sigaction.sa_mask));
-	sigdelset(&(fatal_sigaction.sa_mask), SIGCONT);
-	pthread_sigmask(SIG_BLOCK, &(fatal_sigaction.sa_mask), &orig_set);
-	fatal_sigaction.sa_handler = fatal_handler;
-	fatal_sigaction.sa_flags = 0;
+	WINPR_ASSERT(signals || (count == 0));
+	WINPR_ASSERT(handler);
 
-	for (signal_index = 0; signal_index < ARRAYSIZE(fatal_signals); signal_index++)
+	sigset_t orig_set = { 0 };
+	struct sigaction saction = { 0 };
+
+	pthread_sigmask(SIG_BLOCK, &(saction.sa_mask), &orig_set);
+
+	sigfillset(&(saction.sa_mask));
+	sigdelset(&(saction.sa_mask), SIGCONT);
+
+	saction.sa_handler = handler;
+	saction.sa_flags = 0;
+
+	for (size_t x = 0; x < count; x++)
 	{
-		if (sigaction(fatal_signals[signal_index], NULL, &orig_sigaction) == 0)
+		struct sigaction orig_sigaction = { 0 };
+		if (sigaction(signals[x], NULL, &orig_sigaction) == 0)
 		{
 			if (orig_sigaction.sa_handler != SIG_IGN)
 			{
-				sigaction(fatal_signals[signal_index], &fatal_sigaction, NULL);
+				sigaction(signals[x], &saction, NULL);
 			}
 		}
 	}
 
 	pthread_sigmask(SIG_SETMASK, &orig_set, NULL);
+
+	return TRUE;
+}
+
+int freerdp_handle_signals(void)
+{
+	int rc = -1;
+
+	lock();
+
+	WLog_DBG(TAG, "Registering signal hook...");
+
+	if (!register_handlers(fatal_signals, ARRAYSIZE(fatal_signals), fatal_handler))
+		goto fail;
+	if (!register_handlers(term_signals, ARRAYSIZE(term_signals), term_handler))
+		goto fail;
+
 	/* Ignore SIGPIPE signal. */
 	signal(SIGPIPE, SIG_IGN);
-	return 0;
+	handlers_registered = TRUE;
+	rc = 0;
+fail:
+	unlock();
+	return rc;
+}
+
+BOOL freerdp_add_signal_cleanup_handler(void* context, freerdp_signal_handler_t handler)
+{
+	BOOL rc = FALSE;
+	lock();
+	if (handlers_registered)
+	{
+		if (cleanup_handler_count < ARRAYSIZE(cleanup_handlers))
+		{
+			cleanup_handler_t* cur = &cleanup_handlers[cleanup_handler_count++];
+			cur->context = context;
+			cur->handler = handler;
+		}
+		else
+			WLog_WARN(TAG, "Failed to register cleanup handler, only %" PRIuz " handlers supported",
+			          ARRAYSIZE(cleanup_handlers));
+	}
+	rc = TRUE;
+	unlock();
+	return rc;
+}
+
+BOOL freerdp_del_signal_cleanup_handler(void* context, freerdp_signal_handler_t handler)
+{
+	BOOL rc = FALSE;
+	lock();
+	if (handlers_registered)
+	{
+		for (size_t x = 0; x < cleanup_handler_count; x++)
+		{
+			cleanup_handler_t* cur = &cleanup_handlers[x];
+			if ((cur->context == context) && (cur->handler == handler))
+			{
+				const cleanup_handler_t empty = { 0 };
+				for (size_t y = x + 1; y < cleanup_handler_count - 1; y++)
+				{
+					*cur++ = cleanup_handlers[y];
+				}
+
+				*cur = empty;
+				cleanup_handler_count--;
+				break;
+			}
+		}
+	}
+	rc = TRUE;
+	unlock();
+	return rc;
 }
 
 #endif
