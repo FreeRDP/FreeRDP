c2cd5ed1f0296ec6265bab7eb2354cf17fee3434
Armin Novak <armin.novak@thincast.com>
2023-02-22 11:28:21 +0100

    [build] remove unused option WITH_DEBUG_X11_CLIPRDR

--- cmake/ConfigOptions.cmake
+++ cmake/ConfigOptions.cmake
@@ -140,7 +140,6 @@ option(WITH_DEBUG_SVC "Print static virtual channel debug messages." ${DEFAULT_D
 option(WITH_DEBUG_TRANSPORT "Print transport debug messages." ${DEFAULT_DEBUG_OPTION})
 option(WITH_DEBUG_TIMEZONE "Print timezone debug messages." ${DEFAULT_DEBUG_OPTION})
 option(WITH_DEBUG_WND "Print window order debug messages" ${DEFAULT_DEBUG_OPTION})
-option(WITH_DEBUG_X11_CLIPRDR "Print X11 clipboard redirection debug messages" ${DEFAULT_DEBUG_OPTION})
 option(WITH_DEBUG_X11_LOCAL_MOVESIZE "Print X11 Client local movesize debug messages" ${DEFAULT_DEBUG_OPTION})
 option(WITH_DEBUG_X11 "Print X11 Client debug messages" ${DEFAULT_DEBUG_OPTION})
 option(WITH_DEBUG_XV "Print XVideo debug messages" ${DEFAULT_DEBUG_OPTION})
