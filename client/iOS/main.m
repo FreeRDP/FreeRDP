8af35bd42ab42acac542a5e290ffb779a0ed521a
Armin Novak <armin.novak@thincast.com>
2023-03-01 08:34:52 +0100

    [client] add deprecation/experimental warnings

--- client/iOS/main.m
+++ client/iOS/main.m
@@ -9,9 +9,11 @@
  */
 
 #import <UIKit/UIKit.h>
+#include <freerdp/client/cmdline.h>
 
 int main(int argc, char *argv[])
 {
+	freerdp_client_warn_deprecated(argc, argv);
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 	int retVal = UIApplicationMain(argc, argv, nil, nil);
 	[pool release];
