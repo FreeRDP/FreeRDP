7da875a1df0b498deadc58c54a677cf754579187
Armin Novak <armin.novak@thincast.com>
2023-03-01 16:32:13 +0100

    [client,x11] log location for wrapped X11 calls

--- client/X11/xf_utils.h
+++ client/X11/xf_utils.h
@@ -23,21 +23,54 @@
 
 #include <X11/Xlib.h>
 
-int LogTagAndXGetWindowProperty(const char* tag, Display* display, Window w, Atom property,
-                                long long_offset, long long_length, Bool delete, Atom req_type,
-                                Atom* actual_type_return, int* actual_format_return,
-                                unsigned long* nitems_return, unsigned long* bytes_after_return,
-                                unsigned char** prop_return);
-int LogDynAndXGetWindowProperty(wLog* log, Display* display, Window w, Atom property,
-                                long long_offset, long long_length, Bool delete, Atom req_type,
-                                Atom* actual_type_return, int* actual_format_return,
-                                unsigned long* nitems_return, unsigned long* bytes_after_return,
-                                unsigned char** prop_return);
+#define LogTagAndXGetWindowProperty(tag, display, w, property, long_offset, long_length, delete,   \
+                                    req_type, actual_type_return, actual_format_return,            \
+                                    nitems_return, bytes_after_return, prop_return)                \
+	LogTagAndXGetWindowProperty_ex((tag), __FILE__, __FUNCTION__, __LINE__, (display), (w),        \
+	                               (property), (long_offset), (long_length), (delete), (req_type), \
+	                               (actual_type_return), (actual_format_return), (nitems_return),  \
+	                               (bytes_after_return), (prop_return))
+int LogTagAndXGetWindowProperty_ex(const char* tag, const char* file, const char* fkt, size_t line,
+                                   Display* display, Window w, Atom property, long long_offset,
+                                   long long_length, Bool delete, Atom req_type,
+                                   Atom* actual_type_return, int* actual_format_return,
+                                   unsigned long* nitems_return, unsigned long* bytes_after_return,
+                                   unsigned char** prop_return);
 
-int LogTagAndXChangeProperty(const char* tag, Display* display, Window w, Atom property, Atom type,
-                             int format, int mode, _Xconst unsigned char* data, int nelements);
-int LogDynAndXChangeProperty(wLog* log, Display* display, Window w, Atom property, Atom type,
-                             int format, int mode, _Xconst unsigned char* data, int nelements);
+#define LogDynAndXGetWindowProperty(log, display, w, property, long_offset, long_length, delete,   \
+                                    req_type, actual_type_return, actual_format_return,            \
+                                    nitems_return, bytes_after_return, prop_return)                \
+	LogDynAndXGetWindowProperty_ex((log), __FILE__, __FUNCTION__, __LINE__, (display), (w),        \
+	                               (property), (long_offset), (long_length), (delete), (req_type), \
+	                               (actual_type_return), (actual_format_return), (nitems_return),  \
+	                               (bytes_after_return), (prop_return))
+int LogDynAndXGetWindowProperty_ex(wLog* log, const char* file, const char* fkt, size_t line,
+                                   Display* display, Window w, Atom property, long long_offset,
+                                   long long_length, Bool delete, Atom req_type,
+                                   Atom* actual_type_return, int* actual_format_return,
+                                   unsigned long* nitems_return, unsigned long* bytes_after_return,
+                                   unsigned char** prop_return);
 
-int LogTagAndXDeleteProperty(const char* tag, Display* display, Window w, Atom property);
-int LogDynAndXDeleteProperty(wLog* log, Display* display, Window w, Atom property);
+#define LogTagAndXChangeProperty(tag, display, w, property, type, format, mode, data, nelements) \
+	LogTagAndXChangeProperty_ex((tag), __FILE__, __FUNCTION__, __LINE__, (display), (w),         \
+	                            (property), (type), (format), (mode), (data), (nelements))
+int LogTagAndXChangeProperty_ex(const char* tag, const char* file, const char* fkt, size_t line,
+                                Display* display, Window w, Atom property, Atom type, int format,
+                                int mode, _Xconst unsigned char* data, int nelements);
+
+#define LogDynAndXChangeProperty(log, display, w, property, type, format, mode, data, nelements) \
+	LogDynAndXChangeProperty_ex((log), __FILE__, __FUNCTION__, __LINE__, (display), (w),         \
+	                            (property), (type), (format), (mode), (data), (nelements))
+int LogDynAndXChangeProperty_ex(wLog* log, const char* file, const char* fkt, size_t line,
+                                Display* display, Window w, Atom property, Atom type, int format,
+                                int mode, _Xconst unsigned char* data, int nelements);
+
+#define LogTagAndXDeleteProperty(tag, display, w, property) \
+	LogTagAndXDeleteProperty_ex((tag), __FILE__, __FUNCTION__, __LINE__, (display), (w), (property))
+int LogTagAndXDeleteProperty_ex(const char* tag, const char* file, const char* fkt, size_t line,
+                                Display* display, Window w, Atom property);
+
+#define LogDynAndXDeleteProperty(log, display, w, property) \
+	LogDynAndXDeleteProperty_ex((log), __FILE__, __FUNCTION__, __LINE__, (display), (w), (property))
+int LogDynAndXDeleteProperty_ex(wLog* log, const char* file, const char* fkt, size_t line,
+                                Display* display, Window w, Atom property);
