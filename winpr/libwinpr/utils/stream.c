b566f97a5ee5e266b7858d40e0527149306f4910
Johannes Kauffmann <johanneskauffmann@hotmail.com>
2023-04-02 17:35:45 +0200

    libwinpr: use standard __func__

--- winpr/libwinpr/utils/stream.c
+++ winpr/libwinpr/utils/stream.c
@@ -34,7 +34,7 @@
 	{                                                                                      \
 		if (!(cond))                                                                       \
 		{                                                                                  \
-			WLog_FATAL(STREAM_TAG, "%s [%s:%s:%" PRIuz "]", #cond, __FILE__, __FUNCTION__, \
+			WLog_FATAL(STREAM_TAG, "%s [%s:%s:%" PRIuz "]", #cond, __FILE__, __func__,     \
 			           (size_t)__LINE__);                                                  \
 			winpr_log_backtrace(STREAM_TAG, WLOG_FATAL, 20);                               \
 			abort();                                                                       \
