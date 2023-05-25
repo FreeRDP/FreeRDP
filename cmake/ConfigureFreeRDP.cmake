14942fe6c50a3a718d64b8f5a5a36baefea1baaf
akallabeth <akallabeth@posteo.net>
2023-05-25 15:25:06 +0200

    [cmake] unify RPATH settings and client deps detection

---
+++ cmake/ConfigureFreeRDP.cmake
@@ -0,0 +1,9 @@
+find_package(WinPR 3 REQUIRED)
+include_directories(${WinPR_INCLUDE_DIR})
+
+find_package(FreeRDP 3 REQUIRED)
+include_directories(${FreeRDP_INCLUDE_DIR})
+
+find_package(FreeRDP-Client 3 REQUIRED)
+include_directories(${FreeRDP-Client_INCLUDE_DIR})
+
