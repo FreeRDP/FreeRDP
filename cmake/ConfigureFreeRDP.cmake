a1d7a12a867437c3fd98273f27de96b2d9a9ac34
akallabeth <akallabeth@posteo.net>
2023-05-26 11:03:06 +0200

    [cmake] only check client deps for standalone builds

--- cmake/ConfigureFreeRDP.cmake
+++ cmake/ConfigureFreeRDP.cmake
@@ -1,9 +1,10 @@
-find_package(WinPR 3 REQUIRED)
-include_directories(${WinPR_INCLUDE_DIR})
+if (NOT FREERDP_UNIFIED_BUILD)
+    find_package(WinPR 3 REQUIRED)
+    include_directories(${WinPR_INCLUDE_DIR})
 
-find_package(FreeRDP 3 REQUIRED)
-include_directories(${FreeRDP_INCLUDE_DIR})
+    find_package(FreeRDP 3 REQUIRED)
+    include_directories(${FreeRDP_INCLUDE_DIR})
 
-find_package(FreeRDP-Client 3 REQUIRED)
-include_directories(${FreeRDP-Client_INCLUDE_DIR})
-
+    find_package(FreeRDP-Client 3 REQUIRED)
+    include_directories(${FreeRDP-Client_INCLUDE_DIR})
+endif()
