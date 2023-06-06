4d8d06a27c4e1ec37344c6307875da03049d9649
Sergey Bronnikov <estetus@gmail.com>
2023-06-06 22:06:40 +0300

    [libfreerdp] Fix TestFuzzCommonAssistanceHexStringToBin
    Test breaks contract in freerdp_assistance_hex_string_to_bin():
    function expects zero-terminated string. Patch fixes that.

--- libfreerdp/common/test/TestFuzzCommonAssistanceHexStringToBin.c
+++ libfreerdp/common/test/TestFuzzCommonAssistanceHexStringToBin.c
@@ -2,8 +2,15 @@
 
 int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
 {
-	BYTE* pass = freerdp_assistance_hex_string_to_bin((void*)Data, &Size);
+	char* buf = calloc(Size + 1, sizeof(char));
+	if (buf == NULL)
+		return 0;
+	memcpy(buf, Data, Size);
+	buf[Size] = '\0';
+
+	BYTE* pass = freerdp_assistance_hex_string_to_bin((void*)buf, &Size);
 	free(pass);
+	free(buf);
 
 	return 0;
 }
