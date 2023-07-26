335764ee986cc30769597ececdb2ed4f6fb82dc6
Armin Novak <anovak@thincast.com>
2023-07-26 15:55:06 +0200

    [cmake] build using external lodepng

---
+++ cmake/Findlodepng.cmake
@@ -0,0 +1,19 @@
+# - Try to find lodepng
+# Once done this will define
+#  lodepng_FOUND - cJSON was found
+#  lodepng_INCLUDE_DIRS - cJSON include directories
+#  lodepng_LIBRARIES - cJSON libraries for linking
+
+find_path(lodepng_INCLUDE_DIR
+    NAMES lodepng.h)
+
+find_library(lodepng_LIBRARY
+    NAMES lodepng)
+
+if (lodepng_INCLUDE_DIR AND lodepng_LIBRARY)
+    set(lodepng_FOUND ON)
+    set(lodepng_INCLUDE_DIRS ${lodepng_INCLUDE_DIR})
+    set(lodepng_LIBRARIES ${lodepng_LIBRARY})
+endif()
+
+mark_as_advanced(lodepng_INCLUDE_DIRS lodepng_LIBRARIES)
