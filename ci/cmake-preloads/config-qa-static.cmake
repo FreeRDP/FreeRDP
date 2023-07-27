8ad6fcbb5a1407ab746e2883cadca6f324e61526
Armin Novak <anovak@thincast.com>
2023-07-27 18:07:10 +0200

    [ci] add code quality preload files

---
+++ ci/cmake-preloads/config-qa-static.cmake
@@ -0,0 +1,4 @@
+set (WITH_SERVER ON CACHE BOOL "qa default")
+set (WITH_SAMPLE ON CACHE BOOL "qa default")
+set (WITH_VERBOSE_WINPR_ASSERT OFF CACHE BOOL "qa default")
+set (BUILD_SHARED_LIBS OFF CACHE BOOL "qa default")
