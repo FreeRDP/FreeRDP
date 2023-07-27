850440898576b156b9bf73604dadfe367ddcf250
Eduard Florin Dumitru <eduard.dumitru@uipath.com>
2023-07-27 18:52:29 +0200

    - unit test that the Git hash is present in the InformationalVersion

---
+++ UiPath.FreeRdpClient/UiPath.FreeRdpClient.Tests/InformationalVersionTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+using UiPath.Rdp;
+using UiPath.SessionTools;
+
+namespace UiPath.FreeRdp.Tests;
+
+[Trait("Category", "Build")]
+public class InformationalVersionTests
+{
+    [Theory]
+    [InlineData(typeof(IFreeRdpClient))]
+    [InlineData(typeof(Wts))]
+    public void InformationalVersionIsCorrect(Type specimen)
+    {
+        var assembly = specimen.Assembly;
+        var attribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+        attribute.ShouldNotBeNull();
+
+        var match = Regex.Match(attribute.InformationalVersion);
+        match.Success.ShouldBeTrue();
+    }
+
+    [Theory]
+    [InlineData("1.2.3+6e78f515d09a6f9fd226bb1e81276b8ab4228961")]
+    [InlineData("1+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")]
+    [InlineData("1.2.3+0000000000000000000000000000000000000000")]
+    public void Regex_ShouldMatch_GivenValidInformationalVersion(string input)
+    => Regex.Match(input).Success.ShouldBeTrue();
+
+    [Theory]
+    [InlineData("1.2.3")]
+    [InlineData("+6e78f515d09a6f9fd226bb1e81276b8ab4228961")]
+    [InlineData("1.2.3+ge78f515d09a6f9fd226bb1e81276b8ab4228961")]
+    [InlineData("1.2.3+6e78f515d09a6f9fd226bb1e81")]
+    public void Regex_ShouldNotMatch_GivenInvalidInformationalVersion(string input)
+    => Regex.Match(input).Success.ShouldBeFalse();
+
+    private static readonly Regex Regex = new(@"^(?:[^+]+)\+(?:[0-9a-fA-F]{40})$");
+}
