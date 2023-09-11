88dd68153c834e1d5f59beb59496bfd1bf9697a8
Wang Mingyu <wangmy@cn.fujitsu.com>
2023-09-11 00:31:54 +0000

    Fixes clang error error: incompatible function pointer types assigning to 'OBJECT_NEW_FN' (aka 'void *(*)(void *)') from 'void *(const void *)' [-Wincompatible-function-pointer-types] |         obj->fnObjectNew = uwac_event_clone; |                          ^ ~~~~~~~~~~~~~~~~
    Signed-off-by: Wang Mingyu <wangmy@cn.fujitsu.com>

--- client/Wayland/wlfreerdp.c
+++ client/Wayland/wlfreerdp.c
@@ -598,7 +598,7 @@ static void wlf_client_free(freerdp* instance, rdpContext* context)
 	DeleteCriticalSection(&wlf->critical);
 }
 
-static void* uwac_event_clone(const void* val)
+static void* uwac_event_clone(void* val)
 {
 	UwacEvent* copy;
 	const UwacEvent* ev = (const UwacEvent*)val;
