0f3e9aac6b3f37c8558d4746e0c81e7c32d66786
akallabeth <akallabeth@posteo.net>
2023-09-20 16:34:04 +0200

    [client,mac] fix casting in main()

--- client/Mac/cli/main.m
+++ client/Mac/cli/main.m
@@ -9,19 +9,18 @@
 #import <Cocoa/Cocoa.h>
 #include <freerdp/client/cmdline.h>
 
-int main(int argc, const char *argv[])
+int main(int argc, char *argv[])
 {
-	const char *ctemp;
-
 	freerdp_client_warn_deprecated(argc, argv);
 	for (int i = 0; i < argc; i++)
 	{
-		ctemp = argv[i];
+		char *ctemp = argv[i];
 		if (memcmp(ctemp, "/p:", 3) == 0 || memcmp(ctemp, "-p:", 3) == 0)
 		{
-			memset((char *)ctemp + 3, '*', strlen(ctemp) - 3);
+			memset(ctemp + 3, '*', strlen(ctemp) - 3);
 		}
 	}
 
-	return NSApplicationMain(argc, argv);
+	const char **cargv = (const char **)argv;
+	return NSApplicationMain(argc, cargv);
 }
