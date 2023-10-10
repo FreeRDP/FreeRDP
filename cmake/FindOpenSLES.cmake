046767b1957821bfc82808d86c353589278a7cd5
akallabeth <akallabeth@posteo.net>
2023-10-10 19:34:38 +0200

    [opensles] moved detection to modules
    * Rename variables to match package name
    * Move find_package to where it is actually used

--- cmake/FindOpenSLES.cmake
+++ cmake/FindOpenSLES.cmake
@@ -1,30 +1,30 @@
 # - Find OpenSLES
 # Find the OpenSLES includes and libraries
 #
-#  OPENSLES_INCLUDE_DIR - where to find dsound.h
-#  OPENSLES_LIBRARIES   - List of libraries when using dsound.
-#  OPENSLES_FOUND       - True if dsound found.
+#  OpenSLES_INCLUDE_DIR - where to find dsound.h
+#  OpenSLES_LIBRARIES   - List of libraries when using dsound.
+#  OpenSLES_FOUND       - True if dsound found.
 
 get_property(_FIND_LIBRARY_USE_LIB64_PATHS GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS)
 set_property(GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS 1)
 
-find_path(OPENSLES_INCLUDE_DIR SLES/OpenSLES.h)
+find_path(OpenSLES_INCLUDE_DIR SLES/OpenSLES.h)
 
-find_library(OPENSLES_LIBRARY NAMES OpenSLES)
+find_library(OpenSLES_LIBRARY NAMES OpenSLES)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(OPENSLES DEFAULT_MSG
-	OPENSLES_INCLUDE_DIR OPENSLES_LIBRARY)
+find_package_handle_standard_args(OpenSLES DEFAULT_MSG
+	OpenSLES_INCLUDE_DIR OpenSLES_LIBRARY)
 						
-if(OPENSLES_FOUND)
-	set(OPENSLES_INCLUDE_DIRS ${OPENSLES_INCLUDE_DIR})
-	set(OPENSLES_LIBRARIES ${OPENSLES_LIBRARY})
-else(OPENSLES_FOUND)
+if(OpenSLES_FOUND)
+	set(OpenSLES_INCLUDE_DIRS ${OpenSLES_INCLUDE_DIR})
+	set(OpenSLES_LIBRARIES ${OpenSLES_LIBRARY})
+else(OpenSLES_FOUND)
 	if (OpenSLES_FIND_REQUIRED)
-		message(FATAL_ERROR "Could NOT find OPENSLES")
+		message(FATAL_ERROR "Could NOT find OpenSLES")
 	endif()
-endif(OPENSLES_FOUND)
+endif(OpenSLES_FOUND)
 
-mark_as_advanced(OPENSLES_INCLUDE_DIR OPENSLES_LIBRARY)
+mark_as_advanced(OpenSLES_INCLUDE_DIR OpenSLES_LIBRARY)
 
 set_property(GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS ${_FIND_LIBRARY_USE_LIB64_PATHS})
