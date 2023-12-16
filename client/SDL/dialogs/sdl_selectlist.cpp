d7768645f31dc1a8395fb46696d4bfb0ff98a88e
Armin Novak <anovak@thincast.com>
2023-12-16 17:53:35 +0100

    [client,sdl] improve dialog button highlighting
    move state to buttons or button list

--- client/SDL/dialogs/sdl_selectlist.cpp
+++ client/SDL/dialogs/sdl_selectlist.cpp
@@ -10,7 +10,9 @@ SdlSelectList::SdlSelectList(const std::string& title, const std::vector<std::st
 
 	const size_t total_height = labels.size() * (widget_height + vpadding) + vpadding;
 	_window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-	                           widget_width, total_height + widget_height, 0);
+	                           widget_width, total_height + widget_height,
+	                           SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_MOUSE_FOCUS |
+	                               SDL_WINDOW_INPUT_FOCUS);
 	if (_window == nullptr)
 	{
 		widget_log_error(-1, "SDL_CreateWindow");
@@ -33,9 +35,10 @@ SdlSelectList::SdlSelectList(const std::string& title, const std::vector<std::st
 
 			const std::vector<int> buttonids = { INPUT_BUTTON_ACCEPT, INPUT_BUTTON_CANCEL };
 			const std::vector<std::string> buttonlabels = { "accept", "cancel" };
-			_buttons.populate(_renderer, buttonlabels, buttonids, static_cast<Sint32>(total_height),
-			                  static_cast<Sint32>(widget_width / 2),
-			                  static_cast<Sint32>(widget_height));
+			_buttons.populate(
+			    _renderer, buttonlabels, buttonids, widget_width, static_cast<Sint32>(total_height),
+			    static_cast<Sint32>(widget_width / 2), static_cast<Sint32>(widget_height));
+			_buttons.set_highlight(0);
 		}
 	}
 }
@@ -116,12 +119,7 @@ int SdlSelectList::run()
 							throw;
 					}
 
-					auto button = _buttons.get_selected(event.button);
-					if (button)
-					{
-						if (!button->highlight(_renderer))
-							throw;
-					}
+					_buttons.set_mouseover(event.button.x, event.button.y);
 				}
 				break;
 				case SDL_MOUSEBUTTONDOWN:
