ddc4b7a42fd81d17768380bc3c5cbeaa479daf40
Armin Novak <anovak@thincast.com>
2023-12-18 09:06:21 +0100

    [cmake] set preload verbose makefile

--- ci/cmake-preloads/config-qa-static.cmake
+++ ci/cmake-preloads/config-qa-static.cmake
@@ -1,3 +1,5 @@
+message("PRELOADING cache")
+set (CMAKE_VERBOSE_MAKEFILE ON CACHE BOOL "preload")
 set (WITH_SERVER ON CACHE BOOL "qa default")
 set (WITH_SAMPLE ON CACHE BOOL "qa default")
 set (WITH_VERBOSE_WINPR_ASSERT OFF CACHE BOOL "qa default")
