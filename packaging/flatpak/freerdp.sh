f7640288f7274df329954ffa3ea6ebe5afb776ce
Armin Novak <anovak@thincast.com>
2023-12-21 21:50:18 +0100

    [ci,flatpak] update build instructions

--- packaging/flatpak/freerdp.sh
+++ packaging/flatpak/freerdp.sh
@@ -1,18 +1,34 @@
 #!/bin/sh
-if [ -z $XDG_SESSION_TYPE ];
+
+if [ -z ${FREERDP_SDL_OFF} ];
 then
-	echo "XDG_SESSION_TYPE undefined"
-	exit -1
-elif [ "$XDG_SESSION_TYPE" = "wayland" ];
-then
-	echo "wayland $(which wlfreerdp)"
-	wlfreerdp $@
-elif [ "$XDG_SESSION_TYPE" = "x11" ];
-then
-	echo "X11 $(which xfreerdp)"
-	xfreerdp $@
+	echo "SDL $(which sdl-freerdp)"
+	sdl-freerdp $@
 	exit $rc
 else
-	echo "XDG_SESSION_TYPE $XDG_SESSION_TYPE not handled"
-	exit -1
+	if [ -z $XDG_SESSION_TYPE ];
+	then
+		echo "XDG_SESSION_TYPE undefined"
+		exit -1
+	elif [ "$XDG_SESSION_TYPE" = "wayland" ];
+	then
+		if [ -z $FREERDP_WAYLAND_OFF ];
+		then
+			echo "wayland $(which wlfreerdp)"
+			wlfreerdp $@
+			exit $rc
+		else
+			echo "X11 $(which xfreerdp)"
+			xfreerdp $@
+			exit $rc
+		fi
+	elif [ "$XDG_SESSION_TYPE" = "x11" ];
+	then
+		echo "X11 $(which xfreerdp)"
+		xfreerdp $@
+		exit $rc
+	else
+		echo "XDG_SESSION_TYPE $XDG_SESSION_TYPE not handled"
+		exit -1
+	fi
 fi
