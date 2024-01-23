aa9a9564acb1c7e8975a340ef38c85dd12efd08e
akallabeth <akallabeth@posteo.net>
2024-01-23 14:57:26 +0100

    [ci] enable clang-tidy for ci builds

--- ci/cmake-preloads/config-qa-static.cmake
+++ ci/cmake-preloads/config-qa-static.cmake
@@ -4,3 +4,40 @@ set (WITH_SERVER ON CACHE BOOL "qa default")
 set (WITH_SAMPLE ON CACHE BOOL "qa default")
 set (WITH_VERBOSE_WINPR_ASSERT OFF CACHE BOOL "qa default")
 set (BUILD_SHARED_LIBS OFF CACHE BOOL "qa default")
+
+set (BUILD_WITH_CLANG_TIDY OFF CACHE BOOL "qa default")
+find_program(CLANG_EXE
+    NAMES
+        clang-20
+        clang-19
+        clang-18
+        clang-17
+        clang-16
+        clang-15
+        clang-14
+        clang-13
+        clang-12
+        clang-11
+        clang-10
+        clang
+    REQUIRED
+)
+set (CMAKE_C_COMPILER "${CLANG_EXE}" CACHE STRING "qa default")
+
+find_program(CLANG_XX_EXE
+    NAMES
+        clang++-20
+        clang++-19
+        clang++-18
+        clang++-17
+        clang++-16
+        clang++-15
+        clang++-14
+        clang++-13
+        clang++-12
+        clang++-11
+        clang++-10
+        clang++
+    REQUIRED
+)
+set (CMAKE_CXX_COMPILER "${CLANG_XX_EXE}" CACHE STRING "qa default")
