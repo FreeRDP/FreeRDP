8d3072f739fb015f8c2f5e073eb21d982bfa4d06
akallabeth <akallabeth@posteo.net>
2024-01-23 14:57:26 +0100

    [ci] enable clang-tidy for ci builds

--- ci/cmake-preloads/config-qa.cmake
+++ ci/cmake-preloads/config-qa.cmake
@@ -14,5 +14,40 @@ set (WITH_FFMPEG ON CACHE BOOL "qa default")
 set (WITH_SANITIZE_ADDRESS ON CACHE BOOL "qa default")
 set (CMAKE_C_FLAGS "-Weverything -Wno-exit-time-destructors -Wno-cast-align -Wno-documentation -Wno-documentation-unknown-command -Wno-padded -Wno-covered-switch-default -Wno-declaration-after-statement" CACHE STRING "qa default")
 set (CMAKE_CXX_FLAGS "-Weverything -Wno-exit-time-destructors -Wno-cast-align -Wno-documentation -Wno-documentation-unknown-command -Wno-padded -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-covered-switch-default -Wno-declaration-after-statement" CACHE STRING "qa default")
-set (CMAKE_C_COMPILER "/usr/bin/clang-10" CACHE STRING "qa default")
-set (CMAKE_CXX_COMPILER "/usr/bin/clang++-10" CACHE STRING "qa default")
+
+set (BUILD_WITH_CLANG_TIDY ON CACHE BOOL "qa default")
+find_program(CLANG_EXE
+    NAMES
+        clang-20
+        clang-19
+        clang-18
+        clang-17
+        clang-16
+        clang-15
+        clang-14
+        clang-13
+        clang-12
+        clang-11
+        clang-10
+        clang
+    REQUIRED
+)
+set (CMAKE_C_COMPILER "${CLANG_EXE}" CACHE STRING "qa default")
+
+find_program(CLANG_XX_EXE
+    NAMES
+        clang++-20
+        clang++-19
+        clang++-18
+        clang++-17
+        clang++-16
+        clang++-15
+        clang++-14
+        clang++-13
+        clang++-12
+        clang++-11
+        clang++-10
+        clang++
+    REQUIRED
+)
+set (CMAKE_CXX_COMPILER "${CLANG_XX_EXE}" CACHE STRING "qa default")
