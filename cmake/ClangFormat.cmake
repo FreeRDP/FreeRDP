fc47a019dfe71beee7ee33c7d6618cc8bbe502d1
akallabeth <akallabeth@posteo.net>
2024-01-24 15:08:33 +0100

    [clang] unify tool detection

--- cmake/ClangFormat.cmake
+++ cmake/ClangFormat.cmake
@@ -1,52 +1,12 @@
 # get all project files
 file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.c *.h *.m *.java)
-# minimum version required
-set(_CLANG_FORMAT_MINIMUM_VERSION 10.0.0)
 
-find_program(CLANG_FORMAT
-	NAMES
-	clang-format-20
-	clang-format-19
-	clang-format-18
-	clang-format-17
-	clang-format-16
-	clang-format-15
-	clang-format-14
-	clang-format-13
-	clang-format-12
-	clang-format-11
-	clang-format-10
-	clang-format
-	)
+include(ClangDetectTool)
+clang_detect_tool(CLANG_FORMAT clang-format)
 
 if (NOT CLANG_FORMAT)
 	message(WARNING "clang-format not found in path! code format target not available.")
 else()
-	execute_process(
-		COMMAND ${CLANG_FORMAT} "--version"
-		OUTPUT_VARIABLE _CLANG_FORMAT_VERSION
-		RESULT_VARIABLE _CLANG_FORMAT_VERSION_FAILED
-		)
-
-	if (_CLANG_FORMAT_VERSION_FAILED)
-		message(WARNING "A problem was encounterd with ${CLANG_FORMAT}")
-		return()
-	endif()
-
-	string(REGEX MATCH "([7-9]|[1-9][0-9])\\.[0-9]\\.[0-9]" CLANG_FORMAT_VERSION
-		"${_CLANG_FORMAT_VERSION}")
-
-	if (NOT CLANG_FORMAT_VERSION)
-		message(WARNING "problem parsing clang-format version for ${CLANG_FORMAT}")
-		return()
-	endif()
-
-	if (${CLANG_FORMAT_VERSION} VERSION_LESS ${_CLANG_FORMAT_MINIMUM_VERSION})
-		message(WARNING "clang-format version ${CLANG_FORMAT_VERSION} not supported")
-		message(WARNING "Minimum version required: ${_CLANG_FORMAT_MINIMUM_VERSION}")
-		return()
-	endif()
-
 	add_custom_target(
 			clangformat
 			COMMAND ${CLANG_FORMAT}
--- cmake/ClangTidy.cmake
+++ cmake/ClangTidy.cmake
@@ -1,22 +1,9 @@
 option(BUILD_WITH_CLANG_TIDY "Build with clang-tidy for extra warnings" OFF)
 
 if (BUILD_WITH_CLANG_TIDY)
-    find_program(CLANG_TIDY_EXE
-        NAMES
-            clang-tidy-20
-            clang-tidy-19
-            clang-tidy-18
-            clang-tidy-17
-            clang-tidy-16
-            clang-tidy-15
-            clang-tidy-14
-            clang-tidy-13
-            clang-tidy-12
-            clang-tidy-11
-            clang-tidy-10
-            clang-tidy
-        REQUIRED
-    )
+    include(ClangDetectTool)
+    clang_detect_tool(CLANG_TIDY_EXE clang-tidy REQUIRED)
+
     set(CLANG_TIDY_COMMAND "${CLANG_TIDY_EXE}" --config-file=${CMAKE_SOURCE_DIR}/.clang-tidy)
 
     set(CMAKE_C_CLANG_TIDY "${CLANG_TIDY_COMMAND}" --extra-arg=-std=gnu11)
