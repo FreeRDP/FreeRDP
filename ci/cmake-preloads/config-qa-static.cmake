3a5cf5af9cf2aea4f34a3dc85334a66c7d275f3d
akallabeth <akallabeth@posteo.net>
2024-01-24 15:08:33 +0100

    [clang] unify tool detection
    (cherry picked from commit fc47a019dfe71beee7ee33c7d6618cc8bbe502d1)

--- ci/cmake-preloads/config-qa-static.cmake
+++ ci/cmake-preloads/config-qa-static.cmake
@@ -1,4 +1,15 @@
+message("PRELOADING cache")
+set (CMAKE_VERBOSE_MAKEFILE ON CACHE BOOL "preload")
 set (WITH_SERVER ON CACHE BOOL "qa default")
 set (WITH_SAMPLE ON CACHE BOOL "qa default")
 set (WITH_VERBOSE_WINPR_ASSERT OFF CACHE BOOL "qa default")
 set (BUILD_SHARED_LIBS OFF CACHE BOOL "qa default")
+
+set (BUILD_WITH_CLANG_TIDY OFF CACHE BOOL "qa default")
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/ClangDetectTool.cmake)
+clang_detect_tool(CLANG_EXE clang REQUIRED)
+clang_detect_tool(CLANG_XX_EXE clang++ REQUIRED)
+
+set (CMAKE_C_COMPILER "${CLANG_EXE}" CACHE STRING "qa default")
+set (CMAKE_CXX_COMPILER "${CLANG_XX_EXE}" CACHE STRING "qa default")
