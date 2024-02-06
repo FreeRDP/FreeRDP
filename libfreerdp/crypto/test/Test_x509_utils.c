ac93fc531502ab44fe956f2c4b75e5df8637542e
David Benjamin <davidben@google.com>
2024-02-05 23:09:02 -0500

    Add some tests for x509_utils_get_signature_alg
    Temporarily disable the RSA-PSS tests for now, but this is enough for a
    regression test for the previous issue.

--- libfreerdp/crypto/test/Test_x509_utils.c
+++ libfreerdp/crypto/test/Test_x509_utils.c
@@ -21,7 +21,7 @@ static char* x509_utils_subject_common_name_wo_length(const X509* xcert)
 	return x509_utils_get_common_name(xcert, &length);
 }
 
-static char* certificate_path(void)
+static char* certificate_path(const char* filename)
 {
 	/*
 	Assume the .pem file is in the same directory as this source file.
@@ -30,14 +30,13 @@ static char* certificate_path(void)
 	is an absolute path).
 	*/
 	static const char dirsep = '/';
-	static const char filename[] = "Test_x509_cert_info.pem";
 #ifdef TEST_SOURCE_DIR
 	const char* file = TEST_SOURCE_DIR;
-	const size_t flen = sizeof(filename) + sizeof(dirsep) + strlen(file) + sizeof(char);
+	const size_t flen = strlen(file) + sizeof(dirsep) + strlen(filename) + sizeof(char);
 	char* result = calloc(1, flen);
 	if (!result)
 		return NULL;
-	_snprintf(result, flen - 1, "%s%c%s", file, dirsep, filename);
+	_snprintf(result, flen, "%s%c%s", file, dirsep, filename);
 	return result;
 #else
 	const char* file = __FILE__;
@@ -45,7 +44,7 @@ static char* certificate_path(void)
 
 	if (last_dirsep)
 	{
-		const size_t filenameLen = strnlen(filename, sizeof(filename));
+		const size_t filenameLen = strlen(filename);
 		const size_t dirsepLen = last_dirsep - file + 1;
 		char* result = malloc(dirsepLen + filenameLen + 1);
 		if (!result)
@@ -135,13 +134,108 @@ fail:
 	return success;
 }
 
+/*
+These certificates were generated with the following commands:
+
+openssl ecparam -name P-256 -out /tmp/p256.pem
+openssl req -x509 -newkey ec:/tmp/p256.pem -keyout /dev/null -days 3650 -nodes -subj "/CN=Test" -out ecdsa_sha1_cert.pem -sha1
+openssl req -x509 -newkey ec:/tmp/p256.pem -keyout /dev/null -days 3650 -nodes -subj "/CN=Test" -out ecdsa_sha256_cert.pem -sha256
+openssl req -x509 -newkey ec:/tmp/p256.pem -keyout /dev/null -days 3650 -nodes -subj "/CN=Test" -out ecdsa_sha384_cert.pem -sha384
+openssl req -x509 -newkey ec:/tmp/p256.pem -keyout /dev/null -days 3650 -nodes -subj "/CN=Test" -out ecdsa_sha512_cert.pem -sha512
+
+openssl req -x509 -newkey rsa:2048 -keyout /dev/null -days 3650 -nodes -subj "/CN=Test" -out rsa_pkcs1_sha1_cert.pem -sha1
+openssl req -x509 -newkey rsa:2048 -keyout /dev/null -days 3650 -nodes -subj "/CN=Test" -out rsa_pkcs1_sha256_cert.pem -sha256
+openssl req -x509 -newkey rsa:2048 -keyout /dev/null -days 3650 -nodes -subj "/CN=Test" -out rsa_pkcs1_sha384_cert.pem -sha384
+openssl req -x509 -newkey rsa:2048 -keyout /dev/null -days 3650 -nodes -subj "/CN=Test" -out rsa_pkcs1_sha512_cert.pem -sha512
+
+openssl req -x509 -newkey rsa:2048 -keyout /dev/null -days 3650 -nodes -subj "/CN=Test" -out rsa_pss_sha1_cert.pem -sha1 -sigopt rsa_padding_mode:pss -sigopt rsa_pss_saltlen:digest
+openssl req -x509 -newkey rsa:2048 -keyout /dev/null -days 3650 -nodes -subj "/CN=Test" -out rsa_pss_sha256_cert.pem -sha256 -sigopt rsa_padding_mode:pss -sigopt rsa_pss_saltlen:digest
+openssl req -x509 -newkey rsa:2048 -keyout /dev/null -days 3650 -nodes -subj "/CN=Test" -out rsa_pss_sha384_cert.pem -sha384 -sigopt rsa_padding_mode:pss -sigopt rsa_pss_saltlen:digest
+openssl req -x509 -newkey rsa:2048 -keyout /dev/null -days 3650 -nodes -subj "/CN=Test" -out rsa_pss_sha512_cert.pem -sha512 -sigopt rsa_padding_mode:pss -sigopt rsa_pss_saltlen:digest
+openssl req -x509 -newkey rsa:2048 -keyout /dev/null -days 3650 -nodes -subj "/CN=Test" -out rsa_pss_sha256_mgf1_sha384_cert.pem -sha256 -sigopt rsa_padding_mode:pss -sigopt rsa_pss_saltlen:digest -sigopt rsa_mgf1_md:sha384
+*/
+
+typedef struct
+{
+	const char* filename;
+	WINPR_MD_TYPE expected;
+} signature_alg_test_t;
+
+static const signature_alg_test_t signature_alg_tests[] = {
+	{ "rsa_pkcs1_sha1_cert.pem", WINPR_MD_SHA1 },
+	{ "rsa_pkcs1_sha256_cert.pem", WINPR_MD_SHA256 },
+	{ "rsa_pkcs1_sha384_cert.pem", WINPR_MD_SHA384 },
+	{ "rsa_pkcs1_sha512_cert.pem", WINPR_MD_SHA512 },
+
+	{ "ecdsa_sha1_cert.pem", WINPR_MD_SHA1 },
+	{ "ecdsa_sha256_cert.pem", WINPR_MD_SHA256 },
+	{ "ecdsa_sha384_cert.pem", WINPR_MD_SHA384 },
+	{ "ecdsa_sha512_cert.pem", WINPR_MD_SHA512 },
+
+#if 0
+	{ "rsa_pss_sha1_cert.pem", WINPR_MD_SHA1 },
+	{ "rsa_pss_sha256_cert.pem", WINPR_MD_SHA256 },
+	{ "rsa_pss_sha384_cert.pem", WINPR_MD_SHA384 },
+	{ "rsa_pss_sha512_cert.pem", WINPR_MD_SHA512 },
+	/*
+	PSS may use different digests for the message hash and MGF-1 hash. In this case, RFC 5929
+	leaves the tls-server-end-point hash unspecified, so it should return WINPR_MD_NONE.
+	*/
+	{ "rsa_pss_sha256_mgf1_sha384_cert.pem", WINPR_MD_NONE },
+#endif
+};
+
+static int TestSignatureAlgorithm(const signature_alg_test_t* test)
+{
+	int success = 0;
+	WINPR_MD_TYPE signature_alg;
+	char* path = certificate_path(test->filename);
+	X509* certificate = x509_utils_from_pem(path, strlen(path), TRUE);
+
+	if (!certificate)
+	{
+		printf("%s: failure: cannot read certificate file '%s'\n", __func__, path);
+		success = -1;
+		goto fail;
+	}
+
+	signature_alg = x509_utils_get_signature_alg(certificate);
+	if (signature_alg != test->expected)
+	{
+		const char* signature_alg_string =
+			signature_alg == WINPR_MD_NONE ? "none" : winpr_md_type_to_string(signature_alg);
+		const char* expected_string =
+			test->expected == WINPR_MD_NONE ? "none" : winpr_md_type_to_string(test->expected);
+		printf("%s: failure: for \"%s\", actual: %s, expected %s\n", __func__, test->filename,
+			signature_alg_string, expected_string);
+		success = -1;
+		goto fail;
+	}
+
+fail:
+	X509_free(certificate);
+	free(path);
+	return success;
+}
+
 int Test_x509_utils(int argc, char* argv[])
 {
-	char* cert_path = certificate_path();
+	char* cert_path = certificate_path("Test_x509_cert_info.pem");
 	int ret;
 	WINPR_UNUSED(argc);
 	WINPR_UNUSED(argv);
+
 	ret = TestCertificateFile(cert_path, certificate_tests, ARRAYSIZE(certificate_tests));
 	free(cert_path);
+	if (ret != 0)
+		return ret;
+
+	for (size_t i = 0; i < ARRAYSIZE(signature_alg_tests); i++)
+	{
+		ret = TestSignatureAlgorithm(&signature_alg_tests[i]);
+		if (ret != 0)
+			return ret;
+	}
+
 	return ret;
 }
