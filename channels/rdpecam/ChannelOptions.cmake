3668f9cd5873eeec99d176a1afb84a972c734c8e
akallabeth <akallabeth@posteo.net>
2024-03-15 08:28:41 +0100

    [channels,rdpecam] add client channel stub

--- channels/rdpecam/ChannelOptions.cmake
+++ channels/rdpecam/ChannelOptions.cmake
@@ -9,4 +9,5 @@ define_channel_options(NAME "rdpecam" TYPE "dynamic"
 	DEFAULT ${OPTION_DEFAULT})
 
 define_channel_server_options(${OPTION_SERVER_DEFAULT})
+define_channel_client_options(${OPTION_CLIENT_DEFAULT})
 
