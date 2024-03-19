678ff144abeb1b80e41e9f0185771958662b436f
akallabeth <akallabeth@posteo.net>
2024-03-19 12:19:07 +0100

    [cmake] unify windows resource file generation
    * use a macro to add resource files for executables and shared libraries
    * use that macro to declare targets
    * use that macro to set library/binary versioning
    * use that macro to set target output name
    * use a macro to create manpages and names

--- cmake/ConfigOptions.cmake
+++ cmake/ConfigOptions.cmake
@@ -80,6 +80,7 @@ endif()
 option(WITH_THIRD_PARTY "Build third-party components" OFF)
 
 option(WITH_CLIENT_INTERFACE "Build clients as a library with an interface" OFF)
+CMAKE_DEPENDENT_OPTION(CLIENT_INTERFACE_SHARED "Build clients as a shared library with an interface" OFF "WITH_CLIENT_INTERFACE" OFF)
 option(WITH_SERVER_INTERFACE "Build servers as a library with an interface" ON)
 
 option(WITH_DEBUG_ALL "Print all debug messages." OFF)
