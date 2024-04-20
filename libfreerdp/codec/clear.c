a9ff85fc1edc9a3fd91f6cc728c076e4ce94d291
akallabeth <akallabeth@posteo.net>
2024-04-20 17:16:35 +0200

    [codec,clear] vBarShortEntry count check
    (cherry picked from commit 0ca4973a67eda1219e1457e9c721a75f64bd935c)

--- libfreerdp/codec/clear.c
+++ libfreerdp/codec/clear.c
@@ -821,8 +821,15 @@ static BOOL clear_decompress_bands_data(CLEAR_CONTEXT* clear, wStream* s, UINT32
 					count = (vBarPixelCount > y) ? (vBarPixelCount - y) : 0;
 
 				if (count > 0)
-					pSrcPixel =
-					    &vBarShortEntry->pixels[(y - vBarYOn) * GetBytesPerPixel(clear->format)];
+				{
+					const size_t offset = (y - vBarYOn) * GetBytesPerPixel(clear->format);
+					pSrcPixel = &vBarShortEntry->pixels[offset];
+					if (offset + count > vBarShortEntry->count)
+					{
+						WLog_ERR(TAG, "offset + count > vBarShortEntry->count");
+						return FALSE;
+					}
+				}
 
 				for (x = 0; x < count; x++)
 				{
