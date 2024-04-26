cc2c08a09331719ef6274a87b9204617d9eb089b
akallabeth <akallabeth@posteo.net>
2024-04-26 12:16:55 +0200

    [utils,doc] update timezone sync procedure
    * add new C# utility to generate timezone mappings
    * update procedure to follow for updating timezones
    * remove obsolete TimeZones.csx script
    * update generated files with new tool

--- winpr/libwinpr/timezone/WindowsZones.h
+++ winpr/libwinpr/timezone/WindowsZones.h
@@ -12,7 +12,7 @@ typedef struct
 	const char* tzid;
 } WINDOWS_TZID_ENTRY;
 
-extern const WINDOWS_TZID_ENTRY WindowsTimeZoneIdTable[];
-extern const size_t WindowsTimeZoneIdTableNrElements;
+extern const WINDOWS_TZID_ENTRY WindowsZones[];
+extern const size_t WindowsZonesNrElements;
 
 #endif /* WINPR_WINDOWS_ZONES_H_ */
