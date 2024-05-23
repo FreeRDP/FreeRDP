67818bddb31900cdf3acb26cb0b673cc90b71cc9
akallabeth <akallabeth@posteo.net>
2024-05-23 09:30:33 +0200

    [client,wayland] fix const correctness

--- client/Wayland/wlfreerdp.c
+++ client/Wayland/wlfreerdp.c
@@ -587,7 +587,7 @@ static void wlf_client_free(freerdp* instance, rdpContext* context)
 	DeleteCriticalSection(&wlf->critical);
 }
 
-static void* uwac_event_clone(const void* val)
+static void* uwac_event_clone(void* val)
 {
 	UwacEvent* copy;
 	UwacEvent* ev = (UwacEvent*)val;
