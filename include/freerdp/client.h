b26ab2f069c0fee0caeb6fc7e9eff24874c91945
akallabeth <akallabeth@posteo.net>
2024-09-05 16:08:38 +0200

    [client,common] extended parser

--- include/freerdp/client.h
+++ include/freerdp/client.h
@@ -20,6 +20,8 @@
 #ifndef FREERDP_CLIENT_H
 #define FREERDP_CLIENT_H
 
+#include <winpr/cmdline.h>
+
 #include <freerdp/config.h>
 #include <freerdp/api.h>
 #include <freerdp/types.h>
@@ -151,6 +153,12 @@ extern "C"
 
 	FREERDP_API int freerdp_client_settings_parse_command_line(rdpSettings* settings, int argc,
 	                                                           char** argv, BOOL allowUnknown);
+
+	FREERDP_API int freerdp_client_settings_parse_command_line_ex(
+	    rdpSettings* settings, int argc, char** argv, BOOL allowUnknown,
+	    COMMAND_LINE_ARGUMENT_A* args, size_t count,
+	    int (*handle_option)(const COMMAND_LINE_ARGUMENT* arg, void* custom),
+	    void* handle_userdata);
 
 	FREERDP_API int freerdp_client_settings_parse_connection_file(rdpSettings* settings,
 	                                                              const char* filename);
