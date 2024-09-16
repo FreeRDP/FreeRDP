ed2c5e9a5b9c5219012743c53e7ca9a58696b1f5
akallabeth <akallabeth@posteo.net>
2024-09-16 22:37:13 +0200

    [client,sdl] add scope guard
    add a class wrapping a cleanup function called when the class instance
    is out of scope. useful to clean up C resources in a lambda function

---
+++ client/SDL/common/scoped_guard.hpp
@@ -0,0 +1,56 @@
+/**
+ * FreeRDP: A Remote Desktop Protocol Implementation
+ * SDL ScopeGuard
+ *
+ * Copyright 2024 Armin Novak <armin.novak@thincast.com>
+ * Copyright 2024 Thincast Technologies GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <functional>
+
+class ScopeGuard
+{
+  public:
+	template <class Callable> explicit ScopeGuard(Callable&& cleanupFunction)
+	try : f(std::forward<Callable>(cleanupFunction))
+	{
+	}
+	catch (...)
+	{
+		cleanupFunction();
+		throw;
+	}
+
+	~ScopeGuard()
+	{
+		if (f)
+			f();
+	}
+
+	void dismiss() noexcept
+	{
+		f = nullptr;
+	}
+
+	ScopeGuard(const ScopeGuard&) = delete;
+	ScopeGuard(ScopeGuard&& other) noexcept = delete;
+	auto operator=(const ScopeGuard&) = delete;
+	auto operator=(const ScopeGuard&&) = delete;
+
+  private:
+	std::function<void()> f;
+};
