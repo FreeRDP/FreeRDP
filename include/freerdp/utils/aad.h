b2756842b282282aac2bce3c807118915a2caf3b
akallabeth <akallabeth@posteo.net>
2024-10-28 14:28:50 +0100

    [core,aad] fetch wellknown before GetAccessToken
    All of the URL we need for AAD authentication are dependent on a tenantid.
    Fetch the wellknown file for the desired tenantid from
    https://login.microsoftonline.com/{tenantid}/v2.0/.well-known/openid-configuration
    and parse as JSON to have them available later on.

--- include/freerdp/utils/aad.h
+++ include/freerdp/utils/aad.h
@@ -25,24 +25,115 @@
  *  \since version 3.0.0
  */
 #include <winpr/wlog.h>
+#include <winpr/json.h>
 
 #include <freerdp/api.h>
+#include <freerdp/types.h>
 #include <freerdp/config.h>
 
-#ifdef WITH_AAD
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
-/** Helper to retrieve the AAD access token from JSON input
- *
- *  @param data The JSON to parse
- *  @param length The number of bytes of the JSON data
- *
- *  @since version 3.0.0
- *
- * @return The token string or \b NULL
- */
-WINPR_ATTR_MALLOC(free, 1)
-FREERDP_API char* freerdp_utils_aad_get_access_token(wLog* log, const char* data, size_t length);
+	/** @enum Expected wellknown fields to be supported
+	 *  @since version 3.10.0
+	 */
+	typedef enum
+	{
+		AAD_WELLKNOWN_token_endpoint = 0,
+		AAD_WELLKNOWN_token_endpoint_auth_methods_supported,
+		AAD_WELLKNOWN_jwks_uri,
+		AAD_WELLKNOWN_response_modes_supported,
+		AAD_WELLKNOWN_subject_types_supported,
+		AAD_WELLKNOWN_id_token_signing_alg_values_supported,
+		AAD_WELLKNOWN_response_types_supported,
+		AAD_WELLKNOWN_scopes_supported,
+		AAD_WELLKNOWN_issuer,
+		AAD_WELLKNOWN_request_uri_parameter_supported,
+		AAD_WELLKNOWN_userinfo_endpoint,
+		AAD_WELLKNOWN_authorization_endpoint,
+		AAD_WELLKNOWN_device_authorization_endpoint,
+		AAD_WELLKNOWN_http_logout_supported,
+		AAD_WELLKNOWN_frontchannel_logout_supported,
+		AAD_WELLKNOWN_end_session_endpoint,
+		AAD_WELLKNOWN_claims_supported,
+		AAD_WELLKNOWN_kerberos_endpoint,
+		AAD_WELLKNOWN_tenant_region_scope,
+		AAD_WELLKNOWN_cloud_instance_name,
+		AAD_WELLKNOWN_cloud_graph_host_name,
+		AAD_WELLKNOWN_msgraph_host,
+		AAD_WELLKNOWN_rbac_url
+	} AAD_WELLKNOWN_VALUES;
 
+	/** Helper to retrieve the AAD access token from JSON input
+	 *
+	 *  @param data The JSON to parse
+	 *  @param length The number of bytes of the JSON data
+	 *
+	 *  @since version 3.0.0
+	 *
+	 * @return The token string or \b NULL
+	 */
+	WINPR_ATTR_MALLOC(free, 1)
+	FREERDP_API char* freerdp_utils_aad_get_access_token(wLog* log, const char* data,
+	                                                     size_t length);
+
+	/** Helper to stringify \ref AAD_WELLKNOWN_VALUES enum
+	 *
+	 *  @param which The enum value to stringify
+	 *
+	 *  @return The string representation of the enum value
+	 *  @since version 3.10.0
+	 */
+	FREERDP_API const char* freerdp_utils_aad_wellknwon_value_name(AAD_WELLKNOWN_VALUES which);
+
+	/** Helper to extract a string from AAD::wellknown JSON
+	 *
+	 * @param context The rdpContext to query for
+	 * @param which The enum value of the field to query
+	 *  @return A constant string to be used for queries or \b NULL in case it does not exist.
+	 *
+	 *  @since version 3.10.0
+	 */
+	FREERDP_API const char* freerdp_utils_aad_get_wellknown_string(rdpContext* context,
+	                                                               AAD_WELLKNOWN_VALUES which);
+
+	/** Helper to extract a string from AAD::wellknown JSON
+	 *
+	 * @param context The rdpContext to query for
+	 * @param which The raw string name of the field to query
+	 *  @return A constant string to be used for queries or \b NULL in case it does not exist.
+	 *
+	 *  @since version 3.10.0
+	 */
+	FREERDP_API const char* freerdp_utils_aad_get_wellknown_custom_string(rdpContext* context,
+	                                                                      const char* which);
+
+	/** Helper to extract a \b WINPR_JSON object from AAD::wellknown JSON
+	 *
+	 * @param context The rdpContext to query for
+	 * @param which The enum value of the field to query
+	 *  @return A \b WINPR_JSON object to be used for queries or \b NULL in case it does not exist.
+	 *
+	 *  @since version 3.10.0
+	 */
+	FREERDP_API WINPR_JSON* freerdp_utils_aad_get_wellknown_object(rdpContext* context,
+	                                                               AAD_WELLKNOWN_VALUES which);
+
+	/** Helper to extract a \b WINPR_JSON object from AAD::wellknown JSON
+	 *
+	 * @param context The rdpContext to query for
+	 * @param which The raw string name of the field to query
+	 *  @return A \b WINPR_JSON object to be used for queries or \b NULL in case it does not exist.
+	 *
+	 *  @since version 3.10.0
+	 */
+	FREERDP_API WINPR_JSON* freerdp_utils_aad_get_wellknown_custom_object(rdpContext* context,
+	                                                                      const char* which);
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif /* FREERDP_UTILS_AAD_H */
