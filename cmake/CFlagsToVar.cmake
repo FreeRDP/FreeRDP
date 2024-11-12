4b455ed048f0c54e21c659f6865ab07686342690
akallabeth <akallabeth@posteo.net>
2024-11-12 09:40:55 +0100

    [cmake] use configuration generator expressions

--- cmake/CFlagsToVar.cmake
+++ cmake/CFlagsToVar.cmake
@@ -1,12 +1,11 @@
-function(CFlagsToVar NAME)
+function(CFlagsToVar NAME CFG)
+    string(TOUPPER "${CFG}" UCFG)
 	set(C_FLAGS ${CMAKE_C_FLAGS})
 	string(REPLACE "${CMAKE_SOURCE_DIR}" "<src dir>" C_FLAGS "${C_FLAGS}")
 	string(REPLACE "${CMAKE_BINARY_DIR}" "<build dir>" C_FLAGS "${C_FLAGS}")
 
-	if (CMAKE_BUILD_TYPE)
-	    string(TOUPPER "${CMAKE_BUILD_TYPE}" CAPS_BUILD_TYPE)
-	    string(APPEND C_FLAGS " ${CMAKE_C_FLAGS_${CAPS_BUILD_TYPE}}")
-	endif()
+    string(APPEND C_FLAGS " ${CMAKE_C_FLAGS_${UCFG}}")
+
 	string(REPLACE "\$" "\\\$" C_FLAGS "${C_FLAGS}")
 	string(REPLACE "\"" "\\\"" C_FLAGS "${C_FLAGS}")
 	set(${NAME} ${C_FLAGS} PARENT_SCOPE)
