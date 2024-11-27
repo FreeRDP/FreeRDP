ec684b78851c5edbc67d14ad47ecba16ca04303e
akallabeth <akallabeth@posteo.net>
2024-11-27 13:14:37 +0100

    [cmake,format] add formatter script

---
+++ scripts/cmake-format.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+SCRIPT_PATH=$(dirname "${BASH_SOURCE[0]}")
+SCRIPT_PATH=$(realpath "$SCRIPT_PATH")
+SRC_PATH="${SCRIPT_PATH}/.."
+
+FORMAT_ARG="--check"
+if [ $# -ne 0 ]
+then
+	if [ "$1" = "--help" ] || [ "$1" = "-h" ];
+	then
+		echo "usage: $0"
+		echo "\t--check.-c  ... run format check only, no files changed (default)"
+		echo "\t--format,-f ... format files in place"
+		echo "\t--help,-h   ... print this help"
+
+		exit 1
+	fi
+
+	if [ "$1" = "--check" ] || [ "$1" = "-c" ];
+	then
+		FORMAT_ARG="--check"
+	fi
+	if [ "$1" = "--format" ] || [ "$1" = "-f" ];
+	then
+		FORMAT_ARG="-i"
+	fi
+fi
+
+CMAKE_FILES=$(find ${SRC_PATH} -name "*.cmake" -o -name "CMakeLists.txt")
+for FILE in $CMAKE_FILES;
+do
+	echo "processing file $FILE..."
+	cmake-format -c "$SCRIPT_PATH/cmake-format.yml" $FORMAT_ARG $FILE
+done
