FreeRDP/FreeRDP

commit ba41d5e532121a698b02b00981771802b4fec310

[warnings] fix unchecked return

server/shadow/Win/win_wds.c
@@ -515,24 +515,23 @@ int win_shadow_wds_wnd_init(winShadowSubsystem* subsystem)
 
 int win_shadow_wds_init(winShadowSubsystem* subsystem)
 {
-	int status;
-	HRESULT hr;
-	DWORD dwCookie;
-	long left, top;
-	long right, bottom;
-	long width, height;
-	IUnknown* pUnknown;
-	rdpSettings* settings;
-	BSTR bstrAuthString;
-	BSTR bstrGroupName;
-	BSTR bstrPassword;
-	BSTR bstrPropertyName;
+	int status = -1;
+
+	long left = 0;
+	long top = 0;
+	long right = 0;
+	long bottom = 0;
+	BSTR bstrAuthString = NULL;
+	BSTR bstrGroupName = NULL;
+	BSTR bstrPassword = NULL;
+	BSTR bstrPropertyName = NULL;
 	VARIANT varPropertyValue;
-	rdpAssistanceFile* file;
-	IConnectionPoint* pCP;
-	IConnectionPointContainer* pCPC;
+	rdpAssistanceFile* file = NULL;
+	IConnectionPoint* pCP = NULL;
+	IConnectionPointContainer* pCPC = NULL;
+
 	win_shadow_wds_wnd_init(subsystem);
-	hr = OleInitialize(NULL);
+	HRESULT hr = OleInitialize(NULL);
 
 	if (FAILED(hr))
 	{
@@ -557,7 +556,7 @@ int win_shadow_wds_init(winShadowSubsystem* subsystem)
 		return -1;
 	}
 
-	pUnknown = (IUnknown*)subsystem->pSharingSession;
+	IUnknown* pUnknown = (IUnknown*)subsystem->pSharingSession;
 	hr = pUnknown->lpVtbl->QueryInterface(pUnknown, &IID_IConnectionPointContainer, (void**)&pCPC);
 
 	if (FAILED(hr))
@@ -577,7 +576,7 @@ int win_shadow_wds_init(winShadowSubsystem* subsystem)
 		return -1;
 	}
 
-	dwCookie = 0;
+	DWORD dwCookie = 0;
 	subsystem->pSessionEvents = &Shadow_IRDPSessionEvents;
 	subsystem->pSessionEvents->lpVtbl->AddRef(subsystem->pSessionEvents);
 	hr = pCP->lpVtbl->Advise(pCP, (IUnknown*)subsystem->pSessionEvents, &dwCookie);
@@ -605,8 +604,8 @@ int win_shadow_wds_init(winShadowSubsystem* subsystem)
 		return -1;
 	}
 
-	width = right - left;
-	height = bottom - top;
+	long width = right - left;
+	long height = bottom - top;
 	WLog_INFO(
 	    TAG,
 	    "GetDesktopSharedRect(): left: %ld top: %ld right: %ld bottom: %ld width: %ld height: %ld",
@@ -765,13 +764,19 @@ int win_shadow_wds_init(winShadowSubsystem* subsystem)
 		return status;
 	}
 
-	settings = subsystem->shw->settings;
-	status = freerdp_assistance_populate_settings_from_assistance_file(file, settings);
-	freerdp_settings_set_string(settings, FreeRDP_Domain, "RDP");
-	freerdp_settings_set_string(settings, FreeRDP_Username, "Shadow");
-	freerdp_settings_set_bool(settings, FreeRDP_AutoLogonEnabled, TRUE);
-	freerdp_settings_set_uint32(settings, FreeRDP_DesktopWidth, width);
-	freerdp_settings_set_uint32(settings, FreeRDP_DesktopHeight, height);
+	rdpSetting* settings = subsystem->shw->settings;
+	if (!freerdp_assistance_populate_settings_from_assistance_file(file, settings))
+		return -1;
+	if (!freerdp_settings_set_string(settings, FreeRDP_Domain, "RDP"))
+		return -1;
+	if (!freerdp_settings_set_string(settings, FreeRDP_Username, "Shadow"))
+		return -1;
+	if (!freerdp_settings_set_bool(settings, FreeRDP_AutoLogonEnabled, TRUE))
+		return -1;
+	if (!freerdp_settings_set_uint32(settings, FreeRDP_DesktopWidth, width))
+		return -1;
+	if (!freerdp_settings_set_uint32(settings, FreeRDP_DesktopHeight, height))
+		return -1;
 	status = win_shadow_rdp_start(subsystem);
 
 	if (status < 0)

client/Mac/cli/AppDelegate.m
@@ -54,8 +54,10 @@ void mac_set_view_size(rdpContext *context, MRDPView *view);
 
 		if (freerdp_settings_get_bool(settings, FreeRDP_Fullscreen))
 		{
-			freerdp_settings_set_uint32(settings, FreeRDP_DesktopWidth, screenFrame.size.width);
-			freerdp_settings_set_uint32(settings, FreeRDP_DesktopHeight, screenFrame.size.height);
+			(void)freerdp_settings_set_uint32(settings, FreeRDP_DesktopWidth,
+			                                  screenFrame.size.width);
+			(void)freerdp_settings_set_uint32(settings, FreeRDP_DesktopHeight,
+			                                  screenFrame.size.height);
 		}
 
 		PubSub_SubscribeConnectionResult(context->pubSub, AppDelegate_ConnectionResultEventHandler);

client/Windows/wf_client.c
@@ -264,16 +264,19 @@ static BOOL wf_pre_connect(freerdp* instance)
 
 	if (desktopWidth != freerdp_settings_get_uint32(settings, FreeRDP_DesktopWidth))
 	{
-		freerdp_settings_set_uint32(settings, FreeRDP_DesktopWidth, desktopWidth);
+		if (!freerdp_settings_set_uint32(settings, FreeRDP_DesktopWidth, desktopWidth))
+			return FALSE;
 	}
 
 	if (desktopHeight != freerdp_settings_get_uint32(settings, FreeRDP_DesktopHeight))
 	{
-		freerdp_settings_set_uint32(settings, FreeRDP_DesktopHeight, desktopHeight);
+		if (!freerdp_settings_set_uint32(settings, FreeRDP_DesktopHeight, desktopHeight))
+			return FALSE;
 	}
 
 	rc = freerdp_keyboard_init(freerdp_settings_get_uint32(settings, FreeRDP_KeyboardLayout));
-	freerdp_settings_set_uint32(settings, FreeRDP_KeyboardLayout, rc);
+	if (!freerdp_settings_set_uint32(settings, FreeRDP_KeyboardLayout, rc))
+		return FALSE;
 	PubSub_SubscribeChannelConnected(instance->context->pubSub, wf_OnChannelConnectedEventHandler);
 	PubSub_SubscribeChannelDisconnected(instance->context->pubSub,
 	                                    wf_OnChannelDisconnectedEventHandler);
@@ -1338,7 +1341,8 @@ static BOOL wfreerdp_client_new(freerdp* instance, rdpContext* context)
 	instance->AuthenticateEx = wf_authenticate_ex;
 
 #ifdef WITH_WINDOWS_CERT_STORE
-	freerdp_settings_set_bool(context->settings, FreeRDP_CertificateCallbackPreferPEM, TRUE);
+	if (!freerdp_settings_set_bool(context->settings, FreeRDP_CertificateCallbackPreferPEM, TRUE))
+		return FALSE;
 #endif
 
 	if (!wfc->isConsole)

client/Windows/wf_event.c
@@ -360,8 +360,9 @@ static void wf_send_resize(wfContext* wfc)
 				{
 					WLog_ERR("", "SendMonitorLayout failed.");
 				}
-				freerdp_settings_set_uint32(settings, FreeRDP_SmartSizingWidth, targetWidth);
-				freerdp_settings_set_uint32(settings, FreeRDP_SmartSizingHeight, targetHeight);
+				(void)freerdp_settings_set_uint32(settings, FreeRDP_SmartSizingWidth, targetWidth);
+				(void)freerdp_settings_set_uint32(settings, FreeRDP_SmartSizingHeight,
+				                                  targetHeight);
 			}
 			wfc->lastSentDate = GetTickCount64();
 		}
@@ -725,7 +726,7 @@ LRESULT CALLBACK wf_event_proc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
 				{
 					HMENU hMenu = GetSystemMenu(wfc->hwnd, FALSE);
 					const BOOL rc = freerdp_settings_get_bool(settings, FreeRDP_SmartSizing);
-					freerdp_settings_set_bool(settings, FreeRDP_SmartSizing, !rc);
+					(void)freerdp_settings_set_bool(settings, FreeRDP_SmartSizing, !rc);
 					CheckMenuItem(hMenu, SYSCOMMAND_ID_SMARTSIZING,
 					              freerdp_settings_get_bool(settings, FreeRDP_SmartSizing)
 					                  ? MF_CHECKED
@@ -778,7 +779,8 @@ LRESULT CALLBACK wf_event_proc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
 		case WM_SETFOCUS:
 			DEBUG_KBD("getting focus %X", hWnd);
 
-			freerdp_settings_set_bool(wfc->common.context.settings, FreeRDP_SuspendInput, FALSE);
+			(void)freerdp_settings_set_bool(wfc->common.context.settings, FreeRDP_SuspendInput,
+			                                FALSE);
 
 			if (alt_ctrl_down())
 				g_flipping_in = TRUE;
@@ -788,7 +790,8 @@ LRESULT CALLBACK wf_event_proc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
 			break;
 
 		case WM_KILLFOCUS:
-			freerdp_settings_set_bool(wfc->common.context.settings, FreeRDP_SuspendInput, TRUE);
+			(void)freerdp_settings_set_bool(wfc->common.context.settings, FreeRDP_SuspendInput,
+			                                TRUE);
 
 			if (g_focus_hWnd == hWnd && wfc && !wfc->fullscreen)
 			{

client/common/client.c
@@ -254,21 +254,28 @@ static BOOL freerdp_client_settings_post_process(rdpSettings* settings)
 	 * that the rdp file also triggers this functionality */
 	if (freerdp_settings_get_bool(settings, FreeRDP_SpanMonitors))
 	{
-		freerdp_settings_set_bool(settings, FreeRDP_UseMultimon, TRUE);
-		freerdp_settings_set_bool(settings, FreeRDP_Fullscreen, TRUE);
+		if (!freerdp_settings_set_bool(settings, FreeRDP_UseMultimon, TRUE))
+			goto out_error;
+		if (!freerdp_settings_set_bool(settings, FreeRDP_Fullscreen, TRUE))
+			goto out_error;
 	}
 	else if (freerdp_settings_get_bool(settings, FreeRDP_UseMultimon))
 	{
-		freerdp_settings_set_bool(settings, FreeRDP_Fullscreen, TRUE);
+		if (!freerdp_settings_set_bool(settings, FreeRDP_Fullscreen, TRUE))
+			goto out_error;
 	}
 
 	/* deal with the smartcard / smartcard logon stuff */
 	if (freerdp_settings_get_bool(settings, FreeRDP_SmartcardLogon))
 	{
-		freerdp_settings_set_bool(settings, FreeRDP_TlsSecurity, TRUE);
-		freerdp_settings_set_bool(settings, FreeRDP_RedirectSmartCards, TRUE);
-		freerdp_settings_set_bool(settings, FreeRDP_DeviceRedirection, TRUE);
-		freerdp_settings_set_bool(settings, FreeRDP_PasswordIsSmartcardPin, TRUE);
+		if (!freerdp_settings_set_bool(settings, FreeRDP_TlsSecurity, TRUE))
+			goto out_error;
+		if (!freerdp_settings_set_bool(settings, FreeRDP_RedirectSmartCards, TRUE))
+			goto out_error;
+		if (!freerdp_settings_set_bool(settings, FreeRDP_DeviceRedirection, TRUE))
+			goto out_error;
+		if (!freerdp_settings_set_bool(settings, FreeRDP_PasswordIsSmartcardPin, TRUE))
+			goto out_error;
 	}
 
 	return TRUE;

client/common/cmdline.c
@@ -4195,9 +4195,12 @@ static int freerdp_client_settings_parse_command_line_arguments_int(
 	else
 		compatibility = freerdp_client_detect_command_line(argc - 1, &argv[1], &flags);
 
-	freerdp_settings_set_string(settings, FreeRDP_ProxyHostname, NULL);
-	freerdp_settings_set_string(settings, FreeRDP_ProxyUsername, NULL);
-	freerdp_settings_set_string(settings, FreeRDP_ProxyPassword, NULL);
+	if (!freerdp_settings_set_string(settings, FreeRDP_ProxyHostname, NULL))
+		return -1;
+	if (!freerdp_settings_set_string(settings, FreeRDP_ProxyUsername, NULL))
+		return -1;
+	if (!freerdp_settings_set_string(settings, FreeRDP_ProxyPassword, NULL))
+		return -1;
 
 	if (compatibility)
 	{
@@ -4644,7 +4647,8 @@ static int freerdp_client_settings_parse_command_line_arguments_int(
 		}
 		CommandLineSwitchCase(arg, "disable-output")
 		{
-			freerdp_settings_set_bool(settings, FreeRDP_DeactivateClientDecoding, enable);
+			if (!freerdp_settings_set_bool(settings, FreeRDP_DeactivateClientDecoding, enable))
+				return fail_at(arg, COMMAND_LINE_ERROR);
 		}
 		CommandLineSwitchCase(arg, "home-drive")
 		{

client/iOS/Models/RDPSession.m
@@ -309,8 +309,8 @@ out_free:
 		{
 			[_params setInt:size.width forKey:@"width"];
 			[_params setInt:size.height forKey:@"height"];
-			freerdp_settings_set_uint32(settings, FreeRDP_DesktopWidth, size.width);
-			freerdp_settings_set_uint32(settings, FreeRDP_DesktopHeight, size.height);
+			(void)freerdp_settings_set_uint32(settings, FreeRDP_DesktopWidth, size.width);
+			(void)freerdp_settings_set_uint32(settings, FreeRDP_DesktopHeight, size.height);
 		}
 	}
 
@@ -320,7 +320,7 @@ out_free:
 	if (freerdp_settings_get_uint32(settings, FreeRDP_ColorDepth) <= 16)
 	{
 		const UINT32 w = freerdp_settings_get_uint32(settings, FreeRDP_DesktopWidth) & (~1);
-		freerdp_settings_set_uint32(settings, FreeRDP_DesktopWidth, w);
+		(void)freerdp_settings_set_uint32(settings, FreeRDP_DesktopWidth, w);
 	}
 
 	[self performSelectorInBackground:@selector(runSession) withObject:nil];

libfreerdp/codec/clear.c
@@ -793,10 +793,13 @@ static BOOL clear_decompress_bands_data(CLEAR_CONTEXT* WINPR_RESTRICT clear,
 				if ((y + count) > vBarPixelCount)
 					count = (vBarPixelCount > y) ? (vBarPixelCount - y) : 0;
 
-				while (count--)
+				if (count > 0)
 				{
-					FreeRDPWriteColor(dstBuffer, clear->format, colorBkg);
-					dstBuffer += FreeRDPGetBytesPerPixel(clear->format);
+					while (count--)
+					{
+						FreeRDPWriteColor(dstBuffer, clear->format, colorBkg);
+						dstBuffer += FreeRDPGetBytesPerPixel(clear->format);
+					}
 				}
 
 				/*
@@ -836,12 +839,15 @@ static BOOL clear_decompress_bands_data(CLEAR_CONTEXT* WINPR_RESTRICT clear,
 				y = vBarYOn + vBarShortPixelCount;
 				count = (vBarPixelCount > y) ? (vBarPixelCount - y) : 0;
 
-				while (count--)
+				if (count > 0)
 				{
-					if (!FreeRDPWriteColor(dstBuffer, clear->format, colorBkg))
-						return FALSE;
+					while (count--)
+					{
+						if (!FreeRDPWriteColor(dstBuffer, clear->format, colorBkg))
+							return FALSE;
 
-					dstBuffer += FreeRDPGetBytesPerPixel(clear->format);
+						dstBuffer += FreeRDPGetBytesPerPixel(clear->format);
+					}
 				}
 
 				vBarEntry->count = vBarPixelCount;

commit cad4f8ebd7ba03c982c86c247802603cf56fb4f1

shadow: Fix typo with rdpSettings data type
This commit fixes the following compiler error.

FreeRDP/server/shadow/Win/win_wds.c:767:9: error: unknown type name 'rdpSetting';
did you mean 'rdpSettings'?
  767 |         rdpSetting* settings = subsystem->shw->settings;
      |         ^~~~~~~~~~
      |         rdpSettings

The typo was introduced in ba41d5e532121a698b02b00981771802b4fec310 commit.

server/shadow/Win/win_wds.c
@@ -764,7 +764,7 @@ int win_shadow_wds_init(winShadowSubsystem* subsystem)
 		return status;
 	}
 
-	rdpSetting* settings = subsystem->shw->settings;
+	rdpSettings* settings = subsystem->shw->settings;
 	if (!freerdp_assistance_populate_settings_from_assistance_file(file, settings))
 		return -1;
 	if (!freerdp_settings_set_string(settings, FreeRDP_Domain, "RDP"))