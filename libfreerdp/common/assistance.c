d5a01a9bf880a5d9d9a6388dd0416a355e792a7a
akallabeth <akallabeth@posteo.net>
2024-12-08 09:37:23 +0100

    [common,assistance] fix endianess issue

--- libfreerdp/common/assistance.c
+++ libfreerdp/common/assistance.c
@@ -876,7 +876,7 @@ BYTE* freerdp_assistance_encrypt_pass_stub(const char* password, const char* pas
 	if (!pbIn || !pbOut)
 		goto fail;
 
-	*((UINT32*)pbIn) = (UINT32)cbPassStubW;
+	Data_Write_UINT32(pbIn, cbPassStubW);
 	CopyMemory(&pbIn[4], PassStubW, cbPassStubW);
 	rc4Ctx = winpr_RC4_New(PasswordHash, sizeof(PasswordHash));
 
