4700dab0b2ab328ec4939cc2cbe302b109a60430
akallabeth <akallabeth@posteo.net>
2024-12-18 19:25:29 +0100

    xx

--- winpr/include/winpr/bcrypt.h
+++ winpr/include/winpr/bcrypt.h
@@ -33,66 +33,66 @@ typedef PVOID BCRYPT_KEY_HANDLE;
 typedef PVOID BCRYPT_HASH_HANDLE;
 typedef PVOID BCRYPT_SECRET_HANDLE;
 
-#define BCRYPT_RSA_ALGORITHM u"RSA"
-#define BCRYPT_RSA_SIGN_ALGORITHM u"RSA_SIGN"
-#define BCRYPT_DH_ALGORITHM u"DH"
-#define BCRYPT_DSA_ALGORITHM u"DSA"
-#define BCRYPT_RC2_ALGORITHM u"RC2"
-#define BCRYPT_RC4_ALGORITHM u"RC4"
-#define BCRYPT_AES_ALGORITHM u"AES"
-#define BCRYPT_DES_ALGORITHM u"DES"
-#define BCRYPT_DESX_ALGORITHM u"DESX"
-#define BCRYPT_3DES_ALGORITHM u"3DES"
-#define BCRYPT_3DES_112_ALGORITHM u"3DES_112"
-#define BCRYPT_MD2_ALGORITHM u"MD2"
-#define BCRYPT_MD4_ALGORITHM u"MD4"
-#define BCRYPT_MD5_ALGORITHM u"MD5"
-#define BCRYPT_SHA1_ALGORITHM u"SHA1"
-#define BCRYPT_SHA256_ALGORITHM u"SHA256"
-#define BCRYPT_SHA384_ALGORITHM u"SHA384"
-#define BCRYPT_SHA512_ALGORITHM u"SHA512"
-#define BCRYPT_AES_GMAC_ALGORITHM u"AES-GMAC"
-#define BCRYPT_AES_CMAC_ALGORITHM u"AES-CMAC"
-#define BCRYPT_ECDSA_P256_ALGORITHM u"ECDSA_P256"
-#define BCRYPT_ECDSA_P384_ALGORITHM u"ECDSA_P384"
-#define BCRYPT_ECDSA_P521_ALGORITHM u"ECDSA_P521"
-#define BCRYPT_ECDH_P256_ALGORITHM u"ECDSA_P256"
-#define BCRYPT_ECDH_P384_ALGORITHM u"ECDSA_P384"
-#define BCRYPT_ECDH_P521_ALGORITHM u"ECDSA_P521"
-#define BCRYPT_RNG_ALGORITHM u"RNG"
-#define BCRYPT_RNG_FIPS186_DSA_ALGORITHM u"FIPS186DSARNG"
-#define BCRYPT_RNG_DUAL_EC_ALGORITHM u"DUALECRNG"
+static inline const WCHAR BCRYPT_RSA_ALGORITHM[] = u"RSA";
+static inline const WCHAR BCRYPT_RSA_SIGN_ALGORITHM[] = u"RSA_SIGN";
+static inline const WCHAR BCRYPT_DH_ALGORITHM[] = u"DH";
+static inline const WCHAR BCRYPT_DSA_ALGORITHM[] = u"DSA";
+static inline const WCHAR BCRYPT_RC2_ALGORITHM[] = u"RC2";
+static inline const WCHAR BCRYPT_RC4_ALGORITHM[] = u"RC4";
+static inline const WCHAR BCRYPT_AES_ALGORITHM[] = u"AES";
+static inline const WCHAR BCRYPT_DES_ALGORITHM[] = u"DES";
+static inline const WCHAR BCRYPT_DESX_ALGORITHM[] = u"DESX";
+static inline const WCHAR BCRYPT_3DES_ALGORITHM[] = u"3DES";
+static inline const WCHAR BCRYPT_3DES_112_ALGORITHM[] = u"3DES_112";
+static inline const WCHAR BCRYPT_MD2_ALGORITHM[] = u"MD2";
+static inline const WCHAR BCRYPT_MD4_ALGORITHM[] = u"MD4";
+static inline const WCHAR BCRYPT_MD5_ALGORITHM[] = u"MD5";
+static inline const WCHAR BCRYPT_SHA1_ALGORITHM[] = u"SHA1";
+static inline const WCHAR BCRYPT_SHA256_ALGORITHM[] = u"SHA256";
+static inline const WCHAR BCRYPT_SHA384_ALGORITHM[] = u"SHA384";
+static inline const WCHAR BCRYPT_SHA512_ALGORITHM[] = u"SHA512";
+static inline const WCHAR BCRYPT_AES_GMAC_ALGORITHM[] = u"AES-GMAC";
+static inline const WCHAR BCRYPT_AES_CMAC_ALGORITHM[] = u"AES-CMAC";
+static inline const WCHAR BCRYPT_ECDSA_P256_ALGORITHM[] = u"ECDSA_P256";
+static inline const WCHAR BCRYPT_ECDSA_P384_ALGORITHM[] = u"ECDSA_P384";
+static inline const WCHAR BCRYPT_ECDSA_P521_ALGORITHM[] = u"ECDSA_P521";
+static inline const WCHAR BCRYPT_ECDH_P256_ALGORITHM[] = u"ECDSA_P256";
+static inline const WCHAR BCRYPT_ECDH_P384_ALGORITHM[] = u"ECDSA_P384";
+static inline const WCHAR BCRYPT_ECDH_P521_ALGORITHM[] = u"ECDSA_P521";
+static inline const WCHAR BCRYPT_RNG_ALGORITHM[] = u"RNG";
+static inline const WCHAR BCRYPT_RNG_FIPS186_DSA_ALGORITHM[] = u"FIPS186DSARNG";
+static inline const WCHAR BCRYPT_RNG_DUAL_EC_ALGORITHM[] = u"DUALECRNG";
 
-#define BCRYPT_ECDSA_ALGORITHM u"ECDSA"
-#define BCRYPT_ECDH_ALGORITHM u"ECDH"
-#define BCRYPT_XTS_AES_ALGORITHM u"XTS-AES"
+static inline const WCHAR BCRYPT_ECDSA_ALGORITHM[] = u"ECDSA";
+static inline const WCHAR BCRYPT_ECDH_ALGORITHM[] = u"ECDH";
+static inline const WCHAR BCRYPT_XTS_AES_ALGORITHM[] = u"XTS-AES";
 
-#define MS_PRIMITIVE_PROVIDER u"Microsoft Primitive Provider"
-#define MS_PLATFORM_CRYPTO_PROVIDER u"Microsoft Platform Crypto Provider"
+static inline const WCHAR MS_PRIMITIVE_PROVIDER[] = u"Microsoft Primitive Provider";
+static inline const WCHAR MS_PLATFORM_CRYPTO_PROVIDER[] = u"Microsoft Platform Crypto Provider";
 
 #define BCRYPT_ALG_HANDLE_HMAC_FLAG 0x00000008
 #define BCRYPT_PROV_DISPATCH 0x00000001
 
-#define BCRYPT_OBJECT_LENGTH u"ObjectLength"
-#define BCRYPT_ALGORITHM_NAME u"AlgorithmName"
-#define BCRYPT_PROVIDER_HANDLE u"ProviderHandle"
-#define BCRYPT_CHAINING_MODE u"ChainingMode"
-#define BCRYPT_BLOCK_LENGTH u"BlockLength"
-#define BCRYPT_KEY_LENGTH u"KeyLength"
-#define BCRYPT_KEY_OBJECT_LENGTH u"KeyObjectLength"
-#define BCRYPT_KEY_STRENGTH u"KeyStrength"
-#define BCRYPT_KEY_LENGTHS u"KeyLengths"
-#define BCRYPT_BLOCK_SIZE_LIST u"BlockSizeList"
-#define BCRYPT_EFFECTIVE_KEY_LENGTH u"EffectiveKeyLength"
-#define BCRYPT_HASH_LENGTH u"HashDigestLength"
-#define BCRYPT_HASH_OID_LIST u"HashOIDList"
-#define BCRYPT_PADDING_SCHEMES u"PaddingSchemes"
-#define BCRYPT_SIGNATURE_LENGTH u"SignatureLength"
-#define BCRYPT_HASH_BLOCK_LENGTH u"HashBlockLength"
-#define BCRYPT_AUTH_TAG_LENGTH u"AuthTagLength"
-#define BCRYPT_PRIMITIVE_TYPE u"PrimitiveType"
-#define BCRYPT_IS_KEYED_HASH u"IsKeyedHash"
-#define BCRYPT_KEY_DATA_BLOB u"KeyDataBlob"
+static inline const WCHAR BCRYPT_OBJECT_LENGTH[] = u"ObjectLength";
+static inline const WCHAR BCRYPT_ALGORITHM_NAME[] = u"AlgorithmName";
+static inline const WCHAR BCRYPT_PROVIDER_HANDLE[] = u"ProviderHandle";
+static inline const WCHAR BCRYPT_CHAINING_MODE[] = u"ChainingMode";
+static inline const WCHAR BCRYPT_BLOCK_LENGTH[] = u"BlockLength";
+static inline const WCHAR BCRYPT_KEY_LENGTH[] = u"KeyLength";
+static inline const WCHAR BCRYPT_KEY_OBJECT_LENGTH[] = u"KeyObjectLength";
+static inline const WCHAR BCRYPT_KEY_STRENGTH[] = u"KeyStrength";
+static inline const WCHAR BCRYPT_KEY_LENGTHS[] = u"KeyLengths";
+static inline const WCHAR BCRYPT_BLOCK_SIZE_LIST[] = u"BlockSizeList";
+static inline const WCHAR BCRYPT_EFFECTIVE_KEY_LENGTH[] = u"EffectiveKeyLength";
+static inline const WCHAR BCRYPT_HASH_LENGTH[] = u"HashDigestLength";
+static inline const WCHAR BCRYPT_HASH_OID_LIST[] = u"HashOIDList";
+static inline const WCHAR BCRYPT_PADDING_SCHEMES[] = u"PaddingSchemes";
+static inline const WCHAR BCRYPT_SIGNATURE_LENGTH[] = u"SignatureLength";
+static inline const WCHAR BCRYPT_HASH_BLOCK_LENGTH[] = u"HashBlockLength";
+static inline const WCHAR BCRYPT_AUTH_TAG_LENGTH[] = u"AuthTagLength";
+static inline const WCHAR BCRYPT_PRIMITIVE_TYPE[] = u"PrimitiveType";
+static inline const WCHAR BCRYPT_IS_KEYED_HASH[] = u"IsKeyedHash";
+static inline const WCHAR BCRYPT_KEY_DATA_BLOB[] = u"KeyDataBlob";
 
 #define BCRYPT_BLOCK_PADDING 0x00000001
 
