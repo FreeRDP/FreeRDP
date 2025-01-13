c24f81c8ad127850b7bc190962c41fb082c12d16
akallabeth <akallabeth@posteo.net>
2025-01-13 10:30:10 +0100

    [freerdp] add missing documentation

--- include/freerdp/primitives.h
+++ include/freerdp/primitives.h
@@ -276,16 +276,22 @@ typedef enum
 
 	/** @brief get a specific primitives implementation
 	 *
-	 *  This will try to return the primitives implementation suggested by \b hint
+	 *  This will try to return the primitives implementation suggested by \b type
 	 *  If that does not exist or does not work on the platform any other (e.g. usually pure
 	 * software) is returned
 	 *
-	 *  @param type the type of primitives to return.
+	 *  @param type the type of primitives desired.
 	 *  @return A primitive implementation matching the hint closest or \b NULL in case of failure.
 	 *  @since version 3.11.0
 	 */
 	FREERDP_API primitives_t* primitives_get_by_type(primitive_hints type);
 
+	/** @brief stringify a \b avc444_frame_type
+	 *
+	 *  @param type the type to stringify
+	 *  @return A string representation of \b type
+	 *  @since version 3.11.0
+	 */
 	FREERDP_API const char* primitives_avc444_frame_type_str(avc444_frame_type type);
 
 	/** @brief convert a hint to a string
