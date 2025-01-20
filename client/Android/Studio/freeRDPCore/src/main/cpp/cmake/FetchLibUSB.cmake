5cac433422233693c3a6b4395fc662e1daf84d96
akallabeth <akallabeth@posteo.net>
2025-01-20 23:10:49 +0100

    xx

--- client/Android/Studio/freeRDPCore/src/main/cpp/cmake/FetchLibUSB.cmake
+++ client/Android/Studio/freeRDPCore/src/main/cpp/cmake/FetchLibUSB.cmake
@@ -5,7 +5,7 @@ set(LIBUSB_BUILD_TESTING OFF CACHE INTERNAL "fetch content")
 set(LIBUSB_ENABLE_DEBUG_LOGGING OFF CACHE INTERNAL "fetch content")
 FetchContent_Declare(
   libusb
-  GIT_REPOSITORY https://github.com/libjpeg-turbo/libjpeg-turbo.git
+  GIT_REPOSITORY https://github.com/libusb/libusb-cmake.git
   GIT_TAG        v1.0.27-1
 )
 
