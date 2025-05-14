2865edb5d58ec051966790d82a78ffd7043f27be
akallabeth <akallabeth@posteo.net>
2025-05-14 20:19:45 +0200

    [ci,rpm] add option to provide RPMBUILD topdir

--- packaging/scripts/prepare_rpm_freerdp-nightly.sh
+++ packaging/scripts/prepare_rpm_freerdp-nightly.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+RPMBUILD_BASE="~/rpmbuild/SOURCES"
+if [ $# -gt 0 ]; then
+  RPMBUILD_BASE="$1/SOURCES"
+fi
+
 if [ ! -d external/webview ]; then
   git clone -b navigation-listener --depth=1 https://github.com/akallabeth/webview external/webview
 fi
 
 (
-	cd external/webview
-	git archive --format=tar --prefix=webview HEAD --output ~/rpmbuild/SOURCES/webview.tar.bz2
+  cd external/webview
+  mkdir -p "$RPMBUILD_BASE"
+  git archive --format=tar --prefix=webview HEAD --output $RPMBUILD_BASE/webview.tar.bz2
 )
 
 git rev-parse --short HEAD >source_version
