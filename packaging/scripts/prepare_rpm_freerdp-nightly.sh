702a9dae0651394e018776cd006f3b19fb3f69b9
Armin Novak <armin.novak@thincast.com>
2025-05-14 16:48:10 +0200

    [ci,rpm] fix rpm source file extraction

--- packaging/scripts/prepare_rpm_freerdp-nightly.sh
+++ packaging/scripts/prepare_rpm_freerdp-nightly.sh
@@ -4,4 +4,9 @@ if [ ! -d external/webview ]; then
   git clone -b navigation-listener --depth=1 https://github.com/akallabeth/webview external/webview
 fi
 
+(
+	cd external/webview
+	git archive --format=tar --prefix=webview HEAD --output ~/rpmbuild/SOURCES/webview.tar.bz2
+)
+
 git rev-parse --short HEAD >source_version
