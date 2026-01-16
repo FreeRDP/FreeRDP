39ff701d829eb7be33b2d189b419ba729c985b69
akallabeth <akallabeth@posteo.net>
2026-01-16 11:40:49 +0100

    [c,printf] fix wlog format string to match arguments

--- libfreerdp/cache/glyph.c
+++ libfreerdp/cache/glyph.c
@@ -58,7 +58,7 @@ static UINT32 update_glyph_offset(const BYTE* data, size_t length, UINT32 index,
 				offset |= ((UINT32)data[index++]) << 8;
 			}
 			else
-				WLog_WARN(TAG, "[%s] glyph index out of bound %" PRIu32 " [max %" PRIuz "]", index,
+				WLog_WARN(TAG, "glyph index out of bound %" PRIu32 " [max %" PRIuz "]", index,
 				          length);
 		}
 
