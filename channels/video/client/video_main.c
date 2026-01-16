d5c65d483f8ba990e35a2d8f195cde0015623da3
akallabeth <akallabeth@posteo.net>
2026-01-16 11:40:31 +0100

    [c,printf] fix wlog format string to match arguments

--- channels/video/client/video_main.c
+++ channels/video/client/video_main.c
@@ -937,7 +937,7 @@ static UINT video_VideoData(VideoClientContext* context, const TSMM_VIDEO_DATA* 
 			}
 
 			// NOLINTNEXTLINE(clang-analyzer-unix.Malloc): Queue_Enqueue owns frame
-			WLog_DBG(TAG, "scheduling frame in %" PRIu32 " ms", (frame->publishTime - startTime));
+			WLog_DBG(TAG, "scheduling frame in %" PRIu64 " ms", (frame->publishTime - startTime));
 		}
 	}
 
