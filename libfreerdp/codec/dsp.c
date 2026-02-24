a79e307cb990c3ce86bd5c4a45ba8ef5fa03a895
Armin Novak <armin.novak@thincast.com>
2026-02-24 19:23:05 +0100

    [codec,dsp] fix integer sign warnings

--- libfreerdp/codec/dsp.c
+++ libfreerdp/codec/dsp.c
@@ -1443,9 +1443,13 @@ BOOL freerdp_dsp_supports_format(const AUDIO_FORMAT* WINPR_RESTRICT format, BOOL
 				return TRUE;
 
 #endif
+#if defined(WITH_FDK_AAC)
+			return TRUE;
+#else
+			return FALSE;
+#endif
+
 #if defined(WITH_OPUS)
-			/* fallthrough */
-			WINPR_FALLTHROUGH
 		case WAVE_FORMAT_OPUS:
 			return opus_is_valid_samplerate(format);
 #endif
