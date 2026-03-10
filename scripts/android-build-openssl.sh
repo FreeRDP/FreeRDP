1edd260cf38da434809fc05e5594363e805276a9
Armin Novak <armin.novak@thincast.com>
2026-03-10 14:24:02 +0100

    [build,android] update dependencies

--- scripts/android-build-openssl.sh
+++ scripts/android-build-openssl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 SCM_URL=https://github.com/openssl/openssl/releases/download/
-SCM_TAG=openssl-3.5.0
-SCM_HASH=344d0a79f1a9b08029b0744e2cc401a43f9c90acd1044d09a530b4885a8e9fc0
+SCM_TAG=openssl-3.6.1
+SCM_HASH=b1bfedcd5b289ff22aee87c9d600f515767ebf45f77168cb6d64f231f518a82e
 
 COMPILER=4.9
 
