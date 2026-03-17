d5a75defcddff72b778b70c2c3cf1630415d48f7
Richard Markiewicz <rmarkiewicz@devolutions.net>
2026-03-17 11:23:59 -0400

    [core,update] make the PlaySound callback non-mandatory

--- libfreerdp/core/update.c
+++ libfreerdp/core/update.c
@@ -355,7 +355,7 @@ BOOL update_recv_play_sound(rdpUpdate* update, wStream* s)
 	if (!update_read_play_sound(s, &play_sound))
 		return FALSE;
 
-	return IFCALLRESULT(FALSE, update->PlaySound, update->context, &play_sound);
+	return IFCALLRESULT(TRUE, update->PlaySound, update->context, &play_sound);
 }
 
 POINTER_POSITION_UPDATE* update_read_pointer_position(rdpUpdate* update, wStream* s)
