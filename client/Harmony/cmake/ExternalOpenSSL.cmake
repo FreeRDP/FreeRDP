42ef9842c201c9b81283a7d0d55d6d8f006e9747
Ben Gao <bengao168@msn.com>
2026-05-11 18:07:22 +0800

    style(client,hFreeRDP): run cmake-format and fix codespell warnings
    - Apply cmake-format to all Harmony CMake files:
      client/Harmony/CMakeLists.txt, native/CMakeLists.txt,
      cmake/ExternalOpenSSL.cmake, cmake/ExternalFreeRDP.cmake
    
    - Add 'aheared' and 'rouines' to scripts/codespell.ignore:
      these are original SSLeay license spellings and must be
      reproduced verbatim per OpenSSL's LICENSE text
    
    - Add 'localY' to scripts/codespell.ignore:
      false positive for camelCase coordinate variable name

--- client/Harmony/cmake/ExternalOpenSSL.cmake
+++ client/Harmony/cmake/ExternalOpenSSL.cmake
@@ -1,8 +1,11 @@
 include(ExternalProject)
 
 set(OPENSSL_VERSION "openssl-3.6.1")
-set(OPENSSL_URL_HASH "SHA256=b1bfedcd5b289ff22aee87c9d600f515767ebf45f77168cb6d64f231f518a82e")
-set(OPENSSL_URL "https://github.com/openssl/openssl/releases/download/${OPENSSL_VERSION}/${OPENSSL_VERSION}.tar.gz")
+set(OPENSSL_URL_HASH
+    "SHA256=b1bfedcd5b289ff22aee87c9d600f515767ebf45f77168cb6d64f231f518a82e")
+set(OPENSSL_URL
+    "https://github.com/openssl/openssl/releases/download/${OPENSSL_VERSION}/${OPENSSL_VERSION}.tar.gz"
+)
 
 set(OPENSSL_CACHE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../cache")
 if(EXISTS "${OPENSSL_CACHE_DIR}/${OPENSSL_VERSION}.tar.gz")
@@ -29,23 +32,25 @@ get_filename_component(OHOS_NATIVE_DIR "${OHOS_LLVM_DIR}" DIRECTORY)
 get_filename_component(OHOS_NDK_HOME "${OHOS_NATIVE_DIR}" DIRECTORY)
 
 set(OHOS_SYSROOT "${OHOS_NDK_HOME}/native/sysroot")
-set(OSSL_CFLAGS "-target ${OSSL_TARGET} --sysroot=${OHOS_SYSROOT} -D__MUSL__ ${OSSL_CFLAGS_EXTRA}")
+set(OSSL_CFLAGS
+    "-target ${OSSL_TARGET} --sysroot=${OHOS_SYSROOT} -D__MUSL__ ${OSSL_CFLAGS_EXTRA}"
+)
 
-set(HARMONY_OPENSSL_PATCH "${CMAKE_CURRENT_LIST_DIR}/../../../scripts/harmony-openssl.patch")
+set(HARMONY_OPENSSL_PATCH
+    "${CMAKE_CURRENT_LIST_DIR}/../../../scripts/harmony-openssl.patch")
 set(OPENSSL_INSTALL_DIR "${CMAKE_BINARY_DIR}/deps/openssl")
 
 set(OSSL_ENV
-  "PATH=${OHOS_TOOLCHAIN_BIN}:$ENV{PATH}"
-  "CC=${OHOS_TOOLCHAIN_BIN}/clang"
-  "CXX=${OHOS_TOOLCHAIN_BIN}/clang++"
-  "AR=${OHOS_TOOLCHAIN_BIN}/llvm-ar"
-  "LD=${OHOS_TOOLCHAIN_BIN}/ld.lld"
-  "STRIP=${OHOS_TOOLCHAIN_BIN}/llvm-strip"
-  "RANLIB=${OHOS_TOOLCHAIN_BIN}/llvm-ranlib"
-  "NM=${OHOS_TOOLCHAIN_BIN}/llvm-nm"
-  "CFLAGS=${OSSL_CFLAGS}"
-  "CXXFLAGS=${OSSL_CFLAGS}"
-)
+    "PATH=${OHOS_TOOLCHAIN_BIN}:$ENV{PATH}"
+    "CC=${OHOS_TOOLCHAIN_BIN}/clang"
+    "CXX=${OHOS_TOOLCHAIN_BIN}/clang++"
+    "AR=${OHOS_TOOLCHAIN_BIN}/llvm-ar"
+    "LD=${OHOS_TOOLCHAIN_BIN}/ld.lld"
+    "STRIP=${OHOS_TOOLCHAIN_BIN}/llvm-strip"
+    "RANLIB=${OHOS_TOOLCHAIN_BIN}/llvm-ranlib"
+    "NM=${OHOS_TOOLCHAIN_BIN}/llvm-nm"
+    "CFLAGS=${OSSL_CFLAGS}"
+    "CXXFLAGS=${OSSL_CFLAGS}")
 
 ExternalProject_Add(
   openssl
@@ -54,30 +59,30 @@ ExternalProject_Add(
   URL_HASH ${OPENSSL_URL_HASH}
   PATCH_COMMAND patch -p1 -s < ${HARMONY_OPENSSL_PATCH}
   CONFIGURE_COMMAND
-    ${CMAKE_COMMAND} -E env ${OSSL_ENV}
-    perl <SOURCE_DIR>/Configure ${OSSL_ARCH}
+    ${CMAKE_COMMAND} -E env ${OSSL_ENV} perl <SOURCE_DIR>/Configure ${OSSL_ARCH}
     no-shared no-module no-tests no-apps no-docs no-legacy no-dso no-ssl-trace
-    --prefix=${OPENSSL_INSTALL_DIR}
-    --libdir=${OPENSSL_INSTALL_DIR}
+    --prefix=${OPENSSL_INSTALL_DIR} --libdir=${OPENSSL_INSTALL_DIR}
     --openssldir=${OPENSSL_INSTALL_DIR}
-  BUILD_COMMAND
-    ${CMAKE_COMMAND} -E env ${OSSL_ENV}
-    make build_libs -j
-  INSTALL_COMMAND
-    ${CMAKE_COMMAND} -E make_directory ${OPENSSL_INSTALL_DIR}/include/openssl
-    COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include/openssl ${OPENSSL_INSTALL_DIR}/include/openssl
-    COMMAND ${CMAKE_COMMAND} -E copy_directory <BINARY_DIR>/include/openssl ${OPENSSL_INSTALL_DIR}/include/openssl
-    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/libssl.a ${OPENSSL_INSTALL_DIR}/libssl.a
-    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/libcrypto.a ${OPENSSL_INSTALL_DIR}/libcrypto.a
-  BUILD_BYPRODUCTS
-    ${OPENSSL_INSTALL_DIR}/libssl.a
-    ${OPENSSL_INSTALL_DIR}/libcrypto.a
-)
+  BUILD_COMMAND ${CMAKE_COMMAND} -E env ${OSSL_ENV} make build_libs -j
+  INSTALL_COMMAND ${CMAKE_COMMAND} -E make_directory
+                  ${OPENSSL_INSTALL_DIR}/include/openssl
+  COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include/openssl
+          ${OPENSSL_INSTALL_DIR}/include/openssl
+  COMMAND ${CMAKE_COMMAND} -E copy_directory <BINARY_DIR>/include/openssl
+          ${OPENSSL_INSTALL_DIR}/include/openssl
+  COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/libssl.a
+          ${OPENSSL_INSTALL_DIR}/libssl.a
+  COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/libcrypto.a
+          ${OPENSSL_INSTALL_DIR}/libcrypto.a
+  BUILD_BYPRODUCTS ${OPENSSL_INSTALL_DIR}/libssl.a
+                   ${OPENSSL_INSTALL_DIR}/libcrypto.a)
 
 add_library(openssl-lib INTERFACE IMPORTED GLOBAL)
-set_target_properties(openssl-lib PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "${OPENSSL_INSTALL_DIR}/include"
-  INTERFACE_LINK_LIBRARIES "${OPENSSL_INSTALL_DIR}/libssl.a;${OPENSSL_INSTALL_DIR}/libcrypto.a"
-)
+set_target_properties(
+  openssl-lib
+  PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${OPENSSL_INSTALL_DIR}/include"
+    INTERFACE_LINK_LIBRARIES
+    "${OPENSSL_INSTALL_DIR}/libssl.a;${OPENSSL_INSTALL_DIR}/libcrypto.a")
 add_dependencies(openssl-lib openssl)
 file(MAKE_DIRECTORY ${OPENSSL_INSTALL_DIR}/include)
