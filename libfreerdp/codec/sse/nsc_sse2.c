dcba567468f7266b3b520274376c2b5b500ac444
Armin Novak <armin.novak@thincast.com>
2026-06-09 08:35:14 +0200

    [codec,sse] add encoder checks
    nsc_encode_subsampling_sse2 was lacking input length validation, so add
    it.
    Reported by Youngjae Choi

--- libfreerdp/codec/sse/nsc_sse2.c
+++ libfreerdp/codec/sse/nsc_sse2.c
@@ -380,53 +380,54 @@ static BOOL nsc_encode_argb_to_aycocg_sse2(NSC_CONTEXT* context, const BYTE* dat
 	return TRUE;
 }
 
-static void nsc_encode_subsampling_sse2(NSC_CONTEXT* context)
+static BOOL nsc_encode_subsampling_sse2(NSC_CONTEXT* context)
 {
-	BYTE* co_dst = nullptr;
-	BYTE* cg_dst = nullptr;
-	INT8* co_src0 = nullptr;
-	INT8* co_src1 = nullptr;
-	INT8* cg_src0 = nullptr;
-	INT8* cg_src1 = nullptr;
-	UINT32 tempWidth = 0;
-	UINT32 tempHeight = 0;
-	__m128i t;
-	__m128i val;
 	__m128i mask = _mm_set1_epi16(0xFF);
-	tempWidth = ROUND_UP_TO(context->width, 8);
-	tempHeight = ROUND_UP_TO(context->height, 2);
+	const UINT32 tempWidth = ROUND_UP_TO(context->width, 8);
+	const UINT32 tempHeight = ROUND_UP_TO(context->height, 2);
+
+	if (tempHeight == 0)
+		return FALSE;
+
+	if (tempWidth > context->priv->PlaneBuffersLength / tempHeight)
+		return FALSE;
 
 	for (size_t y = 0; y < tempHeight >> 1; y++)
 	{
-		co_dst = context->priv->PlaneBuffers[1] + y * (tempWidth >> 1);
-		cg_dst = context->priv->PlaneBuffers[2] + y * (tempWidth >> 1);
-		co_src0 = (INT8*)context->priv->PlaneBuffers[1] + (y << 1) * tempWidth;
-		co_src1 = co_src0 + tempWidth;
-		cg_src0 = (INT8*)context->priv->PlaneBuffers[2] + (y << 1) * tempWidth;
-		cg_src1 = cg_src0 + tempWidth;
+		BYTE* co_dst = context->priv->PlaneBuffers[1] + y * (tempWidth >> 1);
+		BYTE* cg_dst = context->priv->PlaneBuffers[2] + y * (tempWidth >> 1);
+		INT8* co_src0 = (INT8*)context->priv->PlaneBuffers[1] + (y << 1) * tempWidth;
+		INT8* co_src1 = co_src0 + tempWidth;
+		INT8* cg_src0 = (INT8*)context->priv->PlaneBuffers[2] + (y << 1) * tempWidth;
+		INT8* cg_src1 = cg_src0 + tempWidth;
 
 		for (UINT32 x = 0; x < tempWidth >> 1; x += 8)
 		{
-			t = LOAD_SI128(co_src0);
-			t = _mm_avg_epu8(t, LOAD_SI128(co_src1));
-			val = _mm_and_si128(_mm_srli_si128(t, 1), mask);
-			val = _mm_avg_epu16(val, _mm_and_si128(t, mask));
-			val = _mm_packus_epi16(val, val);
-			STORE_SI128(co_dst, val);
-			co_dst += 8;
-			co_src0 += 16;
-			co_src1 += 16;
-			t = LOAD_SI128(cg_src0);
-			t = _mm_avg_epu8(t, LOAD_SI128(cg_src1));
-			val = _mm_and_si128(_mm_srli_si128(t, 1), mask);
-			val = _mm_avg_epu16(val, _mm_and_si128(t, mask));
-			val = _mm_packus_epi16(val, val);
-			STORE_SI128(cg_dst, val);
-			cg_dst += 8;
-			cg_src0 += 16;
-			cg_src1 += 16;
+			{
+				__m128i t = LOAD_SI128(co_src0);
+				t = _mm_avg_epu8(t, LOAD_SI128(co_src1));
+				__m128i val = _mm_and_si128(_mm_srli_si128(t, 1), mask);
+				val = _mm_avg_epu16(val, _mm_and_si128(t, mask));
+				val = _mm_packus_epi16(val, val);
+				STORE_SI128(co_dst, val);
+				co_dst += 8;
+				co_src0 += 16;
+				co_src1 += 16;
+			}
+			{
+				__m128i t = LOAD_SI128(cg_src0);
+				t = _mm_avg_epu8(t, LOAD_SI128(cg_src1));
+				__m128i val = _mm_and_si128(_mm_srli_si128(t, 1), mask);
+				val = _mm_avg_epu16(val, _mm_and_si128(t, mask));
+				val = _mm_packus_epi16(val, val);
+				STORE_SI128(cg_dst, val);
+				cg_dst += 8;
+				cg_src0 += 16;
+				cg_src1 += 16;
+			}
 		}
 	}
+	return TRUE;
 }
 
 static BOOL nsc_encode_sse2(NSC_CONTEXT* WINPR_RESTRICT context, const BYTE* WINPR_RESTRICT data,
@@ -436,7 +437,7 @@ static BOOL nsc_encode_sse2(NSC_CONTEXT* WINPR_RESTRICT context, const BYTE* WIN
 		return FALSE;
 
 	if (context->ChromaSubsamplingLevel > 0)
-		nsc_encode_subsampling_sse2(context);
+		return nsc_encode_subsampling_sse2(context);
 
 	return TRUE;
 }
