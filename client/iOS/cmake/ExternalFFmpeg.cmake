28f496c858f6e0d8cc79c1885d246e27a626cb24
bho3538 <bho3538@gmail.com>
2026-06-24 12:20:10 +0900

    modify build scripts (2)

--- client/iOS/cmake/ExternalFFmpeg.cmake
+++ client/iOS/cmake/ExternalFFmpeg.cmake
@@ -8,15 +8,38 @@ if(PLATFORM MATCHES "SIMULATOR")
   set(_ff_sdk "iphonesimulator")
   set(_ff_min "-mios-simulator-version-min=${DEPLOYMENT_TARGET}")
   set(_ff_triple "-target ${_ff_arch}-apple-ios${DEPLOYMENT_TARGET}-simulator")
+  set(_ff_platform "iPhoneSimulator")
 else()
   set(_ff_sdk "iphoneos")
   set(_ff_min "-miphoneos-version-min=${DEPLOYMENT_TARGET}")
   set(_ff_triple "")
+  set(_ff_platform "iPhoneOS")
 endif()
 
 execute_process(COMMAND xcrun --sdk ${_ff_sdk} --show-sdk-path OUTPUT_VARIABLE _ff_sysroot OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 set(_ff_flags "-arch ${_ff_arch} ${_ff_min} -isysroot ${_ff_sysroot} ${_ff_triple}")
+
+set(_ff_fw ${DEPS_INSTALL_DIR}/FFmpeg.framework)
+
+# Framework Info.plist
+file(
+  WRITE ${CMAKE_BINARY_DIR}/FFmpeg-Info.plist
+  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
+<plist version=\"1.0\"><dict>
+  <key>CFBundleDevelopmentRegion</key><string>en</string>
+  <key>CFBundleExecutable</key><string>FFmpeg</string>
+  <key>CFBundleIdentifier</key><string>org.ffmpeg.FFmpeg</string>
+  <key>CFBundleInfoDictionaryVersion</key><string>6.0</string>
+  <key>CFBundleName</key><string>FFmpeg</string>
+  <key>CFBundlePackageType</key><string>FMWK</string>
+  <key>CFBundleShortVersionString</key><string>${FFMPEG_VERSION}</string>
+  <key>CFBundleVersion</key><string>${FFMPEG_VERSION}</string>
+  <key>MinimumOSVersion</key><string>${DEPLOYMENT_TARGET}</string>
+  <key>CFBundleSupportedPlatforms</key><array><string>${_ff_platform}</string></array>
+</dict></plist>
+")
 
 ExternalProject_Add(
   ffmpeg
@@ -36,6 +59,12 @@ ExternalProject_Add(
     --enable-decoder=h264 --enable-parser=h264 --enable-decoder=hevc --enable-parser=hevc
     --enable-videotoolbox --enable-hwaccel=h264_videotoolbox --enable-hwaccel=hevc_videotoolbox
   BUILD_COMMAND make -j
-  INSTALL_COMMAND make -j install
-  BUILD_BYPRODUCTS ${DEPS_INSTALL_DIR}/lib/libavcodec.a ${DEPS_INSTALL_DIR}/lib/libavutil.a
+  INSTALL_COMMAND
+    make -j install
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${_ff_fw}/Headers
+    COMMAND sh -c "clang -dynamiclib -arch ${_ff_arch} -isysroot ${_ff_sysroot} ${_ff_min} ${_ff_triple} -install_name @rpath/FFmpeg.framework/FFmpeg -Wl,-force_load,${DEPS_INSTALL_DIR}/lib/libavcodec.a -Wl,-force_load,${DEPS_INSTALL_DIR}/lib/libavutil.a -framework VideoToolbox -framework CoreMedia -framework CoreVideo -framework CoreFoundation -o ${_ff_fw}/FFmpeg"
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${DEPS_INSTALL_DIR}/include/libavcodec ${_ff_fw}/Headers/libavcodec
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${DEPS_INSTALL_DIR}/include/libavutil ${_ff_fw}/Headers/libavutil
+    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/FFmpeg-Info.plist ${_ff_fw}/Info.plist
+  BUILD_BYPRODUCTS ${_ff_fw}/FFmpeg
 )
