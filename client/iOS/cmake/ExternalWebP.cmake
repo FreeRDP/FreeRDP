44f61ef5b49ae1a0724a190cc4731e0373bc0849
Armin Novak <armin.novak@thincast.com>
2026-07-01 14:01:58 +0200

    [client,ios] update build
    * Detach client build from FreeRDP
    * Move DepVersions.cmake to top level, share between Android and iOS
    * Copy external project from Android client, adapt to iOS toolchain
    * Clean up and simplify iOS top level CMakeLists.txt

---
+++ client/iOS/cmake/ExternalWebP.cmake
@@ -0,0 +1,20 @@
+include(ExternalProject)
+include(DepVersions)
+
+ExternalProject_Add(
+  webp
+  DOWNLOAD_EXTRACT_TIMESTAMP OFF
+  SOURCE_DIR ${CMAKE_SOURCE_DIR}/external/webp
+  URL https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-${WEBP_VERSION}.tar.gz
+  URL_HASH ${WEBP_HASH}
+  CMAKE_ARGS ${IOS_CMAKE_ARGS}
+             -DCMAKE_INSTALL_PREFIX:PATH=${DEPS_INSTALL_DIR}
+             -DCMAKE_INSTALL_LIBDIR:STRING=${CMAKE_INSTALL_LIBDIR}
+             -DWEBP_BUILD_CWEBP:BOOL=OFF
+             -DWEBP_BUILD_DWEBP:BOOL=OFF
+             -DWEBP_BUILD_GIF2WEBP:BOOL=OFF
+             -DWEBP_BUILD_IMG2WEBP:BOOL=OFF
+             -DWEBP_BUILD_VWEBP:BOOL=OFF
+             -DWEBP_BUILD_WEBPINFO:BOOL=OFF
+             -DWEBP_BUILD_WEBPMUX:BOOL=OFF
+)
