eec120d760d6f2855032d70b10c39f8bae908e75
Brad Smith <brad@comstyle.com>
2026-08-13 23:40:20 -0400

    uwac: don't try to use O_TMPFILE on OpenBSD

--- uwac/libuwac/uwac-os.c
+++ uwac/libuwac/uwac-os.c
@@ -43,7 +43,7 @@
 #endif
 
 /* uClibc and uClibc-ng don't provide O_TMPFILE */
-#if !defined(O_TMPFILE) && !defined(__FreeBSD__)
+#if !defined(O_TMPFILE) && !defined(__FreeBSD__) && !defined(__OpenBSD__)
 #define O_TMPFILE (020000000 | O_DIRECTORY)
 #endif
 
@@ -253,9 +253,9 @@ int uwac_create_anonymous_file(off_t size)
 	fd = open(path, O_TMPFILE | O_RDWR | O_EXCL, 0600);
 #else
 	/*
-	 * Some platforms (e.g. FreeBSD) won't support O_TMPFILE and can't
-	 * reasonably emulate it at first blush.  Opt to make them rely on
-	 * the create_tmpfile_cloexec() path instead.
+	 * Some platforms (e.g. FreeBSD/OpenBSD) won't support O_TMPFILE and
+	 * can't reasonably emulate it at first blush. Opt to make them rely
+	 * on the create_tmpfile_cloexec() path instead.
 	 */
 	fd = -1;
 #endif
