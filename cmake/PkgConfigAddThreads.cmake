c6f7250e2d4ceac2750ca391dfc5f8bbaa1797d1
Armin Novak <armin.novak@thincast.com>
2026-08-28 12:00:39 +0200

    [cmake] Add support for CMake < 3.18
    check_linker_flag requires CMake 3.18 or newer. For older systems fall
    back to unconditionally add linker libraries. Print a warning to inform
    packagers about this.

--- cmake/PkgConfigAddThreads.cmake
+++ cmake/PkgConfigAddThreads.cmake
@@ -1,11 +1,22 @@
-include(CheckLinkerFlag)
+if(CMAKE_VERSION GREATER_EQUAL "3.18")
+  include(CheckLinkerFlag)
+endif()
 
 function(pkg_config_add_threads var)
   set(LIBS "")
   find_package(Threads REQUIRED)
 
   foreach(LIB IN LISTS ARGN)
-    check_linker_flag(C "-l${LIB}" HAVE_LIB_${LIB})
+    if(CMAKE_VERSION GREATER_EQUAL "3.18")
+      check_linker_flag(C "-l${LIB}" HAVE_LIB_${LIB})
+    else()
+      message(
+        WARNING
+          "check_linker_flag not available with CMake < 3.18. Unconditionally adding -l${LIB} to pkg-config link library list"
+      )
+      set(HAVE_LIB_${LIB} ON)
+    endif()
+
     if(HAVE_LIB_${LIB})
       list(APPEND LIBS "-l${LIB}")
     endif()
