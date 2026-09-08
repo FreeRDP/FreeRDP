3f1d7f64feb28d0bd0f3a5d02d94d01649ffc814
Armin Novak <armin.novak@thincast.com>
2026-09-08 12:25:48 +0200

    [winpr,utils] speed up fuzzer test
    In TestFuzzCredSSP_Asn1 set a NULL appender to prevent the stdio spam
    from verbose logging to cause unnecessary delay.

--- winpr/libwinpr/utils/test/TestFuzzCredSSP_Asn1.c
+++ winpr/libwinpr/utils/test/TestFuzzCredSSP_Asn1.c
@@ -107,6 +107,7 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 	if (!loggingInitialized)
 	{
 		(void)WLog_SetLogLevel(WLog_GetRoot(), WLOG_TRACE);
+		(void)WLog_SetLogAppenderType(WLog_GetRoot(), WLOG_APPENDER_CALLBACK);
 		loggingInitialized = TRUE;
 	}
 
