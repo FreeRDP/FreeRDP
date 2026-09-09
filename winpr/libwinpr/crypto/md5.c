bee4c7eee33be1ac81f3ac812de1c8ba3f7f2bd1
Armin Novak <armin.novak@thincast.com>
2026-09-09 10:53:43 +0200

    [winpr,crypto] only compile unaligned access if WINPR_ARCH_SUPPORTED

--- winpr/libwinpr/crypto/md5.c
+++ winpr/libwinpr/crypto/md5.c
@@ -92,7 +92,7 @@ static inline winpr_MD5_u32plus I(winpr_MD5_u32plus x, winpr_MD5_u32plus y, winp
  * link-time optimizations.  For the time being, keeping these MD5 routines in
  * their own translation unit avoids the problem.
  */
-#if defined(__i386__) || defined(__x86_64__) || defined(__vax__)
+#if defined(WINPR_ARCH_SUPPORTED)
 #define SET(n) (*(WINPR_PACKED_ALIGN_CAST(const winpr_MD5_u32plus*, &ptr[4ULL * (n)])))
 #define GET(n) SET(n)
 #else
