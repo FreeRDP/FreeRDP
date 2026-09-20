0ef6eb6ba3f90dc42cb7f4b8793e2d365261e4f0
Armin Novak <armin.novak@thincast.com>
2026-09-20 12:14:54 +0200

    [winpr,utils] fix stride calculation for jpeg decompress

--- winpr/libwinpr/utils/image.c
+++ winpr/libwinpr/utils/image.c
@@ -54,6 +54,10 @@
 #include "../log.h"
 #define TAG WINPR_TAG("utils.image")
 
+#ifndef MAX
+#define MAX(a, b) ((a) > (b)) ? (a) : (b)
+#endif
+
 static SSIZE_T winpr_convert_from_jpeg(const BYTE* comp_data, size_t comp_data_bytes, UINT32* width,
                                        UINT32* height, UINT32* bpp, BYTE** ppdecomp_data);
 static SSIZE_T winpr_convert_from_png(const BYTE* comp_data, size_t comp_data_bytes, UINT32* width,
@@ -790,6 +794,7 @@ SSIZE_T winpr_convert_from_jpeg(WINPR_ATTR_UNUSED const BYTE* comp_data,
 		goto fail;
 
 	cinfo.out_color_space = cinfo.num_components > 3 ? JCS_EXT_RGBA : JCS_EXT_BGR;
+	const size_t components = MAX(3, cinfo.num_components);
 
 	*width = WINPR_ASSERTING_INT_CAST(uint32_t, cinfo.image_width);
 	*height = WINPR_ASSERTING_INT_CAST(uint32_t, cinfo.image_height);
@@ -798,8 +803,7 @@ SSIZE_T winpr_convert_from_jpeg(WINPR_ATTR_UNUSED const BYTE* comp_data,
 	if (!jpeg_start_decompress(&cinfo))
 		goto fail;
 
-	size_t stride =
-	    1ULL * cinfo.image_width * WINPR_ASSERTING_INT_CAST(uint32_t, cinfo.num_components);
+	size_t stride = 1ULL * cinfo.image_width * components;
 
 	if ((stride == 0) || (cinfo.image_height == 0))
 		goto fail;
