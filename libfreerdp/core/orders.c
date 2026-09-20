90bb49c576e4a5473da673c76c60ceea661c4ae2
Armin Novak <armin.novak@thincast.com>
2026-09-20 10:02:36 +0200

    [core] check for session acitve

--- libfreerdp/core/orders.c
+++ libfreerdp/core/orders.c
@@ -4528,6 +4528,9 @@ BOOL update_recv_order(rdpUpdate* update, wStream* s)
 	BYTE controlFlags = 0;
 	rdp_update_internal* up = update_cast(update);
 
+	if (rdp_get_state(update->context->rdp) < CONNECTION_STATE_ACTIVE)
+		return FALSE;
+
 	if (!Stream_CheckAndLogRequiredLength(TAG, s, 1))
 		return FALSE;
 
